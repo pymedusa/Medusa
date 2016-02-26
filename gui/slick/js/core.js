bf3cd749506e35141dda38f6c0f6c979ebffaaef
P0psicles <rogier@headshots.nl>
2016-02-26 17:46:11 +0100

    After with a succesfull snatch the download icon is changd to save.png, as indication.
    * Fixed some grunt errors.
    * Added the core.min.js
    * Still got the get_id() error for the provider in webserve.py. Hope is fixed with this.

--- gui/slick/js/core.js
+++ gui/slick/js/core.js
@@ -2449,17 +2449,30 @@ var SICKRAGE = {
                 });
             };
 
-            $('input.manualSearchButton').on('click', function(){
-                show = $(this).attr('data-show');
-                season = $(this).attr('data-season');
-                episode = $(this).attr('data-episode');
-                performSearch = $(this).attr('data-force-search');
+            $('input.manualSearchButton').on('click', function(event){
+                event.preventDefault();
+                var show = $(this).attr('data-show');
+                var season = $(this).attr('data-season');
+                var episode = $(this).attr('data-episode');
+                var performSearch = $(this).attr('data-force-search');
 
                 $('#wrapper').loadContainer(
                         '/home/manualSelect?show=' + show + '&season=' + season + '&episode=' + episode + '&perform_search=' + performSearch,
                         'Loading new search results...',
                         'Time out, refresh page to try again'
                 );
+            });
+            
+            $('a.epManualSnatch').on('click', function(event){
+                event.preventDefault();
+                var link = this;
+                $.getJSON(this.href, 
+                    function (data) {
+                    if (data.result === "success") {
+                        $(link).children('img').attr('src', srRoot + '/images/save.png');
+                    }
+                });
+                
             });
         },
         postProcess: function() {
