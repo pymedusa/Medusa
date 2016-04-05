a10f520094b88a235feb47ce7c8e6a2798a9df40
P0psicles <rogier@headshots.nl>
2016-04-05 11:23:37 +0200

    Added first version (modified) simpleanidb lib's

---
+++ lib/simpleanidb/helper.py
@@ -0,0 +1,17 @@
+from datetime import date
+import requests
+
+def download_file(local_filename, url):
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    return local_filename
+
+def date_to_date(date_str):
+    return date(
+        *map(int, date_str.split("-"))
+    )
