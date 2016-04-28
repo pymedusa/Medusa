6736af1a28ba43a9079aecd95c588f5211b8a942
supergonkas <matigonkas@outlook.com>
2016-04-28 15:03:27 +0100

    Fix RSS on limetorrents (#503)

--- sickbeard/providers/limetorrents.py
+++ sickbeard/providers/limetorrents.py
@@ -23,6 +23,7 @@ import requests
 import traceback
 
 from requests.compat import urljoin
+from contextlib2 import suppress
 
 from sickbeard import logger, tvcache
 from sickbeard.bs4_parser import BS4Parser
@@ -35,6 +36,9 @@ hash_regex = re.compile(r'(.*)([0-9a-f]{40})(.*)', re.I)
 
 
 class LimeTorrentsProvider(TorrentProvider):  # pylint: disable=too-many-instance-attributes
+    """
+    Search provider LimeTorrents
+    """
 
     def __init__(self):
 
@@ -47,7 +51,7 @@ class LimeTorrentsProvider(TorrentProvider):  # pylint: disable=too-many-instanc
             'index': self.url,
             'update': urljoin(self.url, '/post/updatestats.php'),
             'search': urljoin(self.url, '/search/tv/{query}/'),
-            'rss': urljoin(self.url, '/browse-torrents/TV-shows/date/')
+            'rss': urljoin(self.url, '/browse-torrents/TV-shows/date/{page}/')
         }
 
         # Credentials
@@ -62,87 +66,118 @@ class LimeTorrentsProvider(TorrentProvider):  # pylint: disable=too-many-instanc
         self.proper_strings = ['PROPER', 'REPACK', 'REAL']
 
         # Cache
-        cache_params = {'RSS': ['1/', '2/', '3/']}
-        self.cache = tvcache.TVCache(self, min_time=10, search_params=cache_params)
+        self.cache = tvcache.TVCache(self, min_time=10)
 
     def search(self, search_strings, age=0, ep_obj=None):  # pylint: disable=too-many-branches,too-many-locals
+        """
+        Search the provider for results
+
+        :param search_strings: Search to perform
+        :param age: Not used for this provider
+        :param ep_obj: Not used for this provider
+
+        :return: A list of items found
+        """
         results = []
         for mode in search_strings:
-            items = []
             logger.log('Search Mode: {0}'.format(mode), logger.DEBUG)
             for search_string in search_strings[mode]:
-                search_url = self.urls['rss'] if mode == 'RSS' else self.urls['search'].format(query=search_string)
-                data = self.get_url(search_url, returns='text')
+                if mode == 'RSS':
+                    for page in range(1, 4):
+                        search_url = self.urls['rss'].format(page=page)
+                        data = self.get_url(search_url, returns='text')
+                        items = self.parse(data, mode)
+                        results += items
+                else:
+                    search_url = self.urls['search'].format(query=search_string)
+                    data = self.get_url(search_url, returns='text')
+                    items = self.parse(data, mode)
+                    results += items
                 if not data:
                     logger.log('No data returned from provider', logger.DEBUG)
                     continue
+        return results
 
-                with BS4Parser(data, 'html5lib') as html:
-                    torrent_table = html('table', class_='table2')
-                    if mode != 'RSS' and len(torrent_table) < 2:
-                        logger.log(u'Data returned from provider does not contain any torrents', logger.DEBUG)
+    def parse(self, data, mode):
+        """
+        Parse search results for items
+
+        :param data: The raw response from a search
+        :param mode: The current mode used to search, e.g. RSS
+
+        :return: A list of items found
+        """
+        items = []
+        with BS4Parser(data, 'html5lib') as html:
+            torrent_table = html('table', class_='table2')
+            if mode != 'RSS' and len(torrent_table) < 2:
+                logger.log(u'Data returned from provider does not contain any torrents', logger.DEBUG)
+                return
+
+            torrent_table = torrent_table[0 if mode == 'RSS' else 1]
+            torrent_rows = torrent_table('tr')
+            first = True
+            for result in torrent_rows:
+                # Skip the first, since it isn't a valid result
+                if first:
+                    first = False
+                    continue
+                cells = result('td')
+                try:
+                    verified = result('img', title='Verified torrent')
+                    if self.confirmed and not verified:
+                        continue
+                    titleinfo = result('a')
+                    info = titleinfo[1]['href']
+                    torrent_id = id_regex.search(info).group(1)
+                    url = result.find('a', rel='nofollow')
+                    if not url:
+                        continue
+                    torrent_hash = hash_regex.search(url['href']).group(2)
+                    if not torrent_id or not torrent_hash:
+                        continue
+                    with suppress(requests.exceptions.Timeout):
+                        # Suppress the timeout since we are not interested in actually getting the results
+                        hashdata = self.session.get(self.urls['update'], timeout=0.1,
+                                                    params={'torrent_id': torrent_id,
+                                                            'infohash': torrent_hash})
+                    title = titleinfo[1].get_text(strip=True)
+                    # Remove comma as thousands separator from larger number like 2,000 seeders = 2000
+                    seeders = try_int(cells[3].get_text(strip=True).replace(',', ''))
+                    leechers = try_int(cells[4].get_text(strip=True).replace(',', ''))
+                    size = convert_size(cells[2].get_text(strip=True)) or -1
+                    download_url = 'magnet:?xt=urn:btih:{hash}&dn={title}{trackers}'.format(
+                        hash=torrent_hash, title=title, trackers=self._custom_trackers)
+
+                    if seeders < min(self.minseed, 1):
+                        if mode != 'RSS':
+                            logger.log('Discarding torrent because it doesn\'t meet the minimum '
+                                       'seeders or leechers: {0}. Seeders: {1})'.format
+                                       (title, seeders), logger.DEBUG)
                         continue
 
-                    torrent_table = torrent_table[0 if mode == 'RSS' else 1]
-                    torrent_rows = torrent_table('tr')
-                    for result in torrent_rows:
-                        cells = result('td')
-                        try:
-                            verified = result('img', title='Verified torrent')
-                            if self.confirmed and not verified:
-                                continue
-                            titleinfo = result('a')
-                            info = titleinfo[1]['href']
-                            torrent_id = id_regex.search(info).group(1)
-                            url = result.find('a', rel='nofollow')
-                            if not url:
-                                continue
-                            torrent_hash = hash_regex.search(url['href']).group(2)
-                            try:
-                                self.session.get(self.urls['update'], timeout=0.1,
-                                                 params={'torrent_id': torrent_id,
-                                                         'infohash': torrent_hash})
-                            except requests.exceptions.Timeout:
-                                pass
-                            title = titleinfo[1].get_text(strip=True)
-                            # Remove comma from larger number like 2,000 seeders = 2000
-                            seeders = try_int(cells[3].get_text(strip=True).replace(',', ''))
-                            leechers = try_int(cells[4].get_text(strip=True).replace(',', ''))
-                            size = convert_size(cells[2].get_text(strip=True)) or -1
-                            download_url = 'magnet:?xt=urn:btih:{hash}&dn={title}{trackers}' .format(
-                                hash=torrent_hash, title=title, trackers=self._custom_trackers)
+                    item = {
+                        'title': title,
+                        'link': download_url,
+                        'size': size,
+                        'seeders': seeders,
+                        'leechers': leechers,
+                        'hash': torrent_hash or ''
+                    }
 
-                            if seeders < min(self.minseed, 1):
-                                if mode != 'RSS':
-                                    logger.log('Discarding torrent because it doesn\'t meet the minimum '
-                                               'seeders or leechers: {0}. Seeders: {1})'.format
-                                               (title, seeders), logger.DEBUG)
-                                continue
+                    # if mode != 'RSS':
+                    logger.log('Found result: {0} with {1} seeders and {2} leechers'.format
+                               (title, seeders, leechers), logger.DEBUG)
+                    items.append(item)
 
-                            item = {
-                                'title': title,
-                                'link': download_url,
-                                'size': size,
-                                'seeders': seeders,
-                                'leechers': leechers,
-                                'pubdate': None,
-                                'hash': torrent_hash or ''
-                            }
+                except StandardError:
+                    logger.log(u"Failed parsing provider. Traceback: {!r}".format(traceback.format_exc()), logger.ERROR)
+                    continue
 
-                            if mode != 'RSS':
-                                logger.log('Found result: {0} with {1} seeders and {2} leechers'.format
-                                           (title, seeders, leechers), logger.DEBUG)
-                            items.append(item)
+                    # For each search mode sort all the items by seeders if available
 
-                        except StandardError:
-                            logger.log(u"Failed parsing provider. Traceback: %r" % traceback.format_exc(), logger.ERROR)
-                            continue
-
-            # For each search mode sort all the items by seeders if available
             items.sort(key=lambda d: try_int(d.get('seeders', 0)), reverse=True)
-            results += items
-
-        return results
+        return items
 
 
 provider = LimeTorrentsProvider()
