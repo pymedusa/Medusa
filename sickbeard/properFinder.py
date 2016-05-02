4e302403eeb9bce07f0758e9acf74237fc8db6e4
Fernando <fernandog@users.noreply.github.com>
2016-05-02 11:19:26 -0300

    Fix proper thread name

--- sickbeard/properFinder.py
+++ sickbeard/properFinder.py
@@ -147,7 +147,7 @@ class ProperFinder(object):  # pylint: disable=too-few-public-methods
                     proper.provider = cur_provider
                     propers[name] = proper
 
-            threading.currentThread().name = original_thread_name
+        threading.currentThread().name = original_thread_name
 
         # take the list of unique propers and get it sorted by
         sortedPropers = sorted(propers.values(), key=operator.attrgetter('date'), reverse=True)
