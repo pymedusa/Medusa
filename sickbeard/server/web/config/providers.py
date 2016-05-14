a9f808fc368b8da664aa35ce8567c0dd4c874b65
Labrys <Labrys.git@gmail.com>
2016-05-14 18:16:49 -0400

    Optimize imports

--- sickbeard/server/web/config/providers.py
+++ sickbeard/server/web/config/providers.py
@@ -1,30 +1,18 @@
 # coding=utf-8
 
-import os
-import time
-from unrar2 import RarFile
+from __future__ import unicode_literals
 
+import os
 import sickbeard
 from sickbeard import (
-    config, helpers, logger, naming, subtitles, ui,
-)
-from sickbeard.common import (
-    Quality, WANTED,
+    config, logger, ui,
 )
 from sickbeard.providers import newznab, rsstorrent
-from sickbeard.versionChecker import CheckVersion
-
 from sickrage.helper.common import try_int
 from sickrage.helper.encoding import ek
-from sickrage.helper.exceptions import (
-    ex
-)
 from sickrage.providers.GenericProvider import GenericProvider
-
 from tornado.routes import route
-from sickbeard.server.web.core import (
-    WebRoot, PageTemplate
-)
+from sickbeard.server.web.core import PageTemplate
 
 # Conditional imports
 try:
