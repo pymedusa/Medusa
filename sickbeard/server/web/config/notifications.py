bd3b80045c59a25b6eedd566183acc9791fe6f96
Labrys <Labrys.git@gmail.com>
2016-05-14 17:21:53 -0400

    Use unicode literals Use single quoted strings

--- sickbeard/server/web/config/notifications.py
+++ sickbeard/server/web/config/notifications.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from __future__ import unicode_literals
+
 import os
 import sickbeard
 from sickbeard import (
@@ -18,11 +20,11 @@ class ConfigNotifications(Config):
         super(ConfigNotifications, self).__init__(*args, **kwargs)
 
     def index(self):
-        t = PageTemplate(rh=self, filename="config_notifications.mako")
+        t = PageTemplate(rh=self, filename='config_notifications.mako')
 
         return t.render(submenu=self.ConfigMenu(), title='Config - Notifications',
                         header='Notifications', topmenu='config',
-                        controller="config", action="notifications")
+                        controller='config', action='notifications')
 
     def saveNotifications(self, use_kodi=None, kodi_always_on=None, kodi_notify_onsnatch=None,
                           kodi_notify_ondownload=None,
@@ -251,4 +253,4 @@ class ConfigNotifications(Config):
         else:
             ui.notifications.message('Configuration Saved', ek(os.path.join, sickbeard.CONFIG_FILE))
 
-        return self.redirect("/config/notifications/")
+        return self.redirect('/config/notifications/')
