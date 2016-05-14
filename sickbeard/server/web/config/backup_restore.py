8ec8433cf4b6c75dad7fc155d047f0a14d081841
Labrys <Labrys.git@gmail.com>
2016-05-14 16:34:52 -0400

    Optimize imports

--- sickbeard/server/web/config/backup_restore.py
+++ sickbeard/server/web/config/backup_restore.py
@@ -2,36 +2,11 @@
 
 import os
 import time
-from unrar2 import RarFile
-
 import sickbeard
-from sickbeard import (
-    config, helpers, logger, naming, subtitles, ui,
-)
-from sickbeard.common import (
-    Quality, WANTED,
-)
-from sickbeard.providers import newznab, rsstorrent
-from sickbeard.versionChecker import CheckVersion
-
-from sickrage.helper.common import try_int
+from sickbeard import helpers
 from sickrage.helper.encoding import ek
-from sickrage.helper.exceptions import (
-    ex
-)
-from sickrage.providers.GenericProvider import GenericProvider
-
 from tornado.routes import route
-from sickbeard.server.web.core import (
-    WebRoot, PageTemplate
-)
-
-# Conditional imports
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
+from sickbeard.server.web.core import PageTemplate
 from sickbeard.server.web.config.base import Config
 
 
