3b8c8c63f96bf30fc6ceb09b6ae182c25f742e92
P0psicles <rogier@headshots.nl>
2016-06-01 18:00:26 +0200

    Changed back to one line

--- sickbeard/server/web/manage/handler.py
+++ sickbeard/server/web/manage/handler.py
@@ -146,9 +146,8 @@ class Manage(Home, WebRoot):
                     b'AND showid = ?'.format(statuses=','.join(['?'] * len(status_list))),
                     status_list + [cur_indexer_id]
                 )
-                all_eps = ['{season}x{episode}'.format(season=x[b'season'],
-                                                       episode=x[b'episode'])
-                           for x in all_eps_results]
+
+                all_eps = ['{season}x{episode}'.format(season=x[b'season'], episode=x[b'episode']) for x in all_eps_results]
                 to_change[cur_indexer_id] = all_eps
 
             self.setStatus(cur_indexer_id, '|'.join(to_change[cur_indexer_id]), newStatus, direct=True)
