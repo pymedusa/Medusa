238cef657defce83cf976f4d93c4d0cc316be46a
medariox <medariox@users.noreply.github.com>
2016-06-11 18:38:54 +0200

    Improve Hounddawgs

--- sickbeard/providers/cpasbien.py
+++ sickbeard/providers/cpasbien.py
@@ -48,6 +48,7 @@ class CpasbienProvider(TorrentProvider):
         for mode in search_strings:
             items = []
             logger.log('Search Mode: {0}'.format(mode), logger.DEBUG)
+
             for search_string in search_strings[mode]:
 
                 if mode != 'RSS':
