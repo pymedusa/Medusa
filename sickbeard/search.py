d1b0bb8bfed41d6999f01d31dd85df08896455a1
P0psicles <rogier@headshots.nl>
2016-06-11 20:46:32 +0200

    Fix for omgwtfnzb, needed a default value, cause getattr doesn't do that by default.

--- sickbeard/search.py
+++ sickbeard/search.py
@@ -585,7 +585,7 @@ def searchProviders(show, episodes, forced_search=False, downCurQuality=False, m
                         foundResults[cur_provider.name][curEp] = searchResults[curEp]
 
                     # Sort the list by seeders if possible
-                    if cur_provider.provider_type == 'torrent' or getattr(cur_provider, 'torznab'):
+                    if cur_provider.provider_type == 'torrent' or getattr(cur_provider, 'torznab', None):
                         foundResults[cur_provider.name][curEp].sort(key=lambda d: int(d.seeders), reverse=True)
 
                 break
