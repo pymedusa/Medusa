3d98c9da433cee6b5d2e11fe012629c41c0d6feb
Rato <rato.aq2@gmail.com>
2016-06-29 23:16:01 +0200

    Handling hebsubs and netflixuhd

--- sickbeard/name_parser/rules/__init__.py
+++ sickbeard/name_parser/rules/__init__.py
@@ -4,11 +4,12 @@
 Guessit customization
 """
 from guessit.api import default_api
-from sickbeard.name_parser.rules.properties import (audio_codec, format_, language, other, size, screen_size,
+from sickbeard.name_parser.rules.properties import (audio_codec, blacklist, format_, language, other, size, screen_size,
                                                     subtitle_language)
 from sickbeard.name_parser.rules.rules import rules
 
 
+default_api.rebulk.rebulk(blacklist())
 default_api.rebulk.rebulk(format_())
 default_api.rebulk.rebulk(screen_size())
 default_api.rebulk.rebulk(audio_codec())
