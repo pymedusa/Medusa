13ce2b7eaaa2343bbe52c53d6c80dc39bcdfd4a5
p0ps <p0psicles@users.noreply.github.com>
2016-08-14 10:32:08 +0200

    Revert "Add web url global" (#889)

--- sickbeard/server/web/core/base.py
+++ sickbeard/server/web/core/base.py
@@ -249,9 +249,6 @@ class WebHandler(BaseHandler):
             route = route.strip('/').replace('.', '_') or 'index'
             method = getattr(self, route)
 
-            # Configure the WEB_URL global, with the request protocol + host and WEB_ROOT if exists.
-            sickbeard.WEB_URL = self.request.protocol + '://' + self.request.host + sickbeard.WEB_ROOT
-
             results = yield self.async_call(method)
             self.finish(results)
 
