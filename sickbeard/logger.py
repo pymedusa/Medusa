122c7a4bfbb6dc094f647c8526ea07ddb0efb4a5
Rato <rato.aq2@gmail.com>
2016-08-21 18:59:44 +0200

    Fixing missing docstring

--- sickbeard/logger.py
+++ sickbeard/logger.py
@@ -361,6 +361,7 @@ class LogLine(object):
 
     @property
     def issue_title(self):
+        """Return the expected issue title for this logline."""
         result = self.traceback_lines[-1] if self.traceback_lines else self.message
         return result[:1000]
 
