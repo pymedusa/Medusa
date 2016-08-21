4c5cc243f756fa8d82fdc7664bc1e8a3b02cfbda
Rato <ratoaq2@users.noreply.github.com>
2016-08-21 16:46:07 +0200

    Adding S00EP00 pattern to guessit (#898)

--- sickbeard/name_parser/rules/__init__.py
+++ sickbeard/name_parser/rules/__init__.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 """Guessit customization."""
 from guessit.api import default_api
-from ...name_parser.rules.properties import (audio_channels, blacklist, format_, language, other, screen_size,
-                                             size, subtitle_language)
+from ...name_parser.rules.properties import (audio_channels, blacklist, episode, format_, language, other,
+                                             screen_size, size, subtitle_language)
 from ...name_parser.rules.rules import rules
 
 
+default_api.rebulk.rebulk(episode())
 default_api.rebulk.rebulk(blacklist())
 default_api.rebulk.rebulk(format_())
 default_api.rebulk.rebulk(screen_size())
