7f9d5ac813d6e6d76fb41ab82055cfed46d599e3
Fernando <fernandog@users.noreply.github.com>
2016-08-24 11:34:25 -0300

    Improve add-show-options.js

--- gui/slick/js/add-show-options.js
+++ gui/slick/js/add-show-options.js
@@ -1,40 +1,79 @@
 $(document).ready(function() {
-    $('#saveDefaultsButton').click(function () {
-        var anyQualArray = [];
-        var bestQualArray = [];
-        $('#allowed_qualities option:selected').each(function (i, d) {
-            anyQualArray.push($(d).val());
-        });
-        $('#preferred_qualities option:selected').each(function (i, d) {
-            bestQualArray.push($(d).val());
-        });
 
-        $.get('config/general/saveAddShowDefaults', {
-            defaultStatus: $('#statusSelect').val(),
-            anyQualities: anyQualArray.join(','),
-            bestQualities: bestQualArray.join(','),
-            defaultFlattenFolders: $('#flatten_folders').prop('checked'),
-            subtitles: $('#subtitles').prop('checked'),
-            anime: $('#anime').prop('checked'),
-            scene: $('#scene').prop('checked'),
-            defaultStatusAfter: $('#statusSelectAfter').val()
-        });
+   $('#saveDefaultsButton').on('click', function() {
 
-        $(this).attr('disabled', true);
-        new PNotify({
-            title: 'Saved Defaults',
-            text: 'Your "add show" defaults have been set to your current selections.',
-            shadow: false
-        });
-    });
+       var anyQualArray = [];
 
-    $('#statusSelect, #qualityPreset, #flatten_folders, #allowed_qualities, #preferred_qualities, #subtitles, #scene, #anime, #statusSelectAfter').change(function () {
-        $('#saveDefaultsButton').attr('disabled', false);
-    });
+       var bestQualArray = [];
 
-    $('#qualityPreset').on('change', function() {
-        // fix issue #181 - force re-render to correct the height of the outer div
-        $('span.prev').click();
-        $('span.next').click();
-    });
+       $('#anyQualities option:selected').each(function (i, d) {
+
+           anyQualArray.push($(d).val());
+
+       });
+
+       $('#bestQualities option:selected').each(function (i, d) {
+
+           bestQualArray.push($(d).val());
+
+       });
+
+
+
+       $.get('config/general/saveAddShowDefaults', {
+
+           defaultStatus: $('#statusSelect').val(),
+
+           anyQualities: anyQualArray.join(','),
+
+           bestQualities: bestQualArray.join(','),
+
+           defaultFlattenFolders: $('#flatten_folders').prop('checked'),
+
+           subtitles: $('#subtitles').prop('checked'),
+
+           anime: $('#anime').prop('checked'),
+
+           scene: $('#scene').prop('checked'),
+
+           defaultStatusAfter: $('#statusSelectAfter').val()
+
+       });
+
+
+
+       $(this).prop('disabled', true);
+
+       new PNotify({
+
+           title: 'Saved Defaults',
+
+           text: 'Your "add show" defaults have been set to your current selections.',
+
+           shadow: false
+
+       });
+
+   });
+
+
+
+   $('#statusSelect, #qualityPreset, #flatten_folders, #anyQualities, #bestQualities, #subtitles, #scene, #anime, #statusSelectAfter').on('change', function () {
+
+       $('#saveDefaultsButton').prop('disabled', false);
+
+   });
+
+
+
+   $('#qualityPreset').on('change', function() {
+
+       // fix issue #181 - force re-render to correct the height of the outer div
+
+       $('span.prev').click();
+
+       $('span.next').click();
+
+   });
+
 });
