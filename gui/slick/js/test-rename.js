0a09a76304a97a4e3aa9da14aaa10779657cbc6c
Fernando <fernandog@users.noreply.github.com>
2016-08-25 21:05:20 -0300

    fix test rename (#977)

--- gui/slick/js/test-rename.js
+++ gui/slick/js/test-rename.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    if (SICKRAGE.info.fanArtBackground) {
-        $.backstretch('showPoster/?show=' + $('#showID').attr('value') + '&which=fanart');
-        $('.backstretch').css('opacity', SICKRAGE.info.fanartBackgroundOpacity).fadeIn(500);
-    }
     $('.seriesCheck').on('click', function() {
         var serCheck = this;
 
