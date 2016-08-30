5e9d85fddf29d23cd26d083167b221a58a43896c
Dario <medariox@users.noreply.github.com>
2016-08-30 16:57:05 +0200

    Update torrentz2.py

--- sickbeard/providers/torrent/xml/torrentz2.py
+++ sickbeard/providers/torrent/xml/torrentz2.py
@@ -51,7 +51,7 @@ class Torrentz2Provider(TorrentProvider):  # pylint: disable=too-many-instance-a
         # Proper Strings
 
         # Miscellaneous Options
-        #self.confirmed = True
+        # self.confirmed = True
 
         # Torrent Stats
         self.minseed = None
@@ -79,7 +79,7 @@ class Torrentz2Provider(TorrentProvider):  # pylint: disable=too-many-instance-a
                     logger.log('Search string: {search}'.format
                                (search=search_string), logger.DEBUG)
 
-                #search_url = self.urls['verified'] if self.confirmed else self.urls['feed']
+                # search_url = self.urls['verified'] if self.confirmed else self.urls['feed']
                 search_url = self.urls['feed']
                 response = self.get_url(search_url, params={'f': search_string}, returns='response')
                 if not response or not response.text:
