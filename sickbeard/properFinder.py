8799610064bd7b1773d97426930f363a76b612f1
Dario <medariox@users.noreply.github.com>
2016-09-13 21:44:13 +0200

    Update properFinder.py

--- sickbeard/properFinder.py
+++ sickbeard/properFinder.py
@@ -23,7 +23,6 @@ from __future__ import unicode_literals
 import datetime
 import errno
 import operator
-import os
 import re
 import threading
 import time
@@ -36,7 +35,7 @@ from sickrage.helper.common import enabled_providers
 from sickrage.helper.exceptions import AuthException, ex
 from sickrage.show.History import History
 from . import db, helpers, logger
-from .common import DOWNLOADED, Quality, cpu_presets
+from .common import Quality, cpu_presets
 from .name_parser.parser import InvalidNameException, InvalidShowException, NameParser
 from .search import pickBestResult, snatchEpisode
 
@@ -96,15 +95,14 @@ class ProperFinder(object):  # pylint: disable=too-few-public-methods
         # Get the recently aired (last 2 days) shows from db
         search_date = datetime.datetime.today() - datetime.timedelta(days=2)
         main_db_con = db.DBConnection()
-        search_qualities = list(set(Quality.DOWNLOADED))
-        search_q_params = ','.join('?' for _ in search_qualities)
+        search_q_params = ','.join('?' for _ in Quality.DOWNLOADED)
         recently_aired = main_db_con.select(
             b'SELECT s.show_name, e.showid, e.season, e.episode, e.status, e.airdate'
             b' FROM tv_episodes AS e'
             b' INNER JOIN tv_shows AS s ON (e.showid = s.indexer_id)'
             b' WHERE e.airdate >= ?'
             b' AND e.status IN ({0})'.format(search_q_params),
-            [search_date.toordinal()] + search_qualities
+            [search_date.toordinal()] + Quality.DOWNLOADED
         )
 
         if not recently_aired:
@@ -238,21 +236,20 @@ class ProperFinder(object):  # pylint: disable=too-few-public-methods
                     self.processed_propers.append(cur_proper.name)
                     continue
 
-            # check if we actually want this proper (if it's the right quality)
+            # check if we have the episode as DOWNLOADED
             main_db_con = db.DBConnection()
-            sql_results = main_db_con.select(b'SELECT status, release_name FROM tv_episodes WHERE showid = ? AND season = ? AND episode = ?',
+            sql_results = main_db_con.select(b"SELECT status, release_name FROM tv_episodes WHERE "
+                                             b"showid = ? AND season = ? AND episode = ? AND status LIKE '%04'",
                                              [best_result.indexerid, best_result.season, best_result.episode])
             if not sql_results:
                 logger.log('Ignoring proper because we dont have this show and/or episode in library: {name}'.format
                            (name=best_result.name))
                 continue
 
-            # only keep the proper if we have already retrieved the same quality ep (don't get better/worse ones)
-            old_status, old_quality = Quality.splitCompositeStatus(int(sql_results[0][b'status']))
-            if old_status != DOWNLOADED or old_quality != best_result.quality:
-                logger.log('Ignoring proper because quality is different or episode is already '
-                           'archived or is still snatched (must wait download finishes): {name}'.format
-                           (name=best_result.name))
+            # only keep the proper if we have already downloaded an episode with the same quality
+            _, old_quality = Quality.splitCompositeStatus(int(sql_results[0][b'status']))
+            if old_quality != best_result.quality:
+                logger.log('Ignoring proper because quality is different: {name}'.format(name=best_result.name))
                 self.processed_propers.append(cur_proper.name)
                 continue
 
@@ -261,7 +258,7 @@ class ProperFinder(object):  # pylint: disable=too-few-public-methods
             if release_name:
                 current_codec = NameParser()._parse_string(release_name).video_codec
                 # Ignore proper if codec differs from downloaded release codec
-                if current_codec and parse_result.video_codec and parse_result.video_codec != current_codec:
+                if all([current_codec, parse_result.video_codec, parse_result.video_codec != current_codec]):
                     logger.log('Ignoring proper because codec is different: {name}'.format(name=best_result.name))
                     self.processed_propers.append(cur_proper.name)
                     continue
@@ -312,7 +309,6 @@ class ProperFinder(object):  # pylint: disable=too-few-public-methods
 
             history_limit = datetime.datetime.today() - datetime.timedelta(days=30)
 
-            # make sure the episode has been downloaded before
             main_db_con = db.DBConnection()
             history_results = main_db_con.select(
                 b'SELECT resource FROM history '
@@ -321,7 +317,7 @@ class ProperFinder(object):  # pylint: disable=too-few-public-methods
                 b'AND episode = ? '
                 b'AND quality = ? '
                 b'AND date >= ? '
-                b"AND action LIKE '%04'",
+                b"AND (action LIKE '%02' OR action LIKE '%04')",
                 [cur_proper.indexerid, cur_proper.season, cur_proper.episode, cur_proper.quality,
                  history_limit.strftime(History.date_format)])
 
