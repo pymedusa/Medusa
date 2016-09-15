34b93ad62940fb089529989310e7462b8c6e6c9f
Dario <medariox@users.noreply.github.com>
2016-09-15 15:24:15 +0200

    Make try..exception less broad

--- sickbeard/tv.py
+++ sickbeard/tv.py
@@ -1091,36 +1091,39 @@ class TVShow(TVObject):
             # Remove first two chars from ID
             imdb_obj = imdb_api.get_movie(imdb_id[2:])
 
-            self.imdb_info = {
-                'imdb_id': imdb_id,
-                'title': imdb_obj.get('title', ''),
-                'year': imdb_obj.get('year', ''),
-                'akas': '|'.join(imdb_obj.get('akas', '')),
-                'genres': '|'.join(imdb_obj.get('genres', '')),
-                'countries': '|'.join(imdb_obj.get('countries', '')),
-                'country_codes': '|'.join(imdb_obj.get('country codes', '')),
-                'rating': imdb_obj.get('rating', ''),
-                'votes': imdb_obj.get('votes', ''),
-                'last_update': datetime.date.today().toordinal()
-            }
-
-            if imdb_obj.get('runtimes'):
-                self.imdb_info['runtimes'] = re.search(r'\d+', imdb_obj['runtimes'][0]).group(0)
-
-            # Get only the production country certificate if any
-            if imdb_obj.get('certificates') and imdb_obj.get('countries'):
-                for certificate in imdb_obj['certificates']:
-                    if certificate.split(':')[0] in imdb_obj['countries']:
-                        self.imdb_info['certificates'] = certificate.split(':')[1]
-                        break
         except IMDbDataAccessError:
-            logger.log(u'{0}: Failed to obtain info from IMDb: {1}'.format(
-                       self.indexerid, self.imdb_info), logger.DEBUG)
+            logger.log(u'{0}: Failed to obtain info from IMDb for: {1}'.format(
+                       self.indexerid, self.name), logger.DEBUG)
             return
+
         except IMDbParserError:
-            logger.log(u'{0}: Failed to parse info from IMDb: {1}'.format(
-                       self.indexerid, self.imdb_info), logger.ERROR)
+            logger.log(u'{0}: Failed to parse info from IMDb for: {1}'.format(
+                       self.indexerid, self.name), logger.ERROR)
             return
+
+        self.imdb_info = {
+            'imdb_id': imdb_id,
+            'title': imdb_obj.get('title', ''),
+            'year': imdb_obj.get('year', ''),
+            'akas': '|'.join(imdb_obj.get('akas', '')),
+            'genres': '|'.join(imdb_obj.get('genres', '')),
+            'countries': '|'.join(imdb_obj.get('countries', '')),
+            'country_codes': '|'.join(imdb_obj.get('country codes', '')),
+            'rating': imdb_obj.get('rating', ''),
+            'votes': imdb_obj.get('votes', ''),
+            'last_update': datetime.date.today().toordinal()
+        }
+
+        if imdb_obj.get('runtimes'):
+            self.imdb_info['runtimes'] = re.search(r'\d+', imdb_obj['runtimes'][0]).group(0)
+
+        # Get only the production country certificate if any
+        if imdb_obj.get('certificates') and imdb_obj.get('countries'):
+            for certificate in imdb_obj['certificates']:
+                if certificate.split(':')[0] in imdb_obj['countries']:
+                    self.imdb_info['certificates'] = certificate.split(':')[1]
+                    break
+
         logger.log(u'{0}: Obtained info from IMDb: {1}'.format(
                    self.indexerid, self.imdb_info), logger.DEBUG)
 
