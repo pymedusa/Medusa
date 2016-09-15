a5f529c54da2dcf456e96fc00a7618d8c803eaae
Fernando <fernandog@users.noreply.github.com>
2016-09-15 09:33:12 -0300

    Fix pymedusa/SickRage/issues/1113

--- sickbeard/tv.py
+++ sickbeard/tv.py
@@ -28,6 +28,7 @@ import threading
 import traceback
 
 from imdb import imdb
+from imdb._exceptions import IMDbDataAccessError, IMDbParserError
 import shutil_custom
 import sickbeard
 from sickrage.helper.common import (
@@ -1071,46 +1072,55 @@ class TVShow(TVObject):
     def load_imdb_info(self):
         """Load all required show information from IMDb with IMDbPY."""
         imdb_api = imdb.IMDb()
-        if not self.imdbid:
-            self.imdbid = imdb_api.title2imdbID(self.name, kind='tv series')
 
-        if not self.imdbid:
-            logger.log(u'{0}: Not loading show info from IMDb, '
-                       u"because we don't know its ID".format(self.indexerid))
+        try:
+            if not self.imdbid:
+                self.imdbid = imdb_api.title2imdbID(self.name, kind='tv series')
+    
+            if not self.imdbid:
+                logger.log(u'{0}: Not loading show info from IMDb, '
+                           u"because we don't know its ID".format(self.indexerid))
+                return
+    
+            # Make sure we only use one ID
+            imdb_id = self.imdbid.split(',')[0]
+    
+            logger.log(u'{0}: Loading show info from IMDb with ID: {1}'.format(
+                       self.indexerid, imdb_id), logger.DEBUG)
+    
+            # Remove first two chars from ID
+            imdb_obj = imdb_api.get_movie(imdb_id[2:])
+    
+            self.imdb_info = {
+                'imdb_id': imdb_id,
+                'title': imdb_obj.get('title', ''),
+                'year': imdb_obj.get('year', ''),
+                'akas': '|'.join(imdb_obj.get('akas', '')),
+                'genres': '|'.join(imdb_obj.get('genres', '')),
+                'countries': '|'.join(imdb_obj.get('countries', '')),
+                'country_codes': '|'.join(imdb_obj.get('country codes', '')),
+                'rating': imdb_obj.get('rating', ''),
+                'votes': imdb_obj.get('votes', ''),
+                'last_update': datetime.date.today().toordinal()
+            }
+    
+            if imdb_obj.get('runtimes'):
+                self.imdb_info['runtimes'] = re.search(r'\d+', imdb_obj['runtimes'][0]).group(0)
+    
+            # Get only the production country certificate if any
+            if imdb_obj.get('certificates') and imdb_obj.get('countries'):
+                for certificate in imdb_obj['certificates']:
+                    if certificate.split(':')[0] in imdb_obj['countries']:
+                        self.imdb_info['certificates'] = certificate.split(':')[1]
+                        break
+        except IMDbDataAccessError:
+            logger.log(u'{0}: Failed to obtain info from IMDb: {1}'.format(
+                       self.indexerid, self.imdb_info), logger.DEBUG)
             return
-
-        # Make sure we only use one ID
-        imdb_id = self.imdbid.split(',')[0]
-
-        logger.log(u'{0}: Loading show info from IMDb with ID: {1}'.format(
-                   self.indexerid, imdb_id), logger.DEBUG)
-
-        # Remove first two chars from ID
-        imdb_obj = imdb_api.get_movie(imdb_id[2:])
-
-        self.imdb_info = {
-            'imdb_id': imdb_id,
-            'title': imdb_obj.get('title', ''),
-            'year': imdb_obj.get('year', ''),
-            'akas': '|'.join(imdb_obj.get('akas', '')),
-            'genres': '|'.join(imdb_obj.get('genres', '')),
-            'countries': '|'.join(imdb_obj.get('countries', '')),
-            'country_codes': '|'.join(imdb_obj.get('country codes', '')),
-            'rating': imdb_obj.get('rating', ''),
-            'votes': imdb_obj.get('votes', ''),
-            'last_update': datetime.date.today().toordinal()
-        }
-
-        if imdb_obj.get('runtimes'):
-            self.imdb_info['runtimes'] = re.search(r'\d+', imdb_obj['runtimes'][0]).group(0)
-
-        # Get only the production country certificate if any
-        if imdb_obj.get('certificates') and imdb_obj.get('countries'):
-            for certificate in imdb_obj['certificates']:
-                if certificate.split(':')[0] in imdb_obj['countries']:
-                    self.imdb_info['certificates'] = certificate.split(':')[1]
-                    break
-
+        except IMDbParserError:
+            logger.log(u'{0}: Failed to parse info from IMDb: {1}'.format(
+                       self.indexerid, self.imdb_info), logger.ERROR)
+            return
         logger.log(u'{0}: Obtained info from IMDb: {1}'.format(
                    self.indexerid, self.imdb_info), logger.DEBUG)
 
