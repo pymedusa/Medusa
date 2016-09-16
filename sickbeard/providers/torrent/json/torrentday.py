e7f94df1bc4ec39f642426297293912a4d53ff26
Fernando <fernandog@users.noreply.github.com>
2016-09-16 20:08:47 -0300

    Fix TorrentDay login (#1133)

--- sickbeard/providers/torrent/json/torrentday.py
+++ sickbeard/providers/torrent/json/torrentday.py
@@ -185,29 +185,29 @@ class TorrentDayProvider(TorrentProvider):  # pylint: disable=too-many-instance-
             logger.log('Failed to login, you must add your cookies in the provider settings', logger.WARNING)
             return False
 
-            login_params = {
-                'username': self.username,
-                'password': self.password,
-                'submit.x': 0,
-                'submit.y': 0,
-            }
+        login_params = {
+            'username': self.username,
+            'password': self.password,
+            'submit.x': 0,
+            'submit.y': 0,
+        }
 
-            response = self.get_url(self.urls['login'], post_data=login_params, returns='response')
-            if not (response.content and response.status_code == 200):
-                logger.log('Unable to connect to provider', logger.WARNING)
-                return False
+        response = self.get_url(self.urls['login'], post_data=login_params, returns='response')
+        if not (response.content and response.status_code == 200):
+            logger.log('Unable to connect to provider', logger.WARNING)
+            return False
 
-            if re.search('You tried too often', response.text):
-                logger.log('Too many login access attempts', logger.WARNING)
-                return False
+        if re.search('You tried too often', response.text):
+            logger.log('Too many login access attempts', logger.WARNING)
+            return False
 
-            if (dict_from_cookiejar(self.session.cookies).get('uid') and
-                    dict_from_cookiejar(self.session.cookies).get('uid') in response.text):
-                    return True
-            else:
-                logger.log('Failed to login, check your cookies', logger.WARNING)
-                self.session.cookies.clear()
-                return False
+        if (dict_from_cookiejar(self.session.cookies).get('uid') and
+                dict_from_cookiejar(self.session.cookies).get('uid') in response.text):
+                return True
+        else:
+            logger.log('Failed to login, check your cookies', logger.WARNING)
+            self.session.cookies.clear()
+            return False
 
 
 provider = TorrentDayProvider()
