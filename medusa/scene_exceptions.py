Thraxis/pymedusa

sickbeard.scene_numbering.xem_refresh() was ran for every processed episode. Moved this, so it's only run once, after all episodes have been loaded from indexer.
* Implemented the mapped_indexer_id in multiple functions, making it available for indexers that need to use a mapped indexer_id for scene numbering/exceptions.

sickbeard/scene_exceptions.py
@@ -76,11 +76,12 @@ def get_scene_exceptions(indexer_id, season=-1):
     """
 
     exceptionsList = []
+    mapped_indexer_id = helpers.get_mapped_indexer_id(indexer_id)
 
     if indexer_id not in exceptionsCache or season not in exceptionsCache[indexer_id]:
         cache_db_con = db.DBConnection('cache.db')
         exceptions = cache_db_con.select("SELECT show_name FROM scene_exceptions WHERE indexer_id = ? and season = ?",
-                                         [indexer_id, season])
+                                         [mapped_indexer_id or indexer_id, season])
         if exceptions:
             exceptionsList = list({cur_exception["show_name"] for cur_exception in exceptions})
 
@@ -105,9 +106,10 @@ def get_all_scene_exceptions(indexer_id):
     :return: dict of exceptions
     """
     exceptionsDict = {}
+    mapped_indexer_id = helpers.get_mapped_indexer_id(indexer_id)
 
     cache_db_con = db.DBConnection('cache.db')
-    exceptions = cache_db_con.select("SELECT show_name,season FROM scene_exceptions WHERE indexer_id = ?", [indexer_id])
+    exceptions = cache_db_con.select("SELECT show_name,season FROM scene_exceptions WHERE indexer_id = ?", [mapped_indexer_id or indexer_id])
 
     if exceptions:
         for cur_exception in exceptions:
@@ -123,20 +125,21 @@ def get_scene_seasons(indexer_id):
     return a list of season numbers that have scene exceptions
     """
     exceptionsSeasonList = []
+    mapped_indexer_id = helpers.get_mapped_indexer_id(indexer_id)
 
     if indexer_id not in exceptionsSeasonCache:
         cache_db_con = db.DBConnection('cache.db')
         sql_results = cache_db_con.select("SELECT DISTINCT(season) as season FROM scene_exceptions WHERE indexer_id = ?",
-                                          [indexer_id])
+                                          [mapped_indexer_id or indexer_id])
         if sql_results:
             exceptionsSeasonList = list({int(x["season"]) for x in sql_results})
 
-            if indexer_id not in exceptionsSeasonCache:
-                exceptionsSeasonCache[indexer_id] = {}
+            if mapped_indexer_id or indexer_id not in exceptionsSeasonCache:
+                exceptionsSeasonCache[mapped_indexer_id or indexer_id] = {}
 
-            exceptionsSeasonCache[indexer_id] = exceptionsSeasonList
+            exceptionsSeasonCache[mapped_indexer_id or indexer_id] = exceptionsSeasonList
     else:
-        exceptionsSeasonList = exceptionsSeasonCache[indexer_id]
+        exceptionsSeasonList = exceptionsSeasonCache[mapped_indexer_id or indexer_id]
 
     return exceptionsSeasonList
 
@@ -296,6 +299,7 @@ def _anidb_exceptions_fetcher():
     return anidb_exception_dict
 
 xem_session = helpers.make_session()
+
 
 def _xem_exceptions_fetcher():
     if shouldRefresh('xem'):

sickbeard/scene_numbering.py
@@ -28,6 +28,7 @@ import datetime
 import traceback
 
 import sickbeard
+import helpers
 from sickbeard import logger
 from sickbeard import db
 from sickrage.helper.exceptions import ex
@@ -455,7 +456,7 @@ def get_xem_absolute_numbering_for_show(indexer_id, indexer):
     return result
 
 
-def xem_refresh(indexer_id, indexer, force=False):
+def xem_refresh(indexer_id, indexer, mapped_indexer_id=None, force=False):
     """
     Refresh data from xem for a tv show
 
@@ -466,6 +467,12 @@ def xem_refresh(indexer_id, indexer, force=False):
 
     indexer_id = int(indexer_id)
     indexer = int(indexer)
+    if mapped_indexer_id:
+        mapped_indexer_id = int(mapped_indexer_id)
+        logger.log(
+            u'Looking up XEM scene mapping, using a mapped indexer_id from {0} -> {1} for show {2}'.
+            format(indexer_id, mapped_indexer_id, sickbeard.indexerApi(indexer).name),
+            logger.DEBUG)
 
     MAX_REFRESH_AGE_SECS = 86400  # 1 day
 
@@ -495,11 +502,12 @@ def xem_refresh(indexer_id, indexer, force=False):
             # XEM MAP URL
             url = "http://thexem.de/map/havemap?origin=%s" % sickbeard.indexerApi(indexer).config['xem_origin']
             parsedJSON = sickbeard.helpers.getURL(url, session=xem_session, returns='json')
-            if not parsedJSON or 'result' not in parsedJSON or 'success' not in parsedJSON['result'] or 'data' not in parsedJSON or str(indexer_id) not in parsedJSON['data']:
+            if (not parsedJSON or 'result' not in parsedJSON or 'success' not in parsedJSON['result'] or
+                    'data' not in parsedJSON or str(mapped_indexer_id or indexer_id) not in parsedJSON['data']):
                 return
 
             # XEM API URL
-            url = "http://thexem.de/map/all?id=%s&origin=%s&destination=scene" % (indexer_id, sickbeard.indexerApi(indexer).config['xem_origin'])
+            url = "http://thexem.de/map/all?id=%s&origin=%s&destination=scene" % (mapped_indexer_id or indexer_id, sickbeard.indexerApi(indexer).config['xem_origin'])
 
             parsedJSON = sickbeard.helpers.getURL(url, session=xem_session, returns='json')
             if not parsedJSON or 'result' not in parsedJSON or 'success' not in parsedJSON['result']:

pymedusa/Medusa

medusa/scene_exceptions.py
@@ -70,13 +70,17 @@ def set_last_refresh(ex_list):
 
 
 def get_scene_exceptions(indexer_id, season=-1):
-    """Given a indexer_id, return a list of all the scene exceptions."""
+    """
+    Given a indexer_id, return a list of all the scene exceptions.
+    """
+
     exceptions_list = []
+    mapped_indexer_id = helpers.get_mapped_indexer_id(indexer_id)
 
     if indexer_id not in exceptionsCache or season not in exceptionsCache[indexer_id]:
         cache_db_con = db.DBConnection('cache.db')
-        exceptions = cache_db_con.select(b'SELECT show_name FROM scene_exceptions WHERE indexer_id = ? AND season = ?',
-                                         [indexer_id, season])
+        exceptions = cache_db_con.select(b'SELECT show_name FROM scene_exceptions WHERE indexer_id = ? and season = ?',
+                                         [mapped_indexer_id or indexer_id, season])
         if exceptions:
             exceptions_list = list({cur_exception[b'show_name'] for cur_exception in exceptions})
 
@@ -101,10 +105,11 @@ def get_all_scene_exceptions(indexer_id):
     :return: dict of exceptions
     """
     exceptions_dict = {}
+    mapped_indexer_id = helpers.get_mapped_indexer_id(indexer_id)
 
     cache_db_con = db.DBConnection('cache.db')
-    exceptions = cache_db_con.select(b'SELECT show_name, season FROM scene_exceptions WHERE indexer_id = ?',
-                                     [indexer_id])
+    exceptions = cache_db_con.select(b'SELECT show_name,season FROM scene_exceptions WHERE indexer_id = ?', [mapped_indexer_id or indexer_id])
+
     if exceptions:
         for cur_exception in exceptions:
             if not cur_exception[b'season'] in exceptions_dict:
@@ -115,22 +120,25 @@ def get_all_scene_exceptions(indexer_id):
 
 
 def get_scene_seasons(indexer_id):
-    """Return a list of season numbers that have scene exceptions."""
+    """
+    return a list of season numbers that have scene exceptions
+    """
     exceptions_season_list = []
+    mapped_indexer_id = helpers.get_mapped_indexer_id(indexer_id)
 
     if indexer_id not in exceptionsSeasonCache:
         cache_db_con = db.DBConnection('cache.db')
-        sql_results = cache_db_con.select(
-            b'SELECT DISTINCT(season) AS season FROM scene_exceptions WHERE indexer_id = ?', [indexer_id])
+        sql_results = cache_db_con.select(b'SELECT DISTINCT(season) AS season FROM scene_exceptions WHERE indexer_id = ?',
+                                          [mapped_indexer_id or indexer_id])
         if sql_results:
             exceptions_season_list = list({int(x[b'season']) for x in sql_results})
 
-            if indexer_id not in exceptionsSeasonCache:
-                exceptionsSeasonCache[indexer_id] = {}
+            if mapped_indexer_id or indexer_id not in exceptionsSeasonCache:
+                exceptionsSeasonCache[mapped_indexer_id or indexer_id] = {}
 
-            exceptionsSeasonCache[indexer_id] = exceptions_season_list
+            exceptionsSeasonCache[mapped_indexer_id or indexer_id] = exceptions_season_list
     else:
-        exceptions_season_list = exceptionsSeasonCache[indexer_id]
+        exceptions_season_list = exceptionsSeasonCache[mapped_indexer_id or indexer_id]
 
     return exceptions_season_list
 
@@ -282,8 +290,6 @@ def _get_custom_exceptions():
                 continue
 
             set_last_refresh(app.indexerApi(indexer).name)
-
-    return custom_exceptions
 
 
 def _get_xem_exceptions():

medusa/scene_numbering.py
@@ -453,7 +453,7 @@ def get_xem_absolute_numbering_for_show(indexer_id, indexer):
     return result
 
 
-def xem_refresh(indexer_id, indexer, force=False):
+def xem_refresh(indexer_id, indexer, mapped_indexer_id=None, force=False):
     """
     Refresh data from xem for a tv show
 
@@ -464,6 +464,12 @@ def xem_refresh(indexer_id, indexer, force=False):
 
     indexer_id = int(indexer_id)
     indexer = int(indexer)
+    if mapped_indexer_id:
+        mapped_indexer_id = int(mapped_indexer_id)
+        logger.log(
+            u'Looking up XEM scene mapping, using a mapped indexer_id from {0} -> {1} for show {2}'.
+            format(indexer_id, mapped_indexer_id, app.indexerApi(indexer).name),
+            logger.DEBUG)
 
     MAX_REFRESH_AGE_SECS = 86400  # 1 day
 
@@ -493,11 +499,12 @@ def xem_refresh(indexer_id, indexer, force=False):
             # XEM MAP URL
             url = "http://thexem.de/map/havemap?origin=%s" % app.indexerApi(indexer).config['xem_origin']
             parsedJSON = app.helpers.getURL(url, session=xem_session, returns='json')
-            if not parsedJSON or 'result' not in parsedJSON or 'success' not in parsedJSON['result'] or 'data' not in parsedJSON or str(indexer_id) not in parsedJSON['data']:
+            if (not parsedJSON or 'result' not in parsedJSON or 'success' not in parsedJSON['result'] or
+                    'data' not in parsedJSON or str(mapped_indexer_id or indexer_id) not in parsedJSON['data']):
                 return
 
             # XEM API URL
-            url = "http://thexem.de/map/all?id=%s&origin=%s&destination=scene" % (indexer_id, app.indexerApi(indexer).config['xem_origin'])
+            url = "http://thexem.de/map/all?id=%s&origin=%s&destination=scene" % (mapped_indexer_id or indexer_id, app.indexerApi(indexer).config['xem_origin'])
 
             parsedJSON = app.helpers.getURL(url, session=xem_session, returns='json')
             if not parsedJSON or 'result' not in parsedJSON or 'success' not in parsedJSON['result']: