Thraxis/pymedusa

Added attributes for mappable indexer id's to the TVShow class. For example. If an indexer has the thetvdb id. It will try to map it to TVShow.tvdbid.
Always try to map the indexer to the show to other indexers. Don't wait for an image cache update to trigger it.

sickbeard/tv.py
@@ -864,7 +864,13 @@ class TVShow(object):  # pylint: disable=too-many-instance-attributes, too-many-
         self.network = getattr(myEp, 'network', '')
         self.runtime = getattr(myEp, 'runtime', '')
 
+        # New indexers2indexers mappable attributes need to be added here
+        # The indexer_config setting mapped_to, should correspond to the attribute.
+        # Like mapped_to = 'tvdbid' -> self.tvdbid
         self.imdbid = getattr(myEp, 'imdb_id', '')
+        self.tvdbid = getattr(myEp, 'tvdb_id', '')
+        self.tvrageid = getattr(myEp, 'tvrage_id', '')
+        self.tvmazeid = getattr(myEp, 'tvmaze_id', '')
 
         if getattr(myEp, 'airs_dayofweek', None) is not None and getattr(myEp, 'airs_time', None) is not None:
             self.airs = myEp["airs_dayofweek"] + " " + myEp["airs_time"]

pymedusa/Medusa

medusa/tv.py
@@ -151,6 +151,9 @@ class TVShow(TVObject):
         super(TVShow, self).__init__(indexer, indexerid, {'episodes', 'nextaired', 'release_groups'})
         self.name = ''
         self.imdbid = ''
+        self.tvdbid = ''
+        self.tvrageid = ''
+        self.tvmazeid = ''
         self.network = ''
         self.genre = ''
         self.classification = ''
@@ -1055,7 +1058,13 @@ class TVShow(TVObject):
         self.network = getattr(my_ep, 'network', '')
         self.runtime = getattr(my_ep, 'runtime', '')
 
+        # New indexers2indexers mappable attributes need to be added here
+        # The indexer_config setting mapped_to, should correspond to the attribute.
+        # Like mapped_to = 'tvdbid' -> self.tvdbid
         self.imdbid = getattr(my_ep, 'imdb_id', '')
+        self.tvdbid = getattr(my_ep, 'tvdb_id', '')
+        self.tvrageid = getattr(my_ep, 'tvrage_id', '')
+        self.tvmazeid = getattr(my_ep, 'tvmaze_id', '')
 
         if getattr(my_ep, 'airs_dayofweek', None) is not None and getattr(my_ep, 'airs_time', None) is not None:
             self.airs = my_ep['airs_dayofweek'] + ' ' + my_ep['airs_time']