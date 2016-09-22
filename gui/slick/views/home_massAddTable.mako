bitzorro/SickRage

Rewritten addExistingShows, to make use of json and html data attributes. A POST is now used instead of the get with crappy csv.

gui/slick/views/home_massAddTable.mako
@@ -12,21 +12,23 @@
             continue
 
         show_id = curDir['dir']
-        if curDir['existing_info'][0]:
-            show_id = show_id + '|' + str(curDir['existing_info'][0]) + '|' + str(curDir['existing_info'][1])
-            indexer = curDir['existing_info'][2]
+        if curDir['existing_info']['indexer_id']:
+            show_id = show_id + '|' + str(curDir['existing_info']['indexer_id']) + '|' + str(curDir['existing_info']['show_name'])
+            indexer = curDir['existing_info']['indexer']
 
         indexer = 0
-        if curDir['existing_info'][0]:
-            indexer = curDir['existing_info'][2]
+        if curDir['existing_info']['indexer_id']:
+            indexer = curDir['existing_info']['indexer']
         elif sickbeard.INDEXER_DEFAULT > 0:
             indexer = sickbeard.INDEXER_DEFAULT
     %>
     <tr>
-        <td class="col-checkbox"><input type="checkbox" id="${show_id}" class="dirCheck" checked=checked></td>
+        <td class="col-checkbox"><input type="checkbox" id="${show_id}" class="dirCheck" checked=checked
+        data-existing-indexer-id="${curDir['existing_info']['indexer_id']}" data-show-name="${curDir['existing_info']['show_name']}" 
+        data-existing-indexer="${curDir['existing_info']['indexer']}" data-show-dir="${curDir['dir']}" data-allready-added="${curDir['added_already']}"></td>
         <td><label for="${show_id}">${curDir['display_dir']}</label></td>
-        % if curDir['existing_info'][1] and indexer > 0:
-            <td><a href="${anon_url(sickbeard.indexerApi(indexer).config['show_url'], curDir['existing_info'][0])}">${curDir['existing_info'][1]}</a></td>
+        % if curDir['existing_info']['show_name'] and indexer > 0:
+            <td><a href="${anon_url(sickbeard.indexerApi(indexer).config['show_url'], curDir['existing_info']['indexer_id'])}">${curDir['existing_info']['show_name']}</a></td>
         % else:
             <td>?</td>
         % endif

pymedusa/Medusa

gui/slick/views/home_massAddTable.mako
@@ -17,20 +17,22 @@
         if curDir['added_already']:
             continue
         show_id = curDir['dir']
-        if curDir['existing_info'][0]:
-            show_id = show_id + '|' + str(curDir['existing_info'][0]) + '|' + str(curDir['existing_info'][1])
-            indexer = curDir['existing_info'][2]
+        if curDir['existing_info']['indexer_id']:
+            show_id = show_id + '|' + str(curDir['existing_info']['indexer_id']) + '|' + str(curDir['existing_info']['show_name'])
+            indexer = curDir['existing_info']['indexer']
         indexer = 0
-        if curDir['existing_info'][0]:
-            indexer = curDir['existing_info'][2]
+        if curDir['existing_info']['indexer_id']:
+            indexer = curDir['existing_info']['indexer']
         elif app.INDEXER_DEFAULT > 0:
             indexer = app.INDEXER_DEFAULT
     %>
     <tr>
-        <td class="col-checkbox"><input type="checkbox" id="${show_id}" class="dirCheck" checked=checked></td>
+        <td class="col-checkbox"><input type="checkbox" id="${show_id}" class="dirCheck" checked=checked
+        data-existing-indexer-id="${curDir['existing_info']['indexer_id']}" data-show-name="${curDir['existing_info']['show_name']}" 
+        data-existing-indexer="${curDir['existing_info']['indexer']}" data-show-dir="${curDir['dir']}" data-allready-added="${curDir['added_already']}"></td>
         <td><label for="${show_id}">${curDir['display_dir']}</label></td>
-        % if curDir['existing_info'][1] and indexer > 0:
-            <td><a href="${anon_url(app.indexerApi(indexer).config['show_url'], curDir['existing_info'][0])}">${curDir['existing_info'][1]}</a></td>
+        % if curDir['existing_info']['show_name'] and indexer > 0:
+            <td><a href="${anon_url(app.indexerApi(indexer).config['show_url'], curDir['existing_info']['indexer_id'])}">${curDir['existing_info']['show_name']}</a></td>
         % else:
             <td>?</td>
         % endif