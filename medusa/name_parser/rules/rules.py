cc66fd344489a520b9665cd1315835f492daef43
Rato <rato.aq2@gmail.com>
2016-10-12 19:18:04 +0200

    Fix guessit exception for multiple same titles

--- medusa/name_parser/rules/rules.py
+++ medusa/name_parser/rules/rules.py
@@ -1309,12 +1309,13 @@ class FixReleaseGroupGuessedAsTitle(Rule):
         if (titles and len(titles) > 1 and matches.tagged('anime') and
                 'equivalent' not in titles[-1].tags and 'expected' not in titles[-1].tags):
             wrong_title = matches.named('title', predicate=lambda m: m.value != titles[0].value, index=-1)
-            release_group = copy.copy(wrong_title)
-            release_group.name = 'release_group'
-            release_group.tags = []
+            if wrong_title:
+                release_group = copy.copy(wrong_title)
+                release_group.name = 'release_group'
+                release_group.tags = []
 
-            to_remove = matches.named('release_group', predicate=lambda match: match.span != release_group.span)
-            return to_remove, release_group
+                to_remove = matches.named('release_group', predicate=lambda match: match.span != release_group.span)
+                return to_remove, release_group
 
 
 class FixMultipleTitles(Rule):
