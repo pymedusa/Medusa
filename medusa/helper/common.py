6c22096c670108542d60ac7fb8878c8ef7304c31
Fernando <fernandog@users.noreply.github.com>
2016-10-18 11:34:51 -0200

    rato's code

--- medusa/helper/common.py
+++ medusa/helper/common.py
@@ -356,3 +356,24 @@ def remove_strings(old_string, unwanted_strings):
     for item in unwanted_strings:
         old_string = old_string.replace(item, '')
     return old_string
+
+def obj_to_dict(obj, classkey=None):
+    """Transform an object to dict."""
+    if isinstance(obj, dict):
+        data = {}
+        for (k, v) in obj.items():
+            data[k] = obj_to_dict(v, classkey)
+        return data
+    elif hasattr(obj, '_ast'):
+        return obj_to_dict(obj._ast())
+    elif hasattr(obj, '_iter__'):
+        return [obj_to_dict(v, classkey) for v in obj]
+    elif hasattr(obj, '__dict__'):
+        data = dict([(key, obj_to_dict(value, classkey))
+                     for key, value in obj.__dict__.items()
+                     if not callable(value) and not key.startswith('_')])
+        if classkey is not None and hasattr(obj, '__class__'):
+            data[classkey] = obj.__class__.__name__
+        return data
+    else:
+        return obj
