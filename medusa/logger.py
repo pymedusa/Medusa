3700da16b2b2d3e04ed30ead4095ea8e9a78d01f
Rato <rato.aq2@gmail.com>
2016-10-20 21:07:46 +0200

    Only decode if not unicode

--- medusa/logger.py
+++ medusa/logger.py
@@ -193,8 +193,10 @@ def reverse_readlines(filename, buf_size=2097152, encoding=default_encoding):
             offset = min(file_size, offset + buf_size)
             fh.seek(file_size - offset)
             buf = fh.read(min(remaining_size, buf_size))
+            if os.name == 'nt':
+                buf = buf.decode(sys.getfilesystemencoding())
             if not isinstance(buf, text_type):
-                buf = text_type(buf.decode(sys.getfilesystemencoding()) if os.name == 'nt' else buf, errors='replace')
+                buf = text_type(buf, errors='replace')
             remaining_size -= buf_size
             lines = buf.split(new_line)
             # the first line of the buffer is probably not a complete line so
