9c5956d77b20e6db7ba5a5c2a24de406ce808045
P0psicles <rogier@headshots.nl>
2016-10-23 12:32:04 +0200

    Removed airedOn method, as its already part of the base class.

--- lib/tvdb_api/tvdb_api.py
+++ lib/tvdb_api/tvdb_api.py
@@ -155,12 +155,6 @@ class Show(dict):
             # doesn't exist, so attribute error.
             raise tvdb_attributenotfound("Cannot find attribute %s" % (repr(key)))
 
-    def airedOn(self, date):
-        ret = self.search(str(date), 'firstaired')
-        if len(ret) == 0:
-            raise tvdb_episodenotfound("Could not find any episodes that aired on %s" % date)
-        return ret
-
     def search(self, term=None, key=None):
         """
         Search all episodes in show. Can search all data, or a specific key (for
