d922fa3464bcbe9d1682c2479f2529ad553ec760
supergonkas <matigonkas@outlook.com>
2016-10-27 12:42:35 +0100

    Update torrentbytes.py

--- medusa/providers/torrent/html/torrentbytes.py
+++ medusa/providers/torrent/html/torrentbytes.py
@@ -142,7 +142,7 @@ class TorrentBytesProvider(TorrentProvider):  # pylint: disable=too-many-instanc
                         freeleech = cells[labels.index('Name')].find('font', color='green')
                         if freeleech:
                             title = title[:-5] # Remove trailing "[F L]"                     
-                        if self.freeleech:
+                    if self.freeleech:
                         # Free leech torrents are marked with green [F L] in the title (i.e. <font color=green>[F&nbsp;L]</font>)
                         freeleech = cells[labels.index('Name')].find('font', color='green')
                         if not freeleech or freeleech.get_text(strip=True) != '[F\xa0L]':
