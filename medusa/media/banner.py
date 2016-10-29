ed5b4085d95c5fe792dfc26fb17ef727ae1a53f3
Labrys <Labrys.git@gmail.com>
2016-10-28 22:20:26 -0400

    Refactor GenericMedia.py -> generic.py

--- medusa/media/banner.py
+++ medusa/media/banner.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Medusa. If not, see <http://www.gnu.org/licenses/>.
 
-from .GenericMedia import GenericMedia
+from .generic import GenericMedia
 from ..image_cache import ImageCache
 
 
