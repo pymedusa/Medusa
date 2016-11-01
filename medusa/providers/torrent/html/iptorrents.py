0d00b41e6ae422aedcf0fcf0b2149c80b1ecd4e0
fernandog <fernandog@users.noreply.github.com>
2016-11-01 12:04:49 -0200

    Flake. No more issues.

--- medusa/providers/torrent/html/iptorrents.py
+++ medusa/providers/torrent/html/iptorrents.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Medusa. If not, see <http://www.gnu.org/licenses/>.
-
+"""Provider code for IPTorrents."""
 from __future__ import unicode_literals
 
 import re
