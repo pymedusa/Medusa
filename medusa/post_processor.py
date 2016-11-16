61b00f77388e29c16632f38a1437b644a0ce0b50
Dario <medariox@users.noreply.github.com>
2016-11-16 13:53:14 +0100

    Fix "ValueError: path is on drive F:, start on drive D:" for _get_rel_path() #1555

--- medusa/post_processor.py
+++ medusa/post_processor.py
@@ -106,11 +106,15 @@ class PostProcessor(object):
         :rtype: text_type
         """
         if app.TV_DOWNLOAD_DIR:
-            rel_path = os.path.relpath(self.file_path, app.TV_DOWNLOAD_DIR)
-            # check if we really found the relative path
-            if not rel_path.startswith('..'):
-                return rel_path
+            try:
+                rel_path = os.path.relpath(self.file_path, app.TV_DOWNLOAD_DIR)
+                # check if we really found the relative path
+                if not rel_path.startswith('..'):
+                    return rel_path
+            except ValueError:
+                pass
 
+        self._log(u"Couldn't get the relative path, using the parent folder instead", logger.DEBUG)
         parent_name = os.path.basename(os.path.dirname(self.file_path))
         # return self.file_path once this bug is fixed: goo.gl/U4XNoP
         return os.path.join(parent_name, self.file_name)
