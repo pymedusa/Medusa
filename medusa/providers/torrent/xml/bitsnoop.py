f16820fb6112c75dcc8f3458ed8d23e8dc177037
supergonkas <matigonkas@outlook.com>
2016-11-20 18:01:33 +0000

    Avoid url redirects

--- medusa/providers/torrent/xml/bitsnoop.py
+++ medusa/providers/torrent/xml/bitsnoop.py
@@ -39,7 +39,7 @@ class BitSnoopProvider(TorrentProvider):  # pylint: disable=too-many-instance-at
         self.public = True
 
         # URLs
-        self.url = 'http://bitsnoop.com'
+        self.url = 'https://bitsnoop.com'
         self.urls = {
             'base': self.url,
             'rss': urljoin(self.url, '/new_video.html?fmt=rss'),
