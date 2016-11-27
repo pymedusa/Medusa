cdad03093aa699ea2dac50bb81a93c82997b9dfa
supergonkas <matigonkas@outlook.com>
2016-11-27 15:33:48 +0000

    Fix https://github.com/pymedusa/Medusa/issues/1675 (#1684)
    * Fix https://github.com/pymedusa/Medusa/issues/1675
    
    * Fix #1675
    
    * Update limetorrents.py
    
    * Update limetorrents.py
    
    * Update limetorrents.py
    
    * Update limetorrents.py

--- medusa/providers/torrent/html/limetorrents.py
+++ medusa/providers/torrent/html/limetorrents.py
@@ -20,9 +20,12 @@ from __future__ import unicode_literals
 
 import re
 import traceback
+
 from contextlib2 import suppress
-import requests
+
 from requests.compat import urljoin
+from requests.exceptions import ConnectionError as RequestsConnectionError, Timeout
+
 from ..torrent_provider import TorrentProvider
 from .... import logger, tv_cache
 from ....bs4_parser import BS4Parser
@@ -43,12 +46,11 @@ class LimeTorrentsProvider(TorrentProvider):
         self.public = True
 
         # URLs
-        self.url = 'https://www.limetorrents.cc/'
+        self.url = 'https://www.limetorrents.cc'
         self.urls = {
-            'index': self.url,
             'update': urljoin(self.url, '/post/updatestats.php'),
             'search': urljoin(self.url, '/search/tv/{query}/'),
-            'rss': urljoin(self.url, '/browse-torrents/TV-shows/date/{page}/')
+            'rss': urljoin(self.url, '/browse-torrents/TV-shows/date/{page}/'),
         }
 
         # Proper Strings
@@ -110,52 +112,56 @@ class LimeTorrentsProvider(TorrentProvider):
         """
         items = []
 
-        with BS4Parser(data, 'html5lib') as html:
-            torrent_table = html('table', class_='table2')
+        def process_column_header(th):
+            return th.span.get_text() if th.span else th.get_text()
 
-            if mode != 'RSS' and torrent_table and len(torrent_table) < 2:
+        with BS4Parser(data, 'html5lib') as html:
+            torrent_table = html.find('table', class_='table2')
+
+            if not torrent_table:
                 logger.log('Data returned from provider does not contain any {0}torrents'.format(
                            'confirmed ' if self.confirmed else ''), logger.DEBUG)
                 return items
 
-            torrent_table = torrent_table[0 if mode == 'RSS' else 1]
-            torrent_rows = torrent_table('tr')
+            torrent_rows = torrent_table.find_all('tr')
+            labels = [process_column_header(label) for label in torrent_rows[0]]
 
             # Skip the first row, since it isn't a valid result
             for row in torrent_rows[1:]:
-                cells = row('td')
+                cells = row.find_all('td')
+
                 try:
-                    verified = row('img', title='Verified torrent')
+                    title_cell = cells[labels.index('Torrent Name')]
+
+                    verified = title_cell.find('img', title='Verified torrent')
                     if self.confirmed and not verified:
                         continue
 
-                    url = row.find('a', rel='nofollow')
-                    title_info = row('a')
-                    info = title_info[1]['href']
-                    if not all([url, title_info, info]):
+                    title_anchors = title_cell.find_all('a')
+                    if not title_anchors or len(title_anchors) < 2:
                         continue
 
-                    title = title_info[1].get_text(strip=True)
-                    title2 = id_regex.search(info).group(1)
-                    if len(title) < len(title2):
-                        title = title2.replace('-', ' ')
-                    torrent_id = id_regex.search(info).group(2)
-                    torrent_hash = hash_regex.search(url['href']).group(2)
+                    title_url = title_anchors[0].get('href')
+                    title = title_anchors[1].get_text(strip=True)
+                    regex_result = id_regex.search(title_anchors[1].get('href'))
+
+                    alt_title = regex_result.group(1)
+                    if len(title) < len(alt_title):
+                        title = alt_title.replace('-', ' ')
+
+                    torrent_id = regex_result.group(2)
+                    torrent_hash = hash_regex.search(title_url).group(2)
                     if not all([title, torrent_id, torrent_hash]):
                         continue
 
-                    with suppress(requests.exceptions.Timeout):
+                    with suppress(RequestsConnectionError, Timeout):
                         # Suppress the timeout since we are not interested in actually getting the results
-                        self.session.get(self.urls['update'], timeout=0.1,
-                                         params={'torrent_id': torrent_id,
-                                                 'infohash': torrent_hash})
+                        self.session.get(self.urls['update'], timeout=0.1, params={'torrent_id': torrent_id,
+                                                                                   'infohash': torrent_hash})
 
                     # Remove comma as thousands separator from larger number like 2,000 seeders = 2000
-                    seeders = try_int(cells[3].get_text(strip=True).replace(',', ''))
-                    leechers = try_int(cells[4].get_text(strip=True).replace(',', ''))
-                    size = convert_size(cells[2].get_text(strip=True)) or -1
-                    download_url = 'magnet:?xt=urn:btih:{hash}&dn={title}{trackers}'.format(
-                        hash=torrent_hash, title=title, trackers=self._custom_trackers)
+                    seeders = try_int(cells[labels.index('Seed')].get_text(strip=True).replace(',', ''), 1)
+                    leechers = try_int(cells[labels.index('Leech')].get_text(strip=True).replace(',', ''))
 
                     if seeders < min(self.minseed, 1):
                         if mode != 'RSS':
@@ -163,6 +169,10 @@ class LimeTorrentsProvider(TorrentProvider):
                                        "minimum seeders: {0}. Seeders: {1}".format
                                        (title, seeders), logger.DEBUG)
                         continue
+
+                    size = convert_size(cells[labels.index('Size')].get_text(strip=True)) or -1
+                    download_url = 'magnet:?xt=urn:btih:{hash}&dn={title}{trackers}'.format(
+                        hash=torrent_hash, title=title, trackers=self._custom_trackers)
 
                     item = {
                         'title': title,
