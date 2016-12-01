1344eaa2e784ba776ab0d684aef113a03dc57c3f
supergonkas <matigonkas@outlook.com>
2016-12-01 16:31:55 +0000

    Fix pymedusa/Medusa/issues/1701 (#1703)
    * Fix https://github.com/pymedusa/Medusa/issues/1701
    
    * Minor changes
    
    * Update
    
    * Update btn.py

--- medusa/providers/torrent/json/btn.py
+++ medusa/providers/torrent/json/btn.py
@@ -248,13 +248,12 @@ class BTNProvider(TorrentProvider):
 
     def _api_call(self, apikey, params=None, results_per_page=300, offset=0):
         """Call provider API."""
-        server = jsonrpclib.Server(self.urls['base_url'])
         parsed_json = {}
 
         try:
+            server = jsonrpclib.Server(self.urls['base_url'])
             parsed_json = server.getTorrents(apikey, params or {}, int(results_per_page), int(offset))
             time.sleep(cpu_presets[app.CPU_PRESET])
-
         except jsonrpclib.jsonrpc.ProtocolError as error:
             if error.message[1] == 'Invalid API Key':
                 logger.log('Incorrect authentication credentials.', logger.WARNING)
@@ -263,20 +262,10 @@ class BTNProvider(TorrentProvider):
             else:
                 logger.log('JSON-RPC protocol error while accessing provider. Error: {msg!r}'.format
                            (msg=error.message[1]), logger.ERROR)
-            return {}
 
-        except socket.timeout:
-            logger.log('Timeout while accessing provider', logger.WARNING)
-
-        except socket.error as error:
-            # Note that sometimes timeouts are thrown as socket errors
-            logger.log('Socket error while accessing provider. Error: {msg}'.format
-                       (msg=error[1]), logger.WARNING)
-
-        except Exception as error:
-            logger.log('Unknown error while accessing provider. Error: {msg}'.format
-                       (msg=error), logger.ERROR)
-
+        except (socket.error, socket.timeout, ValueError) as error:
+            logger.log('Error while accessing provider. Error: {msg}'.format
+                       (msg=error), logger.WARNING)
         return parsed_json
 
 
