eccc8f0cbc7a9549c11f9935032c74821463f30f
P0psicles <rogier@headshots.nl>
2017-01-08 13:21:48 +0100

    Fixed rarbg still making use of deprecated request=json funtionality. .json() is now part of the providers code.

--- medusa/providers/torrent/json/rarbg.py
+++ medusa/providers/torrent/json/rarbg.py
@@ -221,12 +221,15 @@ class RarbgProvider(TorrentProvider):
             'app_id': app.RARBG_APPID,
         }
 
-        response = self.get_url(self.urls['api'], params=login_params, returns='json')
+        response = self.get_url(self.urls['api'], params=login_params)
         if not response:
             logger.log('Unable to connect to provider', logger.WARNING)
             return False
 
-        self.token = response.get('token')
+        try:
+            self.token = response.json().get('token')
+        except ValueError:
+            self.token = None
         self.token_expires = datetime.datetime.now() + datetime.timedelta(minutes=14) if self.token else None
         return self.token is not None
 
