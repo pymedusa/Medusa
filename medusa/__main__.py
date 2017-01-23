FireBladeNooT/Medusa_1_6

Remove torrent from client when ratio reached

medusa/__main__.py
@@ -66,7 +66,7 @@ from six import text_type
 from . import (
     app, auto_post_processor, cache, db, event_queue, exception_handler,
     helpers, logger as app_logger, metadata, name_cache, naming, network_timezones, providers,
-    scheduler, show_queue, show_updater, subtitles, trakt_checker, version_checker
+    scheduler, show_queue, show_updater, subtitles, torrent_checker, trakt_checker, version_checker
 )
 from .common import SD, SKIPPED, WANTED
 from .config import (
@@ -549,6 +549,7 @@ class Application(object):
 
             app.DOWNLOAD_PROPERS = bool(check_setting_int(app.CFG, 'General', 'download_propers', 1))
             app.PROPERS_SEARCH_DAYS = max(2, min(8, check_setting_int(app.CFG, 'General', 'propers_search_days', 2)))
+            app.REMOVE_FROM_CLIENT = bool(check_setting_int(app.CFG, 'General', 'remove_from_client', 0))
             app.CHECK_PROPERS_INTERVAL = check_setting_str(app.CFG, 'General', 'check_propers_interval', 'daily',
                                                            valid_values=('15m', '45m', '90m', '4h', 'daily'))
             app.RANDOMIZE_PROVIDERS = bool(check_setting_int(app.CFG, 'General', 'randomize_providers', 0))
@@ -1075,6 +1076,12 @@ class Application(object):
             app.subtitlesFinderScheduler = scheduler.Scheduler(subtitles.SubtitlesFinder(), cycleTime=update_interval, threadName="FINDSUBTITLES",
                                                                run_delay=update_interval, silent=not app.USE_SUBTITLES)
 
+            update_interval = datetime.timedelta(minutes=5)
+            app.torrentCheckerScheduler = scheduler.Scheduler(torrent_checker.TorrentChecker(),
+                                                              cycleTime=datetime.timedelta(minutes=30),
+                                                              threadName="TORRENTCHECKER",
+                                                              run_delay=update_interval)
+
             app.__INITIALIZED__ = True
             return True
 
@@ -1196,6 +1203,10 @@ class Application(object):
                 app.traktCheckerScheduler.silent = True
             app.traktCheckerScheduler.start()
 
+            app.torrentCheckerScheduler.enable = True
+            app.torrentCheckerScheduler.silent = False
+            app.torrentCheckerScheduler.start()
+
             app.started = True
 
     @staticmethod
@@ -1220,6 +1231,7 @@ class Application(object):
                 app.traktCheckerScheduler,
                 app.properFinderScheduler,
                 app.subtitlesFinderScheduler,
+                app.torrentCheckerScheduler,
                 app.events
             ]
 
@@ -1321,6 +1333,7 @@ class Application(object):
         new_config['General']['showupdate_hour'] = int(app.SHOWUPDATE_HOUR)
         new_config['General']['download_propers'] = int(app.DOWNLOAD_PROPERS)
         new_config['General']['propers_search_days'] = int(app.PROPERS_SEARCH_DAYS)
+        new_config['General']['remove_from_client'] = int(app.REMOVE_FROM_CLIENT)
         new_config['General']['randomize_providers'] = int(app.RANDOMIZE_PROVIDERS)
         new_config['General']['check_propers_interval'] = app.CHECK_PROPERS_INTERVAL
         new_config['General']['allow_high_priority'] = int(app.ALLOW_HIGH_PRIORITY)

medusa/app.py
@@ -81,7 +81,7 @@ properFinderScheduler = None
 autoPostProcessorScheduler = None
 subtitlesFinderScheduler = None
 traktCheckerScheduler = None
-
+torrentCheckerScheduler = None
 
 showList = []
 
@@ -225,6 +225,7 @@ TORRENT_DIR = None
 DOWNLOAD_PROPERS = False
 CHECK_PROPERS_INTERVAL = None
 PROPERS_SEARCH_DAYS = 2
+REMOVE_FROM_CLIENT = False
 ALLOW_HIGH_PRIORITY = False
 SAB_FORCED = False
 RANDOMIZE_PROVIDERS = False

medusa/helpers.py
@@ -62,7 +62,7 @@ from requests.compat import urlparse
 from six import binary_type, string_types, text_type
 from six.moves import http_client
 
-from . import app
+from . import app, db
 from .common import USER_AGENT
 from .helper.common import episode_num, http_code_description, media_extensions, pretty_file_size, subtitle_extensions
 from .helper.exceptions import ex
@@ -897,6 +897,7 @@ def anon_url(*url):
 #   2) Update the last encryption_version available in server/web/config/general.py
 #   3) Remember to maintain old encryption versions and key generators for retro-compatibility
 
+
 # Key Generators
 unique_key1 = hex(uuid.getnode() ** 2)  # Used in encryption v1
 
@@ -1687,3 +1688,26 @@ def get_broken_providers():
 
     logger.info('Broken providers found: {0}'.format(response))
     return ','.join(response)
+
+
+def is_already_processed_media(full_filename):
+    """Check if resource was already processed."""
+    if not isMediaFile(str(full_filename)):
+        return False
+    main_db_con = db.DBConnection()
+    history_result = main_db_con.select('SELECT 1 FROM history '
+                                        "WHERE action LIKE '%04' "
+                                        'AND resource LIKE ?',
+                                        ['%' + full_filename])
+    if history_result:
+        return True
+
+
+def is_info_hash_in_history(info_hash):
+    """Check if torrent hash is in history."""
+    main_db_con = db.DBConnection()
+    history_result = main_db_con.select('SELECT 1 FROM history '
+                                        'WHERE info_hash=?',
+                                        [info_hash])
+    if history_result:
+        return True

views/status.mako
@@ -23,6 +23,7 @@
         'Post Process': 'autoPostProcessorScheduler',
         'Subtitles Finder': 'subtitlesFinderScheduler',
         'Trakt Checker': 'traktCheckerScheduler',
+        'Torrent Checker': 'torrentCheckerScheduler',
     }
 %>
 <div id="config-content">

pymedusa/Medusa

medusa/__main__.py
@@ -67,7 +67,7 @@ from six import text_type
 from . import (
     app, auto_post_processor, cache, db, event_queue, exception_handler,
     helpers, logger as app_logger, metadata, name_cache, naming, network_timezones, providers,
-    scheduler, show_queue, show_updater, subtitles, trakt_checker, version_checker
+    scheduler, show_queue, show_updater, subtitles, torrent_checker, trakt_checker, version_checker
 )
 from .common import SD, SKIPPED, WANTED
 from .config import (
@@ -551,6 +551,7 @@ class Application(object):
 
             app.DOWNLOAD_PROPERS = bool(check_setting_int(app.CFG, 'General', 'download_propers', 1))
             app.PROPERS_SEARCH_DAYS = max(2, min(8, check_setting_int(app.CFG, 'General', 'propers_search_days', 2)))
+            app.REMOVE_FROM_CLIENT = bool(check_setting_int(app.CFG, 'General', 'remove_from_client', 0))
             app.CHECK_PROPERS_INTERVAL = check_setting_str(app.CFG, 'General', 'check_propers_interval', 'daily',
                                                            valid_values=('15m', '45m', '90m', '4h', 'daily'))
             app.RANDOMIZE_PROVIDERS = bool(check_setting_int(app.CFG, 'General', 'randomize_providers', 0))
@@ -1106,6 +1107,12 @@ class Application(object):
                                                                  run_delay=update_interval,
                                                                  silent=not app.USE_SUBTITLES)
 
+            update_interval = datetime.timedelta(minutes=5)
+            app.torrent_checker_scheduler = scheduler.Scheduler(torrent_checker.TorrentChecker(),
+                                                                cycleTime=datetime.timedelta(minutes=30),
+                                                                threadName="TORRENTCHECKER",
+                                                                run_delay=update_interval)
+
             app.__INITIALIZED__ = True
             return True
 
@@ -1227,6 +1234,10 @@ class Application(object):
                 app.trakt_checker_scheduler.silent = True
             app.trakt_checker_scheduler.start()
 
+            app.torrent_checker_scheduler.enable = True
+            app.torrent_checker_scheduler.silent = False
+            app.torrent_checker_scheduler.start()
+
             app.started = True
 
     @staticmethod
@@ -1251,6 +1262,7 @@ class Application(object):
                 app.trakt_checker_scheduler,
                 app.proper_finder_scheduler,
                 app.subtitles_finder_scheduler,
+                app.torrent_checker_scheduler,
                 app.events
             ]
 
@@ -1352,6 +1364,7 @@ class Application(object):
         new_config['General']['showupdate_hour'] = int(app.SHOWUPDATE_HOUR)
         new_config['General']['download_propers'] = int(app.DOWNLOAD_PROPERS)
         new_config['General']['propers_search_days'] = int(app.PROPERS_SEARCH_DAYS)
+        new_config['General']['remove_from_client'] = int(app.REMOVE_FROM_CLIENT)
         new_config['General']['randomize_providers'] = int(app.RANDOMIZE_PROVIDERS)
         new_config['General']['check_propers_interval'] = app.CHECK_PROPERS_INTERVAL
         new_config['General']['allow_high_priority'] = int(app.ALLOW_HIGH_PRIORITY)

medusa/app.py
@@ -82,7 +82,7 @@ proper_finder_scheduler = None
 auto_post_processor_scheduler = None
 subtitles_finder_scheduler = None
 trakt_checker_scheduler = None
-
+torrent_checker_scheduler = None
 
 showList = []
 
@@ -226,6 +226,7 @@ TORRENT_DIR = None
 DOWNLOAD_PROPERS = False
 CHECK_PROPERS_INTERVAL = None
 PROPERS_SEARCH_DAYS = 2
+REMOVE_FROM_CLIENT = False
 ALLOW_HIGH_PRIORITY = False
 SAB_FORCED = False
 RANDOMIZE_PROVIDERS = False

medusa/helpers.py
@@ -62,7 +62,7 @@ from requests.compat import urlparse
 from six import binary_type, string_types, text_type
 from six.moves import http_client
 
-from . import app
+from . import app, db
 from .common import USER_AGENT
 from .helper.common import episode_num, http_code_description, media_extensions, pretty_file_size, subtitle_extensions
 from .helper.exceptions import ex
@@ -1693,3 +1693,26 @@ def get_broken_providers():
 
     logger.info('Broken providers found: {0}'.format(response))
     return ','.join(response)
+
+
+def is_already_processed_media(full_filename):
+    """Check if resource was already processed."""
+    if not is_media_file(str(full_filename)):
+        return False
+    main_db_con = db.DBConnection()
+    history_result = main_db_con.select('SELECT 1 FROM history '
+                                        "WHERE action LIKE '%04' "
+                                        'AND resource LIKE ?',
+                                        ['%' + full_filename])
+    if history_result:
+        return True
+
+
+def is_info_hash_in_history(info_hash):
+    """Check if torrent hash is in history."""
+    main_db_con = db.DBConnection()
+    history_result = main_db_con.select('SELECT 1 FROM history '
+                                        'WHERE info_hash=?',
+                                        [info_hash])
+    if history_result:
+        return True

views/status.mako
@@ -23,6 +23,7 @@
         'Post Process': 'auto_post_processor_scheduler',
         'Subtitles Finder': 'subtitles_finder_scheduler',
         'Trakt Checker': 'trakt_checker_scheduler',
+        'Torrent Checker': 'torrent_checker_scheduler',
     }
 %>
 <div id="config-content">