88043f9084e414e1f8b8599f2d2d184b4cf40faf
fernandog <fernandog@users.noreply.github.com>
2017-01-28 16:07:18 -0200

    Final flake for boxcar2

--- medusa/notifiers/boxcar2.py
+++ medusa/notifiers/boxcar2.py
@@ -18,7 +18,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Medusa. If not, see <http://www.gnu.org/licenses/>.
-
+"""Boxcar2 module."""
 from __future__ import unicode_literals
 
 from .. import app, common, logger
@@ -26,16 +26,20 @@ from ..helpers import get_url, make_session
 
 
 class Notifier(object):
+    """Boxcar2 class."""
+
     def __init__(self):
+        """Initialize the class."""
         self.session = make_session()
         self.url = 'https://new.boxcar.io/api/notifications'
 
     def test_notify(self, accesstoken, title='Medusa: Test'):
-        return self._sendBoxcar2('This is a test notification from Medusa', title, accesstoken)
+        """Test the notify."""
+        return self._send_boxcar2('This is a test notification from Medusa', title, accesstoken)
 
-    def _sendBoxcar2(self, msg, title, accesstoken):
+    def _send_boxcar2(self, msg, title, accesstoken):
         """
-        Sends a boxcar2 notification to the address provided
+        Send a boxcar2 notification to the address provided.
 
         msg: The message to send
         title: The title of the message
@@ -63,37 +67,41 @@ class Notifier(object):
         return True
 
     def notify_snatch(self, ep_name, is_proper):
-        title=common.notifyStrings[(common.NOTIFY_SNATCH, common.NOTIFY_SNATCH_PROPER)[is_proper]]
+        """Send the snatch message."""
+        title = common.notifyStrings[(common.NOTIFY_SNATCH, common.NOTIFY_SNATCH_PROPER)[is_proper]]
         if app.BOXCAR2_NOTIFY_ONSNATCH:
-            self._notifyBoxcar2(title, ep_name)
+            self._notify_boxcar2(title, ep_name)
 
     def notify_download(self, ep_name, title=common.notifyStrings[common.NOTIFY_DOWNLOAD]):
+        """Send the download message."""
         if app.BOXCAR2_NOTIFY_ONDOWNLOAD:
-            self._notifyBoxcar2(title, ep_name)
+            self._notify_boxcar2(title, ep_name)
 
     def notify_subtitle_download(self, ep_name, lang, title=common.notifyStrings[common.NOTIFY_SUBTITLE_DOWNLOAD]):
+        """Send the subtitle download message."""
         if app.BOXCAR2_NOTIFY_ONSUBTITLEDOWNLOAD:
-            self._notifyBoxcar2(title, ep_name + ': ' + lang)
+            self._notify_boxcar2(title, ep_name + ': ' + lang)
 
     def notify_git_update(self, new_version='??'):
+        """Send update available message."""
         update_text = common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
         title = common.notifyStrings[common.NOTIFY_GIT_UPDATE]
-        self._notifyBoxcar2(title, update_text + new_version)
+        self._notify_boxcar2(title, update_text + new_version)
 
     def notify_login(self, ipaddress=''):
+        """Send the new login message."""
         update_text = common.notifyStrings[common.NOTIFY_LOGIN_TEXT]
         title = common.notifyStrings[common.NOTIFY_LOGIN]
-        self._notifyBoxcar2(title, update_text.format(ipaddress))
+        self._notify_boxcar2(title, update_text.format(ipaddress))
 
-    def _notifyBoxcar2(self, title, message, accesstoken=None):
+    def _notify_boxcar2(self, title, message, accesstoken=None):
         """
-        Sends a boxcar2 notification based on the provided info or SB config
+        Send a boxcar2 notification based on the provided info or SB config.
 
         title: The title of the notification to send
         message: The message string to send
         accesstoken: to send to this device
         """
-
         if not app.USE_BOXCAR2:
             logger.log('Notification for Boxcar2 not enabled, skipping this notification', logger.DEBUG)
             return False
@@ -102,4 +110,4 @@ class Notifier(object):
 
         logger.log('Sending notification for {}'.format(message), logger.DEBUG)
 
-        return self._sendBoxcar2(message, title, accesstoken)
+        return self._send_boxcar2(message, title, accesstoken)
