62ffbddbef567748bc1ae970c157fb15173e3d5e
Dario <medariox@users.noreply.github.com>
2017-02-09 20:54:00 +0100

    Update quality-chooser.js

--- static/js/quality-chooser.js
+++ static/js/quality-chooser.js
@@ -28,41 +28,48 @@ $(document).ready(function() {
         return;
     }
 
+    function setQualityText() {
+    	var preferred = $.map($('#preferred_qualities option:selected'), function(option) {
+    	    return option.text;
+    	});
+    	var allowed = $.map($('#allowed_qualities option:selected'), function(option) {
+    	    return option.text;
+    	});
+    	var both = allowed.concat(preferred.filter(function (item) {
+    	    return allowed.indexOf(item) < 0;
+    	}));
+
+    	var allowed_preferred_explanation = both.join(', ');
+    	var preferred_explanation = preferred.join(', ');
+    	var allowed_explanation = allowed.join(', ');
+
+    	$('#allowed_preferred_explanation').text(allowed_preferred_explanation);
+    	$('#preferred_explanation').text(preferred_explanation);
+    	$('#allowed_explanation').text(allowed_explanation);
+
+    	$('#allowed_text').hide();
+    	$('#preferred_text1').hide();
+    	$('#preferred_text2').hide();
+    	$('#quality_explanation').show();
+
+    	if (preferred.length) {
+    		$('#preferred_text1').show();
+    		$('#preferred_text2').show();
+    	} else if (allowed.length) {
+    		$('#allowed_text').show();
+    	} else {
+    		$('#quality_explanation').hide();
+    	}
+    }
+
     $('#qualityPreset').on('change', function() {
         setFromPresets($('#qualityPreset :selected').val());
     });
 
     $('#qualityPreset, #preferred_qualities, #allowed_qualities').on('change', function(){
-        var preferred = $.map($('#preferred_qualities option:selected'), function(option) {
-            return option.text;
-        });
-        var allowed = $.map($('#allowed_qualities option:selected'), function(option) {
-            return option.text;
-        });
-        var both = allowed.concat(preferred.filter(function (item) {
-            return allowed.indexOf(item) < 0;
-        }));
-        var allowed_preferred_explanation =  both.join(', ');
-        var preferred_explanation = preferred.join(', ');
-        var allowed_explanation = allowed.join(', ');
-        if (preferred.length) {
-            $('#allowed_text1').addClass('hidden')
-            $('#preferred_text1').removeClass('hidden')
-            $('#preferred_text2').removeClass('hidden')
-        } else if (allowed.length) {
-            $('#allowed_text1').removeClass('hidden')
-            $('#preferred_text1').addClass('hidden')
-            $('#preferred_text2').addClass('hidden')
-        } else {
-            $('#allowed_text1').addClass('hidden')
-            $('#preferred_text1').addClass('hidden')
-            $('#preferred_text2').addClass('hidden')
-        }
-
-        $('#allowed_preferred_explanation').text(allowed_preferred_explanation);
-        $('#preferred_explanation').text(preferred_explanation);
-        $('#allowed_explanation').text(allowed_explanation);
+        setQualityText();
     });
 
     setFromPresets($('#qualityPreset :selected').val());
+    setQualityText();
 });
