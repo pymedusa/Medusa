a077e5ab135fe8f0a8230b205b90f225429b5323
fernandog <fernandog@users.noreply.github.com>
2017-02-11 18:20:28 -0200

    Proper update forced search

--- static/js/manage/backlog-overview.js
+++ static/js/manage/backlog-overview.js
@@ -1,4 +1,57 @@
 MEDUSA.manage.backlogOverview = function() {
+
+    $(document).ready(function() {
+        checkManualSearches();
+    });
+
+    function checkManualSearches() {
+        var pollInterval = 5000;
+        var searchStatusUrl = 'home/getManualSearchStatus';
+        var showId = $('#showID').val();
+        var url = showId === undefined ? searchStatusUrl : searchStatusUrl + '?show=' + showId;
+        $.ajax({
+            url: url,
+            error: function() {
+                pollInterval = 30000;
+            },
+            type: 'GET',
+            dataType: 'JSON',
+            complete: function() {
+                setTimeout(checkManualSearches, pollInterval);
+            },
+            timeout: 15000 // timeout every 15 secs
+        }).done(function(data) {
+            if (data.episodes) {
+                pollInterval = 5000;
+            } else {
+                pollInterval = 15000;
+            }
+            updateForcedSearch(data);
+            // cleanupManualSearches(data);
+        });
+    }
+
+    function updateForcedSearch(data) {
+        $.each(data.episodes, function(name, ep) {
+            var el = $('a[id=' + ep.show + 'x' + ep.season + 'x' + ep.episode + ']');
+            var img = el.children('img[data-ep-search]');
+            var parent = el.parent();
+            if (el) {
+                if (ep.searchstatus.toLowerCase() === 'searching') {
+                    // el=$('td#' + ep.season + 'x' + ep.episode + '.search img');
+                    img.prop('src', 'images/loading16.gif');
+                } else if (ep.searchstatus.toLowerCase() === 'finished') {
+                    // el=$('td#' + ep.season + 'x' + ep.episode + '.search img');
+                    if (ep.status.indexOf('Snatched') > 0) {
+                        img.prop('src', 'images/loading16.gif');
+                    } else {
+                        img.prop('src', 'images/search16.png');
+                    }
+                }
+            }
+         });
+    }
+
     $('#pickShow').on('change', function() {
         var id = $(this).val();
         if (id) {
@@ -42,12 +95,7 @@ MEDUSA.manage.backlogOverview = function() {
         var url = $(this).prop('href');
         $.getJSON(url, function(data) {
             // if they failed then just put the red X
-            if (data.result.toLowerCase() === 'success') {
-                img.prop('src', 'images/yes16.png');
-                setTimeout(function() {
-                    img.parent().parent().parent().remove()
-                }, 3000)
-            } else {
+            if (data.result.toLowerCase() === 'failed') {
                 img.prop('src', 'images/no16.png');
             }
             return false;
