52d2f73cc4c7483cfc12247ef5c972242e743fe1
fernandog <fernandog@users.noreply.github.com>
2017-02-20 10:13:01 -0300

    Catch IndexerShowNotFoundInLanguage while adding show

--- medusa/show_queue.py
+++ medusa/show_queue.py
@@ -420,6 +420,18 @@ class QueueItemAdd(ShowQueueItem):
             )
             self._finishEarly()
             return
+        except IndexerShowNotFoundInLanguage as e:
+            logger.log(u'{id}: Data retrieved from {indexer} was incomplete. The indexer does not provide '
+                       u'show information in the searched language {language}. Aborting: {error_msg}'.format
+                       (language=e.language, id=self.show.indexerid, indexer=indexerApi(self.show.indexer).name,
+                        error_msg=ex(e)), logger.WARNING)
+            ui.notifications.error('Error adding show!',
+                                   'Unable to add show {show_name} on {indexer} with this language: {language}'.
+                                   format(show_name=self.show.name,
+                                          indexer=indexerApi(self.show.indexer).name,
+                                          language=e.language))
+            self._finishEarly()
+            return
         except Exception as e:
             logger.log(u"%s Error while loading information from indexer %s. "
                        u"Error: %r" % (self.indexer_id, indexerApi(self.indexer).name, ex(e)), logger.ERROR)
