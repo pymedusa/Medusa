33445d096e50b6acc806d1ce7063aa2381b1858a
P0psicles <rogier@headshots.nl>
2017-02-24 17:43:10 +0100

    Tried to improve the line continuation.

--- static/js/manage/manage-searches.js
+++ static/js/manage/manage-searches.js
@@ -4,44 +4,38 @@ MEDUSA.manage.manageSearches = function() {
     var updateExceptionTable = function(exceptions) {
         var status = $('#sceneExceptionStatus');
 
-        var table = $('<ul></ul>')
-            .append('<li>Last updated custom exceptions ' +
-                new Date(exceptions.data.last_update.custom_exceptions * 1000).toLocaleDateString())
-            .append('<li>Last updated xem exceptions ' +
-                new Date(exceptions.data.last_update.xem * 1000).toLocaleDateString())
-            .append('<li>Last updated anidb exceptions ' +
-                new Date(exceptions.data.last_update.anidb * 1000).toLocaleDateString());
+        var table = $('<ul></ul>').append('<li>Last updated custom exceptions ' +
+            new Date(exceptions.data.last_update.custom_exceptions * 1000)
+                .toLocaleDateString()).append('<li>Last updated xem exceptions ' +
+            new Date(exceptions.data.last_update.xem * 1000)
+                .toLocaleDateString()).append('<li>Last updated anidb exceptions ' +
+            new Date(exceptions.data.last_update.anidb * 1000)
+                .toLocaleDateString());
         status.append(table);
 
         $('.forceSceneExceptionRefresh').removeClass('disabled');
     };
 
-    api.get(url)
-        .then(function(response) {
-            updateExceptionTable(response);
-        })
-        .catch(function(err) {
-            console.log('Trying to get scene exceptions failed with error: ' + err);
-        });
+    api.get(url).then(function(response) {
+        updateExceptionTable(response);
+    }).catch(function(err) {
+        console.log('Trying to get scene exceptions failed with error: ' + err);
+    });
 
     $('.forceSceneExceptionRefresh').on('click', function() {
-        var url = 'manage/searches/sceneexception/retrieve';
         var status = $('#sceneExceptionStatus');
         status[0].innerHTML = 'Retrieving scene exceptions...';
 
         api.post(url, {}, {
             timeout: 60000
-        })
-        .then(function(response) {
+        }).then(function(response) {
             status[0].innerHTML = '';
             status.append(
-                $('<span></span>')
-                    .text(response.data.result)
+                $('<span></span>').text(response.data.result)
             );
             updateExceptionTable(response);
             $('.forceSceneExceptionRefresh').addClass('disabled');
-        })
-        .catch(function(err) {
+        }).catch(function(err) {
             console.log('Trying to update scene exceptions failed with error: ' + err);
         });
     });
