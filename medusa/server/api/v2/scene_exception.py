7c53b5cb2968dd74f396f062f10c1e89c8fb21fa
P0psicles <rogier@headshots.nl>
2017-02-24 10:16:56 +0100

    Fixed docstrings and import order.

--- medusa/server/api/v2/scene_exception.py
+++ medusa/server/api/v2/scene_exception.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 """Request handler for scene_exceptions."""
 
-from .base import BaseRequestHandler
 from medusa.scene_exceptions import get_last_refresh, retrieve_exceptions
+from .base import BaseRequestHandler
 
 
 class SceneExceptionRetrieveHandler(BaseRequestHandler):
@@ -15,6 +15,7 @@ class SceneExceptionRetrieveHandler(BaseRequestHandler):
 
     @staticmethod
     def get_last_updates():
+        """Query the cache table for the last update for every scene exception source."""
         last_updates = {}
         for scene_exception_source in ['custom_exceptions', 'xem', 'anidb']:
             last_updates[scene_exception_source] = get_last_refresh(scene_exception_source)[0]['last_refreshed']
@@ -25,7 +26,7 @@ class SceneExceptionRetrieveHandler(BaseRequestHandler):
         return self.api_finish(data={'last_update': self.get_last_updates()})
 
     def post(self):
-        """Start fetch retrieving scene name exceptions from ."""
+        """Start fetch retrieving scene name exceptions."""
         retrieve_exceptions(force=True)
         return self.api_finish(data={'result': 'Updated scene exceptions',
                                      'last_update': self.get_last_updates()})
