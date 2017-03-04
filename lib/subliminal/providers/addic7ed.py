7bb8017db880cc1e4cca07db3bb8a07330eba1ed
fernandog <fernandog@users.noreply.github.com>
2017-03-04 15:22:52 -0300

    [Local only] Addi7ed: Add retry decorator

--- lib/subliminal/providers/addic7ed.py
+++ lib/subliminal/providers/addic7ed.py
@@ -2,9 +2,14 @@
 import logging
 import re
 
+import requests
+import time
+from functools import wraps
+ 
 from babelfish import Language, language_converters
 from guessit import guessit
 from requests import Session
+from socket import timeout as SocketTimeout
 
 from . import ParserBeautifulSoup, Provider
 from .. import __short_version__
@@ -95,6 +100,44 @@ class Addic7edProvider(Provider):
         self.password = password
         self.logged_in = False
 
+    def retry(ExceptionToCheck, tries=3, delay=5, backoff=3):
+        """Retry calling the decorated function using an exponential backoff.
+
+        http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+        original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+        param ExceptionToCheck: the exception to check. may be a tuple of exceptions to check
+        :type ExceptionToCheck: Exception or tuple
+        :param tries: number of times to try (not retry) before giving up
+        :type tries: int
+        :param delay: initial delay between retries in seconds
+        :type delay: int
+        :param backoff: backoff multiplier e.g. value of 2 will double the delay
+            each retry
+        :type backoff: int
+        """
+
+        def deco_retry(f):
+
+            @wraps(f)
+            def f_retry(*args, **kwargs):
+                mtries, mdelay = tries, delay
+                while mtries > 1:
+                    try:
+                        return f(*args, **kwargs)
+                    except ExceptionToCheck, e:
+                        msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                        logger.warning(msg)
+                        time.sleep(mdelay)
+                        mtries -= 1
+                        mdelay *= backoff
+                return f(*args, **kwargs)
+
+            return f_retry  # true decorator
+
+        return deco_retry
+
+    @retry((requests.exceptions.RequestException, SocketTimeout,))
     def initialize(self):
         self.session = Session()
         self.session.headers['User-Agent'] = 'Subliminal/%s' % __short_version__
@@ -111,6 +154,7 @@ class Addic7edProvider(Provider):
             logger.debug('Logged in')
             self.logged_in = True
 
+    @retry((requests.exceptions.RequestException, SocketTimeout,))
     def terminate(self):
         # logout
         if self.logged_in:
@@ -122,6 +166,7 @@ class Addic7edProvider(Provider):
 
         self.session.close()
 
+    @retry((requests.exceptions.RequestException, SocketTimeout,))
     @region.cache_on_arguments(expiration_time=SHOW_EXPIRATION_TIME)
     def _get_show_ids(self):
         """Get the ``dict`` of show ids per series by querying the `shows.php` page.
@@ -144,6 +189,7 @@ class Addic7edProvider(Provider):
 
         return show_ids
 
+    @retry((requests.exceptions.RequestException, SocketTimeout,))
     @region.cache_on_arguments(expiration_time=SHOW_EXPIRATION_TIME)
     def _search_show_id(self, series, year=None):
         """Search the show id from the `series` and `year`.
@@ -223,6 +269,7 @@ class Addic7edProvider(Provider):
 
         return show_id
 
+    @retry((requests.exceptions.RequestException, SocketTimeout,))
     def query(self, series, season, year=None, country=None):
         # get the show id
         show_id = self.get_show_id(series, year, country)
@@ -273,6 +320,7 @@ class Addic7edProvider(Provider):
         return [s for s in self.query(video.series, video.season, video.year)
                 if s.language in languages and s.episode == video.episode]
 
+    @retry((requests.exceptions.RequestException, SocketTimeout,))
     def download_subtitle(self, subtitle):
         # download the subtitle
         logger.info('Downloading subtitle %r', subtitle)
