717c11fc8a5626928cc657ed2d02decd12f76d0e
p0ps <p0psicles@users.noreply.github.com>
2017-03-11 13:29:38 +0100

    Feature/force exception refresh (#2288)
    * Added scene_exceptions route for force a refresh of the scene_exception lists.
    * Split up the db query from should_refresh as I'd like to use that in the route.
    * Add manage-searches.js.
    * Added get the latest scene exceptions.
    * Force update the latest scene exception through a button in manage searches.
    * Disable force button after running it.
    * Changed description for custom exception into Medusa's Exception, and added refer to wiki.
    * Added link to thexem.de.
    * Changed the refresh route to a rest operation.
    * Added sceneexception rest routes from another branch.
    * Use response code 201 for put.
    * Moved 'or force' before getting the update of the db. Saves a db hit.
    * Return camelCase to js.
    * Set 404 when resource not found instead of 400.
    * Return 200 with body on sceneexception post.
    * Minor initial changes: scene_exception and exception_type should be 2 different resources and have different endpoints
    * Changed PUT into POST, as we're adding a new RPC call. Adding to the operations resource.
    * Change last_updates key to lastUpdates, to make it confirm js standards.
    * Fixed the sceneexceptions route.
    * Added helper methods for indexer_id_to_slug and slug_to_indexer_id, for easy indexer -> slug transformations.
    * Changed series.py to also ustilize the indexer_config.py helper methods.
    * Added filter options to the sceneexceptions route.
    * Sceneexceptions route returns a list. Removed the last_update info per source.
    * Change retrieve_exceptions to make it accept the scene exception type, which can be used to only update that type.
    * Changed routes to accept:
    * POST: /exceptiontype/medusa/operation/
    * POST: /exceptiontype/xem/operation/
    * POST: /exceptiontype/anidb/operation/
    * GET: /sceneexception (with filters)
    * GET/PUT/DELETE: /sceneexception/1
    Added route to retrieve all exceptiontypes:
    * POST: /exceptiontype/operation/
    Changed the js, to utilize the new routes.
    * Remove some traces of the ?detailed param.
    * Make sure we return a dict, when a scene exception is called with exception_id.
    * only show 404 for when quering for a single resource that cannot be found.
    * Added spinner for refreshing sceneExceptions.
    * Cleaned up the <br>
    * use js default .filter in stead of the underscore function.

--- medusa/tv/series.py
+++ medusa/tv/series.py
@@ -83,8 +83,9 @@ from medusa.indexers.indexer_api import indexerApi
 from medusa.indexers.indexer_config import (
     INDEXER_TVRAGE,
     indexerConfig,
+    indexer_id_to_slug,
     mappings,
-    reverse_mappings,
+    reverse_mappings
 )
 from medusa.indexers.indexer_exceptions import (
     IndexerAttributeNotFound,
@@ -266,7 +267,7 @@ class Series(TV):
     @property
     def indexer_slug(self):
         """Return the slug name of the show. Example: tvdb1234."""
-        return '{name}{indexerid}'.format(name=self.indexer_name, indexerid=self.indexerid)
+        return indexer_id_to_slug(self.indexer, self.indexerid)
 
     @location.setter
     def location(self, value):
