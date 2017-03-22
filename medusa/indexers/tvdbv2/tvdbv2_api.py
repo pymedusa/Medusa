068971518418cf1bfe3e2db8facab7a786450a76
medariox <medariox@users.noreply.github.com>
2017-03-22 16:31:54 +0100

    Fix TypeError when updates_api returns None. Fixes #2424

--- medusa/indexers/tvdbv2/tvdbv2_api.py
+++ medusa/indexers/tvdbv2/tvdbv2_api.py
@@ -562,9 +562,10 @@ class TVDBv2(BaseIndexer):
         try:
             while updates and count < weeks:
                 updates = self.updates_api.updated_query_get(from_time).data
-                last_update_ts = max(x.last_updated for x in updates)
-                from_time = last_update_ts
-                total_updates += [int(_.id) for _ in updates]
+                if updates is not None:
+                    last_update_ts = max(x.last_updated for x in updates)
+                    from_time = last_update_ts
+                    total_updates += [int(_.id) for _ in updates]
                 count += 1
         except (ApiException, MaxRetryError, RequestError) as e:
             raise IndexerUnavailable('Error connecting to Tvdb api. Caused by: {0!r}'.format(e))
