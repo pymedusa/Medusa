640c9c443499d96ad7b9cef019a0605dcebdec7a
fernandog <fernandog@users.noreply.github.com>
2017-03-26 11:21:36 -0300

    Move all_possible_names to Series object

--- medusa/show_name_helpers.py
+++ medusa/show_name_helpers.py
@@ -94,48 +94,6 @@ def filterBadReleases(name, parse=True):
     return True
 
 
-def allPossibleShowNames(show, season=-1):
-    """
-    Figures out every possible variation of the name for a particular show.
-
-    Includes indexer name, and any scene exception names, and country code
-    at the end of the name (e.g. "Show Name (AU)".
-
-    show: a Series object that we should get the names of
-    Returns: all possible show names
-    """
-
-    show_names = get_scene_exceptions(show.indexerid, show.indexer, season)
-    show_names.add(show.name)
-
-    new_show_names = set()
-
-    if not show.is_anime:
-        country_list = {}
-        # add the country list
-        country_list.update(common.countryList)
-        # add the reversed mapping of the country list
-        country_list.update({v: k for k, v in common.countryList.items()})
-
-        for name in show_names:
-            if not name:
-                continue
-
-            # if we have "Show Name Australia" or "Show Name (Australia)"
-            # this will add "Show Name (AU)" for any countries defined in
-            # common.countryList (and vice versa)
-            for country in country_list:
-                pattern_1 = ' {0}'.format(country)
-                pattern_2 = ' ({0})'.format(country)
-                replacement = ' ({0})'.format(country_list[country])
-                if name.endswith(pattern_1):
-                    new_show_names.add(name.replace(pattern_1, replacement))
-                elif name.endswith(pattern_2):
-                    new_show_names.add(name.replace(pattern_2, replacement))
-
-    return show_names.union(new_show_names)
-
-
 def determineReleaseName(dir_name=None, nzb_name=None):
     """Determine a release name from an nzb and/or folder name."""
 
--- medusa/tv/series.py
+++ medusa/tv/series.py
@@ -49,6 +49,7 @@ from medusa import (
 from medusa.black_and_white_list import BlackAndWhiteList
 from medusa.common import (
     ARCHIVED,
+    countryList,
     DOWNLOADED,
     FAILED,
     IGNORED,
@@ -1759,6 +1760,47 @@ class Series(TV):
         preferred = Quality.split_quality(self.quality)[1]
 
         return [Quality.qualityStrings[v] for v in preferred]
+
+    def get_all_possible_names(self, season=-1):
+        """
+        Figures out every possible variation of the name for a particular show.
+
+        Includes indexer name, and any scene exception names, and country code
+        at the end of the name (e.g. "Show Name (AU)".
+
+        show: a Series object that we should get the names of
+        Returns: all possible show names
+        """
+
+        show_names = get_scene_exceptions(self.indexerid, self.indexer, season)
+        show_names.add(self.name)
+
+        new_show_names = set()
+
+        if not self.is_anime:
+            country_list = {}
+            # add the country list
+            country_list.update(countryList)
+            # add the reversed mapping of the country list
+            country_list.update({v: k for k, v in countryList.items()})
+
+            for name in show_names:
+                if not name:
+                    continue
+
+                # if we have "Show Name Australia" or "Show Name (Australia)"
+                # this will add "Show Name (AU)" for any countries defined in
+                # common.countryList (and vice versa)
+                for country in country_list:
+                    pattern_1 = ' {0}'.format(country)
+                    pattern_2 = ' ({0})'.format(country)
+                    replacement = ' ({0})'.format(country_list[country])
+                    if name.endswith(pattern_1):
+                        new_show_names.add(name.replace(pattern_1, replacement))
+                    elif name.endswith(pattern_2):
+                        new_show_names.add(name.replace(pattern_2, replacement))
+
+        return show_names.union(new_show_names)
 
     @staticmethod
     def __qualities_to_string(qualities=None):
