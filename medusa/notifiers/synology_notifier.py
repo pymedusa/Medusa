5c77c4cac6ba31b41450a8be900c42059038ec74
Labrys <Labrys.git@gmail.com>
2017-03-30 22:05:37 -0400

    Standardize logging

--- medusa/notifiers/synology_notifier.py
+++ medusa/notifiers/synology_notifier.py
@@ -1,27 +1,15 @@
 # coding=utf-8
 
-# Author: Nyaran <nyayukko@gmail.com>
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
-
+import logging
 import os
 import subprocess
 
-from .. import app, common, logger
-from ..helper.exceptions import ex
+from medusa import app, common
+from medusa.helper.exceptions import ex
+from medusa.logger.adapters.style import BraceAdapter
+
+log = BraceAdapter(logging.getLogger(__name__))
+log.logger.addHandler(logging.NullHandler())
 
 
 class Notifier(object):
@@ -35,28 +23,28 @@ class Notifier(object):
 
     def notify_subtitle_download(self, ep_name, lang):
         if app.SYNOLOGYNOTIFIER_NOTIFY_ONSUBTITLEDOWNLOAD:
-            self._send_synologyNotifier(ep_name + ": " + lang, common.notifyStrings[common.NOTIFY_SUBTITLE_DOWNLOAD])
+            self._send_synologyNotifier(ep_name + ': ' + lang, common.notifyStrings[common.NOTIFY_SUBTITLE_DOWNLOAD])
 
-    def notify_git_update(self, new_version="??"):
+    def notify_git_update(self, new_version='??'):
         if app.USE_SYNOLOGYNOTIFIER:
             update_text = common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
             title = common.notifyStrings[common.NOTIFY_GIT_UPDATE]
             self._send_synologyNotifier(update_text + new_version, title)
 
-    def notify_login(self, ipaddress=""):
+    def notify_login(self, ipaddress=''):
         if app.USE_SYNOLOGYNOTIFIER:
             update_text = common.notifyStrings[common.NOTIFY_LOGIN_TEXT]
             title = common.notifyStrings[common.NOTIFY_LOGIN]
             self._send_synologyNotifier(update_text.format(ipaddress), title)
 
     def _send_synologyNotifier(self, message, title):
-        synodsmnotify_cmd = ["/usr/syno/bin/synodsmnotify", "@administrators", title, message]
-        logger.log(u"Executing command " + str(synodsmnotify_cmd))
-        logger.log(u"Absolute path to command: " + os.path.abspath(synodsmnotify_cmd[0]), logger.DEBUG)
+        synodsmnotify_cmd = ['/usr/syno/bin/synodsmnotify', '@administrators', title, message]
+        log.info(u'Executing command {}', synodsmnotify_cmd)
+        log.debug(u'Absolute path to command: {0}', os.path.abspath(synodsmnotify_cmd[0]))
         try:
             p = subprocess.Popen(synodsmnotify_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                  cwd=app.PROG_DIR)
             out, _ = p.communicate()
-            logger.log(u"Script result: " + str(out), logger.DEBUG)
+            log.debug(u'Script result: {0}', out)
         except OSError as e:
-            logger.log(u"Unable to run synodsmnotify: " + ex(e))
+            log.info(u'Unable to run synodsmnotify: {}', ex(e))
