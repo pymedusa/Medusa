29fdba8f971c7a1f5dd9e69faa87b2a9eb187d9e
Labrys <Labrys.git@gmail.com>
2017-03-30 20:53:47 -0400

    Standardize logging

--- medusa/metadata/wdtv.py
+++ medusa/metadata/wdtv.py
@@ -1,35 +1,23 @@
 # coding=utf-8
-# Author: Nic Wolfe <nic@wolfeden.ca>
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+import logging
 import os
 import re
 
-from .. import helpers, logger
-from ..helper.common import dateFormat, episode_num as ep_num, replace_extension
-from ..indexers.indexer_api import indexerApi
-from ..indexers.indexer_exceptions import IndexerEpisodeNotFound, IndexerSeasonNotFound
-from ..metadata import generic
+from medusa import helpers
+from medusa.helper.common import dateFormat, episode_num as ep_num, replace_extension
+from medusa.indexers.indexer_api import indexerApi
+from medusa.indexers.indexer_exceptions import IndexerEpisodeNotFound, IndexerSeasonNotFound
+from medusa.metadata import generic
 
 try:
     import xml.etree.cElementTree as etree
 except ImportError:
     import xml.etree.ElementTree as etree
+
+log = logging.getLogger(__name__)
+log.logger.addHandler(logging.NullHandler())
 
 
 class WDTVMetadata(generic.GenericMetadata):
@@ -162,12 +150,11 @@ class WDTVMetadata(generic.GenericMetadata):
                 break
 
         if not season_dir:
-            logger.log(u'Unable to find a season directory for season {season_num}'.format
-                       (season_num=season), logger.DEBUG)
+            log.debug(u'Unable to find a season directory for season {0}', season)
             return None
 
-        logger.log(u'Using {path}/folder.jpg as season dir for season {season_num}'.format
-                   (path=season_dir, season_num=season), logger.DEBUG)
+        log.debug(u'Using {path}/folder.jpg as season dir for season {x}',
+                  {'path': season_dir, 'x': season})
 
         return os.path.join(show_obj.location, season_dir, 'folder.jpg')
 
@@ -193,10 +180,12 @@ class WDTVMetadata(generic.GenericMetadata):
             try:
                 my_ep = my_show[ep_to_write.season][ep_to_write.episode]
             except (IndexerEpisodeNotFound, IndexerSeasonNotFound):
-                logger.log(u'Unable to find episode {ep_num} on {indexer}... '
-                           u'has it been removed? Should I delete from db?'.format
-                           (ep_num=ep_num(ep_to_write.season, ep_to_write.episode),
-                            indexer=indexerApi(ep_obj.show.indexer).name))
+                log.info(
+                    u'Unable to find episode {x} on {indexer}... has it been removed? Should I delete from db?', {
+                        'x': ep_num(ep_to_write.season, ep_to_write.episode),
+                        'indexer': indexerApi(ep_obj.show.indexer).name,
+                    }
+                )
                 return None
 
             if ep_obj.season == 0 and not getattr(my_ep, 'firstaired', None):
