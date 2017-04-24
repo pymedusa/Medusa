55492dd4774e81426cfc467531df1c97b14da5f2
Fernando <fernandog@users.noreply.github.com>
2017-04-24 09:51:11 -0300

    Missing slash

--- medusa/server/core.py
+++ medusa/server/core.py
@@ -202,7 +202,7 @@ class AppWebServer(threading.Thread):  # pylint: disable=too-many-instance-attri
             protocol = 'http'
             self.server = HTTPServer(self.app)
 
-        logger.log('Starting Medusa on {scheme}://{host}:{port}{web_root}'.format
+        logger.log('Starting Medusa on {scheme}://{host}:{port}{web_root}/'.format
                    (scheme=protocol,
                     host=self.options['host'],
                     port=self.options['port'],
