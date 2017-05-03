9371c4a4048e5c5447895365abe45e891d673f0e
medariox <medariox@users.noreply.github.com>
2017-05-03 14:30:32 +0200

    Add constructor docstrings

--- medusa/classes.py
+++ medusa/classes.py
@@ -315,6 +315,7 @@ class Viewer(object):
     """Keep the Errors to be displayed in the UI."""
 
     def __init__(self):
+        """Initialize the default constructor."""
         self._errors = dict()
 
     def add(self, logline):
