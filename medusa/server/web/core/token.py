835335976c582f7764ebc5002ba14e012c62b437
P0psicles <rogier@headshots.nl>
2017-05-06 17:34:36 +0200

    Removed unused import.

--- medusa/server/web/core/token.py
+++ medusa/server/web/core/token.py
@@ -4,11 +4,14 @@ from __future__ import unicode_literals
 
 from tornado.web import authenticated
 
-from ..core.base import WebHandler, BaseHandler
+from ..core.base import BaseHandler
 from medusa import app
 
 
 class TokenHandler(BaseHandler):
+    """
+    Handle the request for /token, and return the app.API_KEY if authenticated.
+    """
     def __init__(self, *args, **kwargs):
         super(TokenHandler, self).__init__(*args, **kwargs)
 
