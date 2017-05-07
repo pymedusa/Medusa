87dd7c71a1fdb9c4a3a5fd392d04833c052d584e
Labrys <Labrys.git@gmail.com>
2017-05-07 02:48:45 -0400

    Standardize logging

--- medusa/providers/torrent/html/limetorrents.py
+++ medusa/providers/torrent/html/limetorrents.py
@@ -1,41 +1,29 @@
 # coding=utf-8
-# Author: Gonçalo M. (aka duramato/supergonkas) <supergonkas@gmail.com>
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
+
 """Provider code for Limetorrents."""
+
 from __future__ import unicode_literals
 
+import logging
 import re
 import traceback
 
 from contextlib2 import suppress
 
-from medusa import (
-    logger,
-    tv,
-)
+from medusa import tv
 from medusa.bs4_parser import BS4Parser
 from medusa.helper.common import (
     convert_size,
     try_int,
 )
+from medusa.logger.adapters.style import BraceAdapter
 from medusa.providers.torrent.torrent_provider import TorrentProvider
 
 from requests.compat import urljoin
 from requests.exceptions import ConnectionError as RequestsConnectionError, Timeout
+
+log = BraceAdapter(logging.getLogger(__name__))
+log.logger.addHandler(logging.NullHandler())
 
 id_regex = re.compile(r'(?:\/)(.*)(?:-torrent-([0-9]*)\.html)', re.I)
 hash_regex = re.compile(r'(.*)([0-9a-f]{40})(.*)', re.I)
@@ -86,15 +74,15 @@ class LimeTorrentsProvider(TorrentProvider):
         results = []
 
         for mode in search_strings:
-            logger.log('Search mode: {0}'.format(mode), logger.DEBUG)
+            log.debug('Search mode: {0}', mode)
 
             for search_string in search_strings[mode]:
 
                 if mode != 'RSS':
-                    logger.log('Search string: {search}'.format
-                               (search=search_string), logger.DEBUG)
+                    log.debug('Search string: {search}',
+                              {'search': search_string})
                     if self.confirmed:
-                        logger.log('Searching only confirmed torrents', logger.DEBUG)
+                        log.debug('Searching only confirmed torrents')
 
                     search_url = self.urls['search'].format(query=search_string)
                 else:
@@ -103,7 +91,7 @@ class LimeTorrentsProvider(TorrentProvider):
 
                 response = self.get_url(search_url, returns='response')
                 if not response or not response.text:
-                    logger.log('No data returned from provider', logger.DEBUG)
+                    log.debug('No data returned from provider')
                     continue
 
                 results += self.parse(response.text, mode)
@@ -128,8 +116,8 @@ class LimeTorrentsProvider(TorrentProvider):
             torrent_table = html.find('table', class_='table2')
 
             if not torrent_table:
-                logger.log('Data returned from provider does not contain any {0}torrents'.format(
-                           'confirmed ' if self.confirmed else ''), logger.DEBUG)
+                log.debug('Data returned from provider does not contain any {0}torrents',
+                          'confirmed ' if self.confirmed else '')
                 return items
 
             torrent_rows = torrent_table.find_all('tr')
@@ -174,9 +162,9 @@ class LimeTorrentsProvider(TorrentProvider):
 
                     if seeders < min(self.minseed, 1):
                         if mode != 'RSS':
-                            logger.log("Discarding torrent because it doesn't meet the "
-                                       "minimum seeders: {0}. Seeders: {1}".format
-                                       (title, seeders), logger.DEBUG)
+                            log.log("Discarding torrent because it doesn't meet the"
+                                    " minimum seeders: {0}. Seeders: {1}",
+                                    title, seeders)
                         continue
 
                     size = convert_size(cells[labels.index('Size')].get_text(strip=True)) or -1
@@ -192,13 +180,13 @@ class LimeTorrentsProvider(TorrentProvider):
                         'pubdate': None,
                     }
                     if mode != 'RSS':
-                        logger.log('Found result: {0} with {1} seeders and {2} leechers'.format
-                                   (title, seeders, leechers), logger.DEBUG)
+                        log.debug('Found result: {0} with {1} seeders and {2} leechers',
+                                  title, seeders, leechers)
 
                     items.append(item)
                 except (AttributeError, TypeError, KeyError, ValueError, IndexError):
-                    logger.log('Failed parsing provider. Traceback: {0!r}'.format
-                               (traceback.format_exc()), logger.ERROR)
+                    log.error('Failed parsing provider. Traceback: {0!r}',
+                              traceback.format_exc())
 
         return items
 
