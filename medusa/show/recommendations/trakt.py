bece396db6cc68d6ec9c8599fcd27f39200ec0e2
Labrys <Labrys.git@gmail.com>
2017-05-07 03:34:48 -0400

    Flake

--- medusa/show/recommendations/trakt.py
+++ medusa/show/recommendations/trakt.py
@@ -1,38 +1,27 @@
 # coding=utf-8
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import unicode_literals
 
 import logging
 import os
 import time
+
+from medusa import app
+from medusa.helper.common import try_int
+from medusa.helper.exceptions import MultipleShowObjectsException
+from medusa.indexers.indexer_api import indexerApi
+from medusa.indexers.indexer_config import INDEXER_TVDBV2
+from medusa.logger.adapters.style import BraceAdapter
+from medusa.show.recommendations.recommended import RecommendedShow
+
 import requests
 from simpleanidb import Anidb
 from traktor import (TokenExpiredException, TraktApi, TraktException)
 from tvdbapiv2.exceptions import ApiException
-from .recommended import RecommendedShow
-from ... import app
-from ...helper.common import try_int
-from ...helper.exceptions import MultipleShowObjectsException, ex
-from ...indexers.indexer_api import indexerApi
-from ...indexers.indexer_config import INDEXER_TVDBV2
-from medusa.logger.adapters.style import BraceAdapter
 
 log = BraceAdapter(logging.getLogger(__name__))
 log.logger.addHandler(logging.NullHandler())
+
 
 class MissingPosterList(list):
     """Smart custom list, with a cache expiration.
