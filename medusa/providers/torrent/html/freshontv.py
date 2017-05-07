3161f9df9efa48d7f8270167230559b519fda894
Labrys <Labrys.git@gmail.com>
2017-05-07 02:47:22 -0400

    Standardize logging

--- medusa/providers/torrent/html/freshontv.py
+++ medusa/providers/torrent/html/freshontv.py
@@ -1,36 +1,21 @@
 # coding=utf-8
-# Author: Idan Gutman
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
+
 """Provider code for FreshOnTV."""
+
 from __future__ import unicode_literals
 
+import logging
 import re
 import time
 import traceback
 
-from medusa import (
-    logger,
-    tv,
-)
+from medusa import tv
 from medusa.bs4_parser import BS4Parser
 from medusa.helper.common import (
     convert_size,
     try_int,
 )
+from medusa.logger.adapters.style import BraceAdapter
 from medusa.providers.torrent.torrent_provider import TorrentProvider
 
 from requests.compat import urljoin
@@ -39,6 +24,9 @@ from requests.utils import (
     dict_from_cookiejar,
 )
 from six import text_type
+
+log = BraceAdapter(logging.getLogger(__name__))
+log.logger.addHandler(logging.NullHandler())
 
 
 class FreshOnTVProvider(TorrentProvider):
@@ -95,13 +83,13 @@ class FreshOnTVProvider(TorrentProvider):
         freeleech = '3' if self.freeleech else '0'
 
         for mode in search_strings:
-            logger.log('Search mode: {0}'.format(mode), logger.DEBUG)
+            log.debug('Search mode: {0}', mode)
 
             for search_string in search_strings[mode]:
 
                 if mode != 'RSS':
-                    logger.log('Search string: {search}'.format
-                               (search=search_string), logger.DEBUG)
+                    log.debug('Search string: {search}',
+                              {'search': search_string})
 
                 search_url = self.urls['search'] % (freeleech, search_string)
                 response = self.get_url(search_url, returns='response')
@@ -109,7 +97,7 @@ class FreshOnTVProvider(TorrentProvider):
                 max_page_number = 0
 
                 if not response or not response.text:
-                    logger.log('No data returned from provider', logger.DEBUG)
+                    log.debug('No data returned from provider')
                     continue
 
                 with BS4Parser(response.text, 'html5lib') as html:
@@ -136,8 +124,8 @@ class FreshOnTVProvider(TorrentProvider):
                         if max_page_number > 3 and mode == 'RSS':
                             max_page_number = 3
                     except Exception:
-                        logger.log('Failed parsing provider. Traceback: {0!r}'.format
-                                   (traceback.format_exc()), logger.ERROR)
+                        log.error('Failed parsing provider. Traceback: {0!r}',
+                                  traceback.format_exc())
                         continue
 
                 responses = [response]
@@ -175,7 +163,7 @@ class FreshOnTVProvider(TorrentProvider):
 
             # Continue only if at least one release is found
             if not torrent_rows:
-                logger.log('Data returned from provider does not contain any torrents', logger.DEBUG)
+                log.debug('Data returned from provider does not contain any torrents')
                 return items
 
             for row in torrent_rows:
@@ -198,9 +186,9 @@ class FreshOnTVProvider(TorrentProvider):
                     # Filter unseeded torrent
                     if seeders < min(self.minseed, 1):
                         if mode != 'RSS':
-                            logger.log("Discarding torrent because it doesn't meet the "
-                                       "minimum seeders: {0}. Seeders: {1}".format
-                                       (title, seeders), logger.DEBUG)
+                            log.debug("Discarding torrent because it doesn't meet the"
+                                      " minimum seeders: {0}. Seeders: {1}",
+                                      title, seeders)
                         continue
 
                     torrent_size = row.find('td', class_='table_size').get_text(strip=True)
@@ -217,13 +205,13 @@ class FreshOnTVProvider(TorrentProvider):
                         'pubdate': None,
                     }
                     if mode != 'RSS':
-                        logger.log('Found result: {0} with {1} seeders and {2} leechers'.format
-                                   (title, seeders, leechers), logger.DEBUG)
+                        log.debug('Found result: {0} with {1} seeders and {2} leechers',
+                                  title, seeders, leechers)
 
                     items.append(item)
                 except (AttributeError, TypeError, KeyError, ValueError, IndexError):
-                    logger.log('Failed parsing provider. Traceback: {0!r}'.format
-                               (traceback.format_exc()), logger.ERROR)
+                    log.debug('Failed parsing provider. Traceback: {0!r}',
+                              traceback.format_exc())
 
         return items
 
@@ -242,7 +230,7 @@ class FreshOnTVProvider(TorrentProvider):
         else:
             response = self.get_url(self.urls['login'], post_data=login_params, returns='response')
             if not response or not response.text:
-                logger.log('Unable to connect to provider', logger.WARNING)
+                log.warning('Unable to connect to provider')
                 return False
 
             if re.search('/logout.php', response.text):
@@ -256,7 +244,7 @@ class FreshOnTVProvider(TorrentProvider):
                                         'pass': self._hash}
                         return True
                 except Exception:
-                    logger.log('Unable to login to provider (cookie)', logger.WARNING)
+                    log.warning('Unable to login to provider (cookie)')
 
                     return False
             else:
@@ -264,14 +252,14 @@ class FreshOnTVProvider(TorrentProvider):
                              response.text) or re.search('Username or password is incorrect. If you have an account '
                                                          'here please use the recovery system or try again.',
                                                          response.text):
-                    logger.log('Invalid username or password. Check your settings', logger.WARNING)
+                    log.warning('Invalid username or password. Check your settings')
 
                     return False
 
     def _check_auth(self):
 
         if not self.username or not self.password:
-            logger.log('Invalid username or password. Check your settings', logger.WARNING)
+            log.warning('Invalid username or password. Check your settings')
 
         return True
 
