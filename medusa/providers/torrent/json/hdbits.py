32293f10f7a743604d308b8b9389ec11a6499323
Labrys <Labrys.git@gmail.com>
2017-05-07 12:56:07 -0400

    Flake, fix logging

--- medusa/providers/torrent/json/hdbits.py
+++ medusa/providers/torrent/json/hdbits.py
@@ -1,20 +1,7 @@
 # coding=utf-8
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
+
 """Provider code for HDBits."""
+
 from __future__ import unicode_literals
 
 import json
@@ -29,6 +16,7 @@ from requests.compat import urlencode, urljoin
 
 log = BraceAdapter(logging.getLogger(__name__))
 log.logger.addHandler(logging.NullHandler())
+
 
 class HDBitsProvider(TorrentProvider):
     """HDBits Torrent provider."""
@@ -58,23 +46,24 @@ class HDBitsProvider(TorrentProvider):
         self.minleech = None
 
         # Cache
-        self.cache = tv.Cache(self, min_time=10)  # only poll HDBits every 10 minutes max
+        self.cache = tv.Cache(self, min_time=10)  # Only poll HDBits every 10 minutes max
 
     def search(self, search_strings, age=0, ep_obj=None):
         """
         Search a provider and parse the results.
-        :param search_strings: A dict with mode (key) and the search value (value)
+
+        :param search_strings: A dict with {mode: search value}
         :param age: Not used
         :param ep_obj: Not used
         :returns: A list of search results (structure)
         """
-
         results = []
 
         log.debug('Search strings {0}', search_strings)
 
         self._check_auth()
 
+        # Search Params
         post_data = {
             'username': self.username,
             'passkey': self.passkey,
@@ -108,8 +97,10 @@ class HDBitsProvider(TorrentProvider):
     def parse(self, data, mode):
         """
         Parse search results for items.
+
         :param data: The raw response from a search
         :param mode: The current mode used to search, e.g. RSS
+
         :return: A list of items found
         """
         items = []
@@ -119,11 +110,9 @@ class HDBitsProvider(TorrentProvider):
             log.error('Resulting JSON from provider isn\'t correct, not parsing it')
 
         for row in torrent_rows:
-
             title = row.get('name', '')
             torrent_id = row.get('id', '')
             download_url = self.urls['download'].format(urlencode({'id': torrent_id, 'passkey': self.passkey}))
-
             if not all([title, download_url]):
                 continue
             seeders = row.get('seeders', 1)
@@ -145,7 +134,14 @@ class HDBitsProvider(TorrentProvider):
                 'leechers': leechers,
                 'pubdate': pubdate,
             }
-            log.debug('Found result: {title} with {x} seeders and {y} leechers', {'title': title}, {'x': seeders}, {'y': leechers})
+            log.debug(
+                'Found result: {title} with {x} seeders'
+                ' and {y} leechers', {
+                    'title': title,
+                    'x': seeders,
+                    'y': leechers
+                }
+            )
 
             items.append(item)
 
@@ -156,6 +152,7 @@ class HDBitsProvider(TorrentProvider):
         if not self.username or not self.passkey:
             log.warning('Your authentication credentials for {provider} are missing, check your config.', {'provider': self.name})
             return False
+
         return True
 
     def _check_auth_from_data(self, parsed_json):
