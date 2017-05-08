466086d51a471cf67297043fcd57a9028dc949ef
P0psicles <rogier@headshots.nl>
2017-05-08 22:01:22 +0200

    Docstring flake stuff.

--- medusa/server/web/core/token.py
+++ medusa/server/web/core/token.py
@@ -1,20 +1,21 @@
 # coding=utf-8
-
+"""Handle the requests fro /token."""
 from __future__ import unicode_literals
 
 from tornado.web import authenticated
 
-from ..core.base import BaseHandler
 from medusa import app
+from medusa.server.web.core.base import BaseHandler
 
 
 class TokenHandler(BaseHandler):
-    """
-    Handle the request for /token, and return the app.API_KEY if authenticated.
-    """
+    """Handle the request for /token, and return the app.API_KEY if authenticated."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize token handler."""
         super(TokenHandler, self).__init__(*args, **kwargs)
 
     @authenticated
     def get(self, *args, **kwargs):
+        """Return the app.API_KEY for /token get requests."""
         self.finish({'token': app.API_KEY})
