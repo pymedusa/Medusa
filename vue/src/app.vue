f0a8a91fb8d29ecfe7bc750256e7bd553e992331
Alexis Tyler <xo@wvvw.me>
2017-05-09 14:02:18 +0930

    add loader, allow stage-2 babel and fixup watchify

--- vue/src/app.vue
+++ vue/src/app.vue
@@ -1,12 +1,18 @@
 <template>
     <div id="app">
+        <loader type="square"></loader>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import loader from './components/loader.vue';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        loader
+    }
 };
 </script>
 
