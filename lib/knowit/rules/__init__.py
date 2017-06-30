d4b50743feb79d7cb8837236b85d2cd6d781b793
Dario <dariovizz@hotmail.it>
2017-06-30 18:07:20 +0200

    Remove knowit and appdirs from lib folder

--- lib/knowit/rules/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from .audio import AudioChannelsRule
-from .audio import AudioCodecRule
-from .language import LanguageRule
-from .subtitle import ClosedCaptionRule
-from .subtitle import HearingImpairedRule
-from .video import ResolutionRule
--- lib/knowit/rules/audio/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from .channels import AudioChannelsRule
-from .codec import AudioCodecRule
