0250ed7970e6c99f964f1f998d73f21c8412ea3d
P0psicles <rogier@headshots.nl>
2017-07-09 11:43:21 +0200

    Switched newpct to use session instead of get_url.

--- medusa/providers/torrent/html/newpct.py
+++ medusa/providers/torrent/html/newpct.py
@@ -89,7 +89,7 @@ class NewpctProvider(TorrentProvider):
                               {'search': search_string})
 
                 search_params['q'] = search_string
-                response = self.get_url(self.urls['search'], params=search_params)
+                response = self.session.get(self.urls['search'], params=search_params)
                 if not response or not response.text:
                     log.debug('No data returned from provider')
                     continue
