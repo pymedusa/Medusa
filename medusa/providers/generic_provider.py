fe65a534c42845bc6e3749203e31736d193d563d
Labrys <Labrys.git@gmail.com>
2017-09-17 10:40:03 -0400

    Fix double mis-conversion for latin1 http://www.i18nqa.com/debug/bug-double-conversion.html http://www.i18nqa.com/debug/utf8-debug.html

--- medusa/providers/generic_provider.py
+++ medusa/providers/generic_provider.py
@@ -451,11 +451,25 @@ class GenericProvider(object):
     @staticmethod
     def get_url_hook(response, **kwargs):
         """Get URL hook."""
-        log.debug('{0} URL: {1} [Status: {2}]', response.request.method, response.request.url, response.status_code)
+        request = response.request
+        log.debug('{0} URL: {1} [Status: {2}]', request.method, request.url, response.status_code)
 
-        if response.request.method == 'POST':
-            data = response.request.body.decode('utf-8')
-            log.debug('With post data: {0}', data)
+        if request.method == 'POST':
+            body = request.body
+            # try to log post data using various codecs to decode
+            codecs = ('utf-8', 'latin1', 'cp1252')
+            for codec in codecs:
+                try:
+                    data = body.decode(codec)
+                except UnicodeError as error:
+                    log.debug('Failed to decode post data as {codec}: {msg}',
+                              codec=codec, msg=error)
+                else:
+                    log.debug('With post data: {0}', data)
+                    break
+            else:
+                log.warning('Failed to decode post data with {codecs}',
+                            codecs=codecs)
 
     def get_url(self, url, post_data=None, params=None, timeout=30, **kwargs):
         """Load the given URL."""
