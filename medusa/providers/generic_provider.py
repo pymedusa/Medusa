d48fb4a88d6b8cb360c67f2dc579f1ec79ad5639
Dario <dariovizz@hotmail.it>
2017-10-19 12:01:31 +0200

    Add dayfirst and yearfirst to parse_pubdate, add tests

--- medusa/providers/generic_provider.py
+++ medusa/providers/generic_provider.py
@@ -531,7 +531,7 @@ class GenericProvider(object):
         return []
 
     @staticmethod
-    def parse_pubdate(pubdate, human_time=False, timezone=None):
+    def parse_pubdate(pubdate, human_time=False, timezone=None, **kwargs):
         """
         Parse publishing date into a datetime object.
 
@@ -539,9 +539,15 @@ class GenericProvider(object):
         :param human_time: string uses human slang ("4 hours ago")
         :param timezone: use a different timezone ("US/Eastern")
 
+        :keyword dayfirst: Interpret the first value as the day
+        :keyword yearfirst: Interpret the first value as the year
+
         :returns: a datetime object or None
         """
         now_alias = ('right now', 'just now', 'now')
+
+        df = kwargs.pop('dayfirst', False)
+        yf = kwargs.pop('yearfirst', False)
 
         # This can happen from time to time
         if pubdate is None:
@@ -557,7 +563,7 @@ class GenericProvider(object):
                     seconds = parse(match.group('time'))
                 return datetime.now(tz.tzlocal()) - timedelta(seconds=seconds)
 
-            dt = parser.parse(pubdate, fuzzy=True)
+            dt = parser.parse(pubdate, dayfirst=df, yearfirst=yf, fuzzy=True)
             # Always make UTC aware if naive
             if dt.tzinfo is None or dt.tzinfo.utcoffset(dt) is None:
                 dt = dt.replace(tzinfo=tz.gettz('UTC'))
