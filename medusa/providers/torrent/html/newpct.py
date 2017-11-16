8a7063f27cb44898f6e14612b9a7997022d298b6
Dario <dariovizz@hotmail.it>
2017-11-16 21:00:08 +0100

    flake

--- medusa/providers/torrent/html/newpct.py
+++ medusa/providers/torrent/html/newpct.py
@@ -6,8 +6,8 @@ from __future__ import unicode_literals
 
 import logging
 import re
-
 from collections import OrderedDict
+
 from medusa import tv
 from medusa.bs4_parser import BS4Parser
 from medusa.helper.common import convert_size, try_int
