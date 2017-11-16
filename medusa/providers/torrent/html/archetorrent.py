803426567198ea3bf57424cfcee970a940503185
adaur <adaur.underground@gmail.com>
2017-11-16 15:55:36 -0500

    Review requested

--- medusa/providers/torrent/html/archetorrent.py
+++ medusa/providers/torrent/html/archetorrent.py
@@ -11,7 +11,6 @@ from medusa import tv
 from medusa.bs4_parser import BS4Parser
 from medusa.helper.common import (
     convert_size,
-    try_int,
 )
 from medusa.logger.adapters.style import BraceAdapter
 from medusa.providers.torrent.torrent_provider import TorrentProvider
