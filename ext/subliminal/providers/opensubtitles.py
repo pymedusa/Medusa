cb73fce7e722df41297ff9e17dc10759347d68e9
Fernando <fernandog@users.noreply.github.com>
2017-11-25 17:46:10 -0200

    Sync with latest subliminal (#3313)
    * Sync with latest subliminal
    
    LegendasTV:
    - Check if UNRAR is installed
    - Don't discard multi episode archive
    
    Opensubtitles:
    - Use season and episode while imdb search
    
    * Newer

--- ext/subliminal/providers/opensubtitles.py
+++ ext/subliminal/providers/opensubtitles.py
@@ -27,7 +27,8 @@ class OpenSubtitlesSubtitle(Subtitle):
 
     def __init__(self, language, hearing_impaired, page_link, subtitle_id, matched_by, movie_kind, hash, movie_name,
                  movie_release_name, movie_year, movie_imdb_id, series_season, series_episode, filename, encoding):
-        super(OpenSubtitlesSubtitle, self).__init__(language, hearing_impaired, page_link, encoding)
+        super(OpenSubtitlesSubtitle, self).__init__(language, hearing_impaired=hearing_impaired,
+                                                    page_link=page_link, encoding=encoding)
         self.subtitle_id = subtitle_id
         self.matched_by = matched_by
         self.movie_kind = movie_kind
@@ -158,7 +159,10 @@ class OpenSubtitlesProvider(Provider):
         if hash and size:
             criteria.append({'moviehash': hash, 'moviebytesize': str(size)})
         if imdb_id:
-            criteria.append({'imdbid': imdb_id[2:]})
+            if season and episode:
+                criteria.append({'imdbid': imdb_id[2:], 'season': season, 'episode': episode})
+            else:
+                criteria.append({'imdbid': imdb_id[2:]})
         if tag:
             criteria.append({'tag': tag})
         if query and season and episode:
