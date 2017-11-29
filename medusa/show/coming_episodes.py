84321a5053655c1dfd34b86b749816aec26498cb
Dario <medariox@users.noreply.github.com>
2017-11-29 22:01:57 +0100

    Update coming_episodes.py

--- medusa/show/coming_episodes.py
+++ medusa/show/coming_episodes.py
@@ -1,8 +1,6 @@
 # coding=utf-8
 # This file is part of Medusa.
 #
-
-#
 # Medusa is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
