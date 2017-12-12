b4a08fdffd12f235d1e9d697110d75b9f34d4c54
supergonkas <supergonkas@gmail.com>
2017-12-12 19:11:01 +0000

    Add details url to iptorrents

--- medusa/providers/torrent/html/iptorrents.py
+++ medusa/providers/torrent/html/iptorrents.py
@@ -114,10 +114,10 @@ class IPTorrentsProvider(TorrentProvider):
             for row in torrents[1:]:
                 try:
                     title = row('td')[1].find('a').text
-                    download_url = self.urls['base_url'] + row('td')[3].find('a')['href']
+                    download_url = urljoin(self.url, row('td')[3].find('a')['href'])
                     if not all([title, download_url]):
                         continue
-
+                    detail_url = urljoin(self.url, row('td')[1].find('a')['href'])
                     seeders = int(row.find('td', attrs={'class': 'ac t_seeders'}).text)
                     leechers = int(row.find('td', attrs={'class': 'ac t_leechers'}).text)
 
@@ -139,6 +139,7 @@ class IPTorrentsProvider(TorrentProvider):
                         'seeders': seeders,
                         'leechers': leechers,
                         'pubdate': None,
+                        'detail_url': detail_url,
                     }
                     if mode != 'RSS':
                         log.debug('Found result: {0} with {1} seeders and {2} leechers',
