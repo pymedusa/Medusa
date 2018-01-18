87cbe756d12cc4a635cc47d8fd7bc3d26acbf908
Dario <medariox@users.noreply.github.com>
2018-01-18 09:25:06 +0100

    Fix thumbnails not being generated on post-processing. Fixes #3539 (#3622)
    * Fix thumbnails not being generated on post-processing. Fixes #3539
    
    * Indent sql
    
    * Fix the query for multi-indexer use.

--- medusa/tv/episode.py
+++ medusa/tv/episode.py
@@ -660,6 +660,35 @@ class Episode(TV):
             self.reset_dirty()
             return True
 
+    def set_indexer_data(self, season=None, indexer_api=None):
+        """Set episode information from indexer.
+
+        :param season:
+        :param indexer_api:
+        :rtype: bool
+        """
+        if season is None:
+            season = self.season
+
+        if indexer_api is None:
+            api = self.series.indexer_api
+        else:
+            api = indexer_api
+
+        try:
+            api._get_episodes(self.series.series_id, aired_season=season)
+        except IndexerError as error:
+            log.warning(
+                '{id}: {indexer} threw up an error: {error_msg}', {
+                    'id': self.series.series_id,
+                    'indexer': indexerApi(self.indexer).name,
+                    'error_msg': ex(error),
+                }
+            )
+            return False
+
+        return True
+
     def load_from_indexer(self, season=None, episode=None, tvapi=None, cached_season=None):
         """Load episode information from indexer.
 
