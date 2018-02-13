019163a7f6ce0408d3985adc6a2413e875156d68
Labrys <Labrys.git@gmail.com>
2018-02-12 22:30:35 -0500

    Refactor

--- medusa/torrent_checker.py
+++ medusa/torrent_checker.py
@@ -1,28 +1,14 @@
-# coding=utf-8
-# Author: fernandog
-#
-# This file is part of Medusa.
-#
-# Medusa is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Medusa is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Medusa. If not, see <http://www.gnu.org/licenses/>.
 """Torrent checker module."""
 
 import logging
 
 from medusa import app
 from medusa.clients import torrent
+from medusa.logger.adapters.style import BraceAdapter
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+log = BraceAdapter(log)
 
 
 class TorrentChecker(object):
@@ -42,7 +28,7 @@ class TorrentChecker(object):
         try:
             client = torrent.get_client_class(app.TORRENT_METHOD)()
             client.remove_ratio_reached()
-        except Exception as e:
-            logger.debug('Failed to check torrent status. Error: {error}', error=e)
+        except Exception as error:
+            log.debug('Failed to check torrent status. Error: {0}', error)
 
         self.amActive = False
