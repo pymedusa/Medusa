e9a49d5c9fdd2c9df3ea4cb005deb33e261e5a5c
Labrys <Labrys.git@gmail.com>
2018-02-14 19:55:40 -0500

    Refactor

--- medusa/exception_handler.py
+++ medusa/exception_handler.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 
 import logging
 
+from medusa.logger.adapters.style import BraceAdapter
+
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
+log = BraceAdapter(log)
 
 
 def handle(err, message='', *args, **kwargs):
@@ -18,10 +21,14 @@ def handle(err, message='', *args, **kwargs):
     m = message.format(*args, **kwargs) + ': ' if message else ''
     if isinstance(err, EnvironmentError):
         if err.errno == 28:
-            log.warning('{m}Out of disk space: {error_msg}', {'m': m, 'error_msg': err})
+            log.warning('{m}Out of disk space: {error_msg}',
+                        {'m': m, 'error_msg': err})
         elif err.errno == 13:
-            log.warning('{m}Permission denied: {error_msg}', {'m': m, 'error_msg': err})
+            log.warning('{m}Permission denied: {error_msg}',
+                        {'m': m, 'error_msg': err})
         else:
-            log.exception('{m}Environment error: {error_msg}', {'m': m, 'error_msg': err})
+            log.exception('{m}Environment error: {error_msg}',
+                          {'m': m, 'error_msg': err})
     else:
-        log.exception('{m}Exception generated: {error_msg}', {'m': m, 'error_msg': err})
+        log.exception('{m}Exception generated: {error_msg}',
+                      {'m': m, 'error_msg': err})
