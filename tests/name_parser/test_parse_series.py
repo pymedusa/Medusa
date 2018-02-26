c6cee9b4331597267df6e4f4c148e70e7352ada4
P0psicles <rogier@headshots.nl>
2018-02-26 13:16:24 +0100

    Added test for _parse_series. Using the series: `Regular Show`

--- tests/name_parser/test_parse_series.py
+++ tests/name_parser/test_parse_series.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 """Tests for medusa/test_list_associated_files.py."""
 
-from medusa import helpers
 import medusa.scene_numbering
 import medusa.scene_exceptions
 from medusa.name_parser.parser import NameParser
@@ -10,59 +9,18 @@ import pytest
 
 
 @pytest.mark.parametrize('p', [
+    # The regular Show uses xem data. To map scene S06E29 to indexer S06E28
     {
-        'name': u'[HorribleSubs] Cardcaptor Sakura Clear Card - 08 [720p].mkv',
+        'name': u'Regular.Show.S06E29.Dumped.at.the.Altar.720p.HDTV.x264-W4F',
         'indexer_id': 1,
-        'indexer': 70668,
-        'indexer_ep_season': 4,
-        'indexer_ep_episodes': 8,
-        'indexer_incorrect_absolute_ep_season': 1,
-        'indexer_incorrect_absolute_ep_episodes': 8,
+        'indexer': 188401,
         'mocks': {
-            'get_scene_exceptions_by_name': [(70668, 2, 1)],
-            'get_indexer_absolute_numbering': 78,
-            'get_all_episodes_from_absolute_number': (4, [8])
+            'get_indexer_numbering': (6, 28),
         },
         'series_info':{
-            'name': u'Cardcaptor Sakura'
+            'name': u'Regular.Show'
         },
-        'expected': ([8], [4], [78]),
-    },
-    {
-        'name': u'[BakedFish].Cardcaptor.Sakura:.Clear.Card-hen.-.08.[720p][AAC]',
-        'indexer_id': 1,
-        'indexer': 70668,
-        'indexer_ep_season': 4,
-        'indexer_ep_episodes': 8,
-        'indexer_incorrect_absolute_ep_season': 1,
-        'indexer_incorrect_absolute_ep_episodes': 8,
-        'mocks': {
-            'get_scene_exceptions_by_name': [(70668, 2, 1)],
-            'get_indexer_absolute_numbering': 78,
-            'get_all_episodes_from_absolute_number': (4, [8])
-        },
-        'series_info': {
-            'name': u'Cardcaptor Sakura'
-        },
-        'expected': ([8], [4], [78]),
-    },
-    {
-        'name': u'(Hi10).Cardcaptor.Sakura.-.08.(BD.1080p).(deanzel).(DualA).(4410B9A7)',
-        'indexer_id': 1,
-        'indexer': 70668,
-        'indexer_ep_season': 4,
-        'indexer_ep_episodes': 8,
-        'indexer_incorrect_absolute_ep_season': 1,
-        'indexer_incorrect_absolute_ep_episodes': 8,
-        'mocks': {
-            'get_scene_exceptions_by_name': [(None, None, None)],
-            'get_indexer_absolute_numbering': 8,
-            'get_all_episodes_from_absolute_number': (1, [8])
-        },
-        'series_info': {
-            'name': u'Cardcaptor Sakura'
-        },
-        'expected': ([8], [1], [8]),
+        'expected': ([28], [6], []),
     },
 ])
 def test_series_parsing(p, monkeypatch, create_tvshow):
@@ -72,24 +30,10 @@ def test_series_parsing(p, monkeypatch, create_tvshow):
     def mock_get_indexer_numbering():
         return p['mocks']['get_indexer_numbering']
 
-    # Series
-    # a = helpers.get_absolute_number_from_season_and_episode(result.series, season, episode)
-    def mock_get_absolute_number_from_season_and_episode():
-        return p['mocks']['get_absolute_number_from_season_and_episode']
-
-    def mock_get_show():
-        return p['mocks']['get_show']
-
     monkeypatch.setattr(
         medusa.scene_numbering,
         'get_indexer_numbering',
         mock_get_indexer_numbering
-    )
-
-    monkeypatch.setattr(
-        helpers,
-        'get_absolute_number_from_season_and_episode',
-        mock_get_absolute_number_from_season_and_episode
     )
 
     parser = NameParser()
