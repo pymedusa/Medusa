73b2733628ff91ceed031f6a38315b710294077b
P0psicles <rogier@headshots.nl>
2018-02-28 18:26:06 +0100

    Renamed air_by_date test case, as that still needs to be implemented.

--- tests/name_parser/parse_air_by_date.py
+++ tests/name_parser/parse_air_by_date.py
@@ -11,28 +11,11 @@ import pytest
 
 # FIXME: Create meaningful test parameters for air by date shows.
 @pytest.mark.parametrize('p', [
-    {
-        'name': u'[HorribleSubs] Cardcaptor Sakura Clear Card - 08 [720p].mkv',
-        'indexer_id': 1,
-        'indexer': 70668,
-        'indexer_ep_season': 4,
-        'indexer_ep_episodes': 8,
-        'indexer_incorrect_absolute_ep_season': 1,
-        'indexer_incorrect_absolute_ep_episodes': 8,
-        'mocks': [
-            ('scene_numbering.get_indexer_numbering', None),
-        ],
-        'series_info':{
-            'name': u'Cardcaptor Sakura'
-        },
-        'expected': ([8], [4], [78]),
-    },
-
 ])
 def test_air_by_date_parsing(p, monkeypatch_function_return, create_tvshow):
     monkeypatch_function_return(p['mocks'])
     parser = NameParser()
-    guess = guessit.guessit(p['name'], dict(show_type='anime'))
+    guess = guessit.guessit(p['name'])
     result = parser.to_parse_result(p['name'], guess)
 
     # confirm passed in show object indexer id matches result show object indexer id
