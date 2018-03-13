55bbce2a05808f4af562b0ee7072c2f4e49c0ecb
Kriskras99 <Kriskras99@users.noreply.github.com>
2018-03-13 12:19:50 +0100

    Add CinemaZ provider (#3856)
    * Improve PrivateHD provider
    
    * Add CinemaZ provider
    
    * Add CinemaZ icons to default themes
    
    * Remove unnecessary if-statement
    
    * Style fixes for PrivateHD and CinemaZ
    
    * Remove unnecessary double check in PrivateHD and CinemaZ

--- medusa/providers/torrent/html/cinemaz.py
+++ medusa/providers/torrent/html/cinemaz.py
@@ -1,19 +1,15 @@
 # coding=utf-8
 
-"""Provider code for HDSpace."""
+"""Provider code for CinemaZ."""
 
 from __future__ import unicode_literals
 
 import logging
-import re
-import traceback
 
 from medusa import tv
 from medusa.bs4_parser import BS4Parser
-from medusa.helper.common import (
-    convert_size,
-    try_int,
-)
+from medusa.helper.common import convert_size
+from medusa.helper.exceptions import AuthException
 from medusa.logger.adapters.style import BraceAdapter
 from medusa.providers.torrent.torrent_provider import TorrentProvider
 
@@ -24,34 +20,34 @@ log = BraceAdapter(logging.getLogger(__name__))
 log.logger.addHandler(logging.NullHandler())
 
 
-class HDSpaceProvider(TorrentProvider):
-    """HDSpace Torrent provider."""
+class CinemaZProvider(TorrentProvider):
+    """CinemaZ Torrent provider."""
 
     def __init__(self):
         """Initialize the class."""
-        super(HDSpaceProvider, self).__init__('HDSpace')
+        super(CinemaZProvider, self).__init__('CinemaZ')
 
         # Credentials
         self.username = None
         self.password = None
 
         # URLs
-        self.url = 'https://hd-space.org'
+        self.url = 'https://cinemaz.to'
         self.urls = {
-            'login': urljoin(self.url, 'index.php?page=login'),
-            'search': urljoin(self.url, 'index.php'),
+            'login': urljoin(self.url, 'auth/login'),
+            'search': urljoin(self.url, 'torrents'),
         }
 
         # Proper Strings
+        self.proper_strings = ['PROPER', 'REPACK', 'REAL', 'RERIP']
 
         # Miscellaneous Options
+        self.freeleech = False
 
         # Torrent Stats
-        self.minseed = None
-        self.minleech = None
 
         # Cache
-        self.cache = tv.Cache(self, min_time=10)  # only poll HDSpace every 10 minutes max
+        self.cache = tv.Cache(self)  # only poll CinemaZ every 10 minutes max
 
     def search(self, search_strings, age=0, ep_obj=None, **kwargs):
         """
@@ -66,40 +62,31 @@ class HDSpaceProvider(TorrentProvider):
         if not self.login():
             return results
 
-        # Search Params
-        search_params = {
-            'page': 'torrents',
-            'search': '',
-            'active': 0,
-            'options': 0,
-            'category': '15;40;21;22;24;25;27;28',
-        }
-
         for mode in search_strings:
             log.debug('Search mode: {0}', mode)
 
             for search_string in search_strings[mode]:
-
                 if mode != 'RSS':
                     log.debug('Search string: {search}',
                               {'search': search_string})
-                    search_params['search'] = search_string
+
+                search_params = {
+                    'in': 1,
+                    'search': search_string,
+                    'type': 2,
+                    'discount[]': 1 if self.freeleech else None,
+                    'tv_type[]': {'episode': 1, 'season': 2}.get(mode.lower())
+                }
+
+                if not search_string:
+                    del search_params['search']
 
                 response = self.session.get(self.urls['search'], params=search_params)
-                if not response or not response.text or 'please try later' in response.text:
+                if not response or not response.text:
                     log.debug('No data returned from provider')
                     continue
 
-                # Search result page contains some invalid html that prevents html parser from returning all data.
-                # We cut everything before the table that contains the data we are interested in thus eliminating
-                # the invalid html portions
-                try:
-                    index = response.text.index('<div id="information"')
-                except ValueError:
-                    log.debug('Could not find main torrent table')
-                    continue
-
-                results += self.parse(response.text[index:], mode)
+                results += self.parse(response.text, mode)
 
         return results
 
@@ -115,11 +102,8 @@ class HDSpaceProvider(TorrentProvider):
         items = []
 
         with BS4Parser(data, 'html5lib') as html:
-            if not html:
-                log.debug('No html data parsed from provider')
-                return items
-
             torrents = html('tr')
+
             if not torrents or len(torrents) < 2:
                 log.debug('Data returned from provider does not contain any torrents')
                 return items
@@ -131,33 +115,12 @@ class HDSpaceProvider(TorrentProvider):
                     continue
 
                 try:
-                    comments_counter = row.find_all('td', class_='lista', attrs={'align': 'center'})[4].find('a')
-                    if comments_counter:
-                        title = comments_counter['title'][10:]
-                    else:
-                        title = row.find('td', class_='lista', attrs={'align': 'left'}).find('a').get_text()
-                    dl_href = row.find('td', class_='lista', attrs={'width': '20',
-                                                                    'style': 'text-align: center;'}).find('a').get('href')
-                    download_url = urljoin(self.url, dl_href)
-                    if not all([title, dl_href]):
-                        continue
-
-                    seeders = try_int(row.find('span', class_='seedy').find('a').get_text(), 1)
-                    leechers = try_int(row.find('span', class_='leechy').find('a').get_text())
-
-                    # Filter unseeded torrent
-                    if seeders < min(self.minseed, 1):
-                        if mode != 'RSS':
-                            log.debug("Discarding torrent because it doesn't meet the"
-                                      " minimum seeders: {0}. Seeders: {1}",
-                                      title, seeders)
-                        continue
-
-                    torrent_size = row.find('td', class_='lista222', attrs={'width': '100%'}).get_text()
-                    size = convert_size(torrent_size) or -1
-
-                    pubdate_raw = row.find_all('td', class_='lista', attrs={'align': 'center'})[3].get_text()
-                    pubdate = self.parse_pubdate(pubdate_raw)
+                    title = row.find(class_='torrent-filename').get_text(strip=True)
+                    download_url = row.find(class_='torrent-download-icon').get('href')
+                    seeders = row.contents[13].get_text()
+                    leechers = row.contents[15].get_text()
+                    size = convert_size(row.contents[11].get_text(strip=True), default=-1)
+                    pubdate = self.parse_pubdate(row.contents[7].contents[1].get('title'))
 
                     item = {
                         'title': title,
@@ -172,8 +135,7 @@ class HDSpaceProvider(TorrentProvider):
 
                     items.append(item)
                 except (AttributeError, TypeError, KeyError, ValueError, IndexError):
-                    log.error('Failed parsing provider. Traceback: {0!r}',
-                              traceback.format_exc())
+                    log.exception('Failed parsing provider.')
 
         return items
 
@@ -185,10 +147,16 @@ class HDSpaceProvider(TorrentProvider):
         if 'pass' in dict_from_cookiejar(self.session.cookies):
             return True
 
+        login_html = self.session.get(self.urls['login']).get_text()
+        with BS4Parser(login_html, 'html5lib') as html:
+            token = html.find('input', attrs={'name': '_token'}).get('value')
+
         login_params = {
-            'uid': self.username,
-            'pwd': self.password,
-            'submit': 'Confirm',
+            '_token': token,
+            'email_username': self.username,
+            'password': self.password,
+            'remember': 1,
+            'submit': 'Login',
         }
 
         response = self.session.post(self.urls['login'], data=login_params)
@@ -196,7 +164,7 @@ class HDSpaceProvider(TorrentProvider):
             log.warning('Unable to connect to provider')
             return False
 
-        if re.search('Automatic log off after 15 minutes inactivity', response.text):
+        if 'These credentials do not match our records.' in response.text:
             log.warning('Invalid username or password. Check your settings')
             return False
 
@@ -205,9 +173,10 @@ class HDSpaceProvider(TorrentProvider):
     def _check_auth(self):
 
         if not self.username or not self.password:
-            log.warning('Invalid username or password. Check your settings')
+            raise AuthException('Your authentication credentials for {0} are missing,'
+                                ' check your config.'.format(self.name))
 
         return True
 
 
-provider = HDSpaceProvider()
+provider = CinemaZProvider()
