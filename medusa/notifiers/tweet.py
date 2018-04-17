400e4f08c7e70e6781383fd7c6f5bcaccd419f8b
sharkykh <sharkykh@gmail.com>
2018-04-18 02:30:40 +0300

    Fix Twitter DM

--- medusa/notifiers/tweet.py
+++ medusa/notifiers/tweet.py
@@ -138,7 +138,7 @@ class Notifier(object):
         api = twitter.Api(username, password, access_token_key, access_token_secret)
 
         try:
-            api.PostDirectMessage(dmdest, message.encode('utf8')[:139])
+            api.PostDirectMessage(message.encode('utf8')[:139], screen_name=dmdest)
         except Exception as error:
             log.error(u'Error Sending Tweet (DM): {!r}', error)
             return False
