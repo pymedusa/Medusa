86f88ce20995a98ed9d93b8739f9074b9e85e5e0
sharkykh <sharkykh@gmail.com>
2018-05-14 19:38:34 +0300

    Use spaces

--- themes/light/assets/js/vue-submit-form.js
+++ themes/light/assets/js/vue-submit-form.js
@@ -15,10 +15,10 @@ window.vueSubmitForm = async function(formId) {
     this.$http[method](path, { body: formData, redirect: 'follow' })
         .then(resp => {
             // If the new url differs from our current url and the form action - we've been redirected
-		    if (resp.url !== window.location.href && resp.url !== base + path) {
-		        window.location.href = resp.url;
-			} else if (redirect) {
-				window.location.href = base + redirect;
-			}
+            if (resp.url !== window.location.href && resp.url !== base + path) {
+                window.location.href = resp.url;
+            } else if (redirect) {
+                window.location.href = base + redirect;
+            }
         });
 };
