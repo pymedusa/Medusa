53b7fbfc8c2d1c93fca5ddedc58f150348ddd61f
sharkykh <sharkykh@gmail.com>
2018-06-03 00:25:03 +0300

    Make addShows/addNewShow return a redirection url instead of redirecting
    Backported from #4267 and tweaked

--- themes-default/slim/static/js/vue-submit-form.js
+++ themes-default/slim/static/js/vue-submit-form.js
@@ -14,10 +14,19 @@ window.vueSubmitForm = async function(formId) {
     // }), {});
     this.$http[method](path, { body: formData, redirect: 'follow' })
         .then(resp => {
-            // If the new url differs from our current url and the form action - we've been redirected
-            if (resp.url !== window.location.href && resp.url !== base + path) {
-                window.location.href = resp.url;
-            } else if (redirect) {
+            try {
+                const json = JSON.parse(resp.body);
+                const { redirect } = JSON.parse(resp.body);
+                if (redirect) {
+                    window.location.href = base + redirect;
+                    return;
+                }
+            } catch {
+                // Ignore
+            }
+
+            // Fallback
+            if (redirect) {
                 window.location.href = base + redirect;
             }
         });
