fe443f1d309bd1f0505860b989fee9ece47e4a55
sharkykh <sharkykh@gmail.com>
2018-06-10 01:19:47 +0300

    Test Quality.is_higher_quality
    and Quality.wanted_quality

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 """Tests for medusa.common.py."""
 
-from medusa.common import DOWNLOADED, Quality
+from medusa.common import Quality
+
 import pytest
 
 
@@ -209,3 +210,98 @@ class TestQuality(object):
 
         # Then
         assert expected == actual
+
+    @pytest.mark.parametrize('p', [
+        {  # p0 - No allowed / preferred
+            'current_quality': Quality.HDTV,
+            'new_quality': Quality.FULLHDTV,
+            'allowed_qualities': [],
+            'preferred_qualities': [],
+            'expected': None
+        },
+        {  # p1 - Allowed -> Allowed upgrade
+            'current_quality': Quality.HDTV,
+            'new_quality': Quality.FULLHDTV,
+            'allowed_qualities': [Quality.HDTV, Quality.FULLHDTV],
+            'preferred_qualities': [],
+            'expected': True
+        },
+        {  # p2 - Allowed -> Preferred upgrade
+            'current_quality': Quality.HDTV,
+            'new_quality': Quality.HDWEBDL,
+            'allowed_qualities': [Quality.HDTV],
+            'preferred_qualities': [Quality.HDWEBDL],
+            'expected': True
+        },
+        {  # p3 - Preferred -> Preferred upgrade
+            'current_quality': Quality.HDWEBDL,
+            'new_quality': Quality.FULLHDWEBDL,
+            'allowed_qualities': [],
+            'preferred_qualities': [Quality.HDWEBDL, Quality.FULLHDWEBDL],
+            'expected': True
+        },
+        {  # p4 - Allowed -> Allowed downgrade
+            'current_quality': Quality.FULLHDTV,
+            'new_quality': Quality.HDTV,
+            'allowed_qualities': [Quality.HDTV, Quality.FULLHDTV],
+            'preferred_qualities': [],
+            'expected': False
+        },
+        {  # p5 - Allowed -> Preferred downgrade
+            'current_quality': Quality.FULLHDTV,
+            'new_quality': Quality.HDTV,
+            'allowed_qualities': [Quality.FULLHDTV],
+            'preferred_qualities': [Quality.HDTV],
+            'expected': True
+        },
+        {  # p6 - Preferred -> Allowed downgrade
+            'current_quality': Quality.FULLHDWEBDL,
+            'new_quality': Quality.HDWEBDL,
+            'allowed_qualities': [Quality.HDWEBDL],
+            'preferred_qualities': [Quality.FULLHDWEBDL],
+            'expected': False
+        },
+        {  # p7 - Preferred -> Preferred downgrade
+            'current_quality': Quality.FULLHDWEBDL,
+            'new_quality': Quality.HDWEBDL,
+            'allowed_qualities': [],
+            'preferred_qualities': [Quality.HDWEBDL, Quality.FULLHDWEBDL],
+            'expected': False
+        },
+        {  # p8 - Current quality not in allowed
+            'current_quality': Quality.NA,
+            'new_quality': Quality.FULLHDTV,
+            'allowed_qualities': [Quality.FULLHDTV],
+            'preferred_qualities': [],
+            'expected': True
+        },
+        {  # p9 - Current quality not in preferred
+            'current_quality': Quality.NA,
+            'new_quality': Quality.FULLHDTV,
+            'allowed_qualities': [],
+            'preferred_qualities': [Quality.FULLHDTV],
+            'expected': True
+        },
+    ])
+    def test_is_higher_quality(self, p):
+        # Given
+        expected = p.pop('expected')
+        kwargs = p
+
+        # When
+        actual = Quality.is_higher_quality(**kwargs)
+
+        # Then
+        assert expected == actual
+
+    def test_wanted_quality(self):
+        # Given
+        quality = Quality.FULLHDWEBDL
+        allowed_qualities = [Quality.HDTV, Quality.HDWEBDL, Quality.HDBLURAY]
+        preferred_qualities = [Quality.FULLHDTV, Quality.FULLHDWEBDL, Quality.FULLHDBLURAY]
+
+        # When
+        actual = Quality.wanted_quality(quality, allowed_qualities, preferred_qualities)
+
+        # Then
+        assert actual is True
