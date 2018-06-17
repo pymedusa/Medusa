f0933b5fc34d02a7ba9c99da0db6251b27aa7643
Dario <medariox@users.noreply.github.com>
2018-06-17 17:11:28 +0200

    Backport changes from cpython shutil

--- lib/shutil_custom/__init__.py
+++ lib/shutil_custom/__init__.py
@@ -6,13 +6,8 @@ try:
 except ImportError:
     from shutil import Error as SameFileError
 
-try:
-    O_BINARY = os.O_BINARY  # Windows
-except AttributeError:
-    O_BINARY = 0
 
-
-BUFFER_SIZE = 128*1024
+BUFFER_SIZE = 1024 * 1024 if os.name == 'nt' else 16 * 1024
 
 
 def copyfile_custom(src, dst):
@@ -20,21 +15,22 @@ def copyfile_custom(src, dst):
     def special_file(fn):
         try:
             st = os.stat(fn)
-        except OSError:  # File most likely does not exist
+        except OSError:
+            # File most likely does not exist
             pass
         else:
             if stat.S_ISFIFO(st.st_mode):
-                raise SpecialFileError("`%s` is a named pipe" % fn)
+                raise SpecialFileError("{!r} is a named pipe".format(fn))
 
-    with open(src, 'rb') as fin, open(dst, 'wb') as fout:
+    with open(src, 'rb') as fsrc, open(dst, 'wb') as fdst:
         if _samefile(src, dst):
-            raise SameFileError("`%s` and `%s` are the same file" % (src, dst))
+            raise SameFileError("{!r} and {!r} are the same file".format(src, dst))
 
         special_file(src)
         special_file(dst)
 
-        while True:
-            data = fin.read(BUFFER_SIZE)
-            if data == '':
+        while 1:
+            buf = fsrc.read(BUFFER_SIZE)
+            if not buf:
                 break
-            fout.write(data)
+            fdst.write(buf)
