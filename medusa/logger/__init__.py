06deabffdc86e3419d4c94a02422896d4b39d26e
sharkykh <sharkykh@gmail.com>
2018-06-17 23:50:32 +0300

    Fix similar issues tests and empty issue titles (#4377)
    * Fix find_similar_issues logging and tests
    
    * Fix empty issue title and add testFixes titles like #4369
    
    * Add debug log to GithubException
    
    * Try to make issue submitter faster by increasing per_page to 100
    
    * Count and log number of issues
    
    * Review

--- medusa/logger/__init__.py
+++ medusa/logger/__init__.py
@@ -346,7 +346,10 @@ class LogLine(object):
     @property
     def issue_title(self):
         """Return the expected issue title for this logline."""
-        result = self.traceback_lines[-1] if self.traceback_lines else self.message
+        if self.traceback_lines:
+            result = next((line for line in reversed(self.traceback_lines) if line.strip()), self.message)
+        else:
+            result = self.message
         return result[:1000]
 
     def to_json(self):
