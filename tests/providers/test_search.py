65ec6ac27d4d7d82af87aed6e19280e2adb8c93c
sharkykh <sharkykh@gmail.com>
2018-06-28 15:38:45 +0300

    Fix test_search indentation

--- tests/providers/test_search.py
+++ tests/providers/test_search.py
@@ -39,26 +39,26 @@ def test_search_daily(providers, limit=3):
 
 def test_search_backlog(providers, limit=2):
 
-        # Given
-        for provider in providers:
+    # Given
+    for provider in providers:
 
-            # When
-            html = os.path.join(__location__, provider.type, provider.name,
-                                provider.name + '_backlog.yaml')
+        # When
+        html = os.path.join(__location__, provider.type, provider.name,
+                            provider.name + '_backlog.yaml')
 
-            with vcr.use_cassette(html):
-                actual = provider.klass.search(provider.data['backlog']['search_strings'])
+        with vcr.use_cassette(html):
+            actual = provider.klass.search(provider.data['backlog']['search_strings'])
 
-            for i, result in enumerate(actual):
-                # Only compare up to the info hash if we have magnets
-                if provider.data['backlog']['results'][i]['link'].startswith('magnet:'):
-                    result['link'] = result['link'][:60]
-                # Only verify that we got a datetime object for now
-                pubdate = provider.data['backlog']['results'][i]['pubdate']
-                if pubdate and isinstance(pubdate, datetime.datetime):
-                    result['pubdate'] = pubdate
+        for i, result in enumerate(actual):
+            # Only compare up to the info hash if we have magnets
+            if provider.data['backlog']['results'][i]['link'].startswith('magnet:'):
+                result['link'] = result['link'][:60]
+            # Only verify that we got a datetime object for now
+            pubdate = provider.data['backlog']['results'][i]['pubdate']
+            if pubdate and isinstance(pubdate, datetime.datetime):
+                result['pubdate'] = pubdate
 
-                assert result == provider.data['backlog']['results'][i]
+            assert result == provider.data['backlog']['results'][i]
 
-                if i + 1 == limit:
-                    break
+            if i + 1 == limit:
+                break
