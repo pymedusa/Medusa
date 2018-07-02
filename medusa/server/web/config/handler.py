d2c0cc7acc0e0a2c5783e13c4ada1f6aceaad149
Alexis Tyler <xo@wvvw.me>
2018-07-02 15:45:29 +0930

    remove unused code

--- medusa/server/web/config/handler.py
+++ medusa/server/web/config/handler.py
@@ -2,16 +2,7 @@
 
 """Base handler for Config pages."""
 
-from __future__ import unicode_literals
-
-import os
-
-from medusa import (
-    app,
-    db,
-)
 from medusa.server.web.core import PageTemplate, WebRoot
-from medusa.version_checker import CheckVersion
 
 from tornroutes import route
 
@@ -44,45 +35,9 @@ class Config(WebRoot):
 
     def index(self):
         """
-        Render the Help & Info page
+        Render the Help & Info page.
+
+        [Converted to VueRouter]
         """
         t = PageTemplate(rh=self, filename='index.mako')
-
-        try:
-            import pwd
-            app_user = pwd.getpwuid(os.getuid()).pw_name
-        except ImportError:
-            try:
-                import getpass
-                app_user = getpass.getuser()
-            except Exception:
-                app_user = 'Unknown'
-
-        try:
-            import locale
-            app_locale = locale.getdefaultlocale()
-        except Exception:
-            app_locale = 'Unknown', 'Unknown'
-
-        try:
-            import ssl
-            ssl_version = ssl.OPENSSL_VERSION
-        except Exception:
-            ssl_version = 'Unknown'
-
-        app_version = ''
-        if app.VERSION_NOTIFY:
-            updater = CheckVersion().updater
-            if updater:
-                app_version = updater.get_cur_version()
-
-        main_db_con = db.DBConnection()
-        cur_branch_major_db_version, cur_branch_minor_db_version = main_db_con.checkDBVersion()
-
-        return t.render(
-            submenu=self.ConfigMenu(), title='Medusa Configuration',
-            header='Medusa Configuration', topmenu='config',
-            app_user=app_user, app_locale=app_locale, ssl_version=ssl_version,
-            app_version=app_version, cur_branch_major_db_version=cur_branch_major_db_version,
-            cur_branch_minor_db_version=cur_branch_minor_db_version
-        )
+        return t.render(submenu=self.ConfigMenu(), topmenu='config')
