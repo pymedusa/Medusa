c55542c2affeb3dc446793a7ecc49e33ea8a8b36
Alexis Tyler <xo@wvvw.me>
2018-07-05 21:37:13 +0930

    add failing test for app-link to

--- themes-default/slim/test/specs/app-link.spec.js
+++ themes-default/slim/test/specs/app-link.spec.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import Puex from 'puex';
+import VueRouter from 'vue-router';
 import { createLocalVue, mount } from '@vue/test-utils';
 import fixtures from '../__fixtures__/app-link';
 
@@ -9,6 +10,7 @@ const AppLink = require('../../static/js/templates/app-link.vue');
 test.beforeEach(t => {
     t.context.localVue = createLocalVue();
     t.context.localVue.use(Puex);
+    t.context.localVue.use(VueRouter);
 
     const { state } = fixtures;
     t.context.state = state;
@@ -72,6 +74,31 @@ test('renders internal link', t => {
         propsData: {
             base,
             href: './config'
+        }
+    });
+
+    t.snapshot(wrapper.html());
+    t.is(wrapper.attributes().href, 'http://localhost:8081/config');
+    t.is(wrapper.attributes().target, '_self');
+    t.is(wrapper.attributes().rel, undefined);
+});
+
+test.failing('renders router-link from href string', t => {
+    const { base, localVue, store } = t.context;
+    const router = new VueRouter({
+        mode: 'history',
+        routes: [{
+            path: '/config',
+            name: 'config'
+        }]
+    });
+    const wrapper = mount(AppLink, {
+        localVue,
+        store,
+        router,
+        propsData: {
+            base,
+            to: 'config'
         }
     });
 
