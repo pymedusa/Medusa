343fdd0dccf4f123bb1d8aed3dac2a78632d98e9
Dario <dariovizz@hotmail.it>
2018-07-12 19:23:24 +0200

    Use standard episode identifier (s2e3) like APIv2

--- medusa/server/web/home/handler.py
+++ medusa/server/web/home/handler.py
@@ -947,7 +947,7 @@ class Home(WebRoot):
         for cur_result in sql_results:
             cur_ep_cat = series_obj.get_overview(cur_result[b'status'], cur_result[b'quality'], manually_searched=cur_result[b'manually_searched'])
             if cur_ep_cat:
-                ep_cats['{season}x{episode}'.format(season=cur_result[b'season'], episode=cur_result[b'episode'])] = cur_ep_cat
+                ep_cats['s{season}e{episode}'.format(season=cur_result[b'season'], episode=cur_result[b'episode'])] = cur_ep_cat
                 ep_counts[cur_ep_cat] += 1
 
         bwl = None
@@ -1349,8 +1349,8 @@ class Home(WebRoot):
             cur_ep_cat = series_obj.get_overview(cur_result[b'status'], cur_result[b'quality'],
                                                  manually_searched=cur_result[b'manually_searched'])
             if cur_ep_cat:
-                ep_cats['{season}x{episode}'.format(season=cur_result[b'season'],
-                                                    episode=cur_result[b'episode'])] = cur_ep_cat
+                ep_cats['s{season}e{episode}'.format(season=cur_result[b'season'],
+                                                     episode=cur_result[b'episode'])] = cur_ep_cat
                 ep_counts[cur_ep_cat] += 1
 
         return t.render(
