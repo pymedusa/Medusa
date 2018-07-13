75bf19895d17ec6c6163bd02fbf94d4755b21022
Alexis Tyler <xo@wvvw.me>
2018-07-13 17:14:08 +0930

    add app-link to app-header test

--- themes-default/slim/test/specs/app-header.spec.js
+++ themes-default/slim/test/specs/app-header.spec.js
@@ -5,12 +5,14 @@ import { createLocalVue, mount } from '@vue/test-utils';
 import fixtures from '../__fixtures__/app-header';
 
 // Needs to be required otherwise nyc won't see it
+const AppLink = require('../../static/js/templates/app-link.vue');
 const AppHeader = require('../../static/js/templates/app-header.vue');
 
 test.beforeEach(t => {
     t.context.localVue = createLocalVue();
     t.context.localVue.use(Puex);
     t.context.localVue.use(VueRouter);
+    t.context.localVue.component('app-link', AppLink);
 
     const { state } = fixtures;
     t.context.state = state;
@@ -18,13 +20,10 @@ test.beforeEach(t => {
 });
 
 test.failing('renders', t => {
-    const { localVue, stubs, store, state } = t.context;
+    const { localVue, store, state } = t.context;
     const wrapper = mount(AppHeader, {
         localVue,
-        stubs,
         store,
-        propsData: {
-        },
         computed: {
             config() {
                 return Object.assign(state.config, {
