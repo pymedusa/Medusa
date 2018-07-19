ea6a194ef90e69162656a2f96188fb71aa66b549
Alexis Tyler <xo@wvvw.me>
2018-07-19 11:28:19 +0930

    lint

--- themes-default/slim/webpack.config.js
+++ themes-default/slim/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.vue', '.json'],
         alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+            vue$: 'vue/dist/vue.esm.js'
         }
     }
 };
