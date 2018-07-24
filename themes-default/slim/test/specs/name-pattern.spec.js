22eb0926239f7a235ccc87b36cf9a928bc896af8
P0psicles <rogier@headshots.nl>
2018-07-24 14:07:19 +0200

    Fix name-pattern.spec.js snapshot test. Mock checkNaming and testNaming.

--- themes-default/slim/test/specs/name-pattern.spec.js
+++ themes-default/slim/test/specs/name-pattern.spec.js
@@ -12,20 +12,26 @@ test('renders', t => {
     const { localVue } = t.context;
     const wrapper = mount(NamePattern, {
         localVue,
-        propsData() {
-            return {
-                namingPattern: 'S%0SE%0E - %EN',
-                namingPresets: [
-                    '%SN - %Sx%0E - %EN',
-                    '%S.N.S%0SE%0E.%E.N',
-                    '%Sx%0E - %EN',
-                    'S%0SE%0E - %EN',
-                    'Season %0S/%S.N.S%0SE%0E.%Q.N-%RG'
-                ],
-                type: '',
-                multiEpStyle: 1,
-                animeNamingType: 0
-            };
+        propsData: {
+            namingPattern: 'S%0SE%0E - %EN',
+            namingPresets: [
+                '%SN - %Sx%0E - %EN',
+                '%S.N.S%0SE%0E.%E.N',
+                '%Sx%0E - %EN',
+                'S%0SE%0E - %EN',
+                'Season %0S/%S.N.S%0SE%0E.%Q.N-%RG'
+            ],
+            type: '',
+            multiEpStyle: 1,
+            animeNamingType: 0
+        },
+        methods: {
+            checkNaming(pattern, selectedMultiEpStyle, animeType) {
+                console.debug(`Mocking method checkNaming with params: ${pattern}, ${selectedMultiEpStyle} and ${animeType}`);
+            },
+            testNaming(pattern, selectedMultiEpStyle, animeType) {
+                console.debug(`Mocking method testNaming with params: ${pattern}, ${selectedMultiEpStyle} and ${animeType}`);
+            }
         }
     });
 
