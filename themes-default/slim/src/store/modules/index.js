84cdba9ad8d5d62b87a721ac7c35fc164483a093
sharkykh <sharkykh@gmail.com>
2018-08-06 20:30:44 +0300

    Split config sections to separate modules
    Also fixes qualities & statuses having incorrect states.

--- themes-default/slim/src/store/modules/index.js
+++ themes-default/slim/src/store/modules/index.js
@@ -3,11 +3,17 @@ import config from './config';
 import defaults from './defaults';
 import socket from './socket';
 import notifications from './notifications';
+import qualities from './qualities';
+import statuses from './statuses';
+import metadata from './metadata';
 
 export {
     auth,
     config,
     defaults,
     socket,
-    notifications
+    notifications,
+    qualities,
+    statuses,
+    metadata
 };
