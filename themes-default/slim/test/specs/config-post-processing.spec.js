f3ad21c818dc6f4ba8f8a5f9bbd6198769a5f09a
sharkykh <sharkykh@gmail.com>
2018-08-23 00:24:22 +0300

    Add render test for config-post-processing
    Stub all the components used

---
+++ themes-default/slim/test/specs/config-post-processing.spec.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import { createLocalVue, mount } from '@vue/test-utils';
+import { ConfigPostProcessing } from '../../src/components';
+import fixtures from '../__fixtures__/common';
+
+test.beforeEach(t => {
+    t.context.localVue = createLocalVue();
+    t.context.localVue.use(Vuex);
+    t.context.localVue.use(VueRouter);
+
+    const { state } = fixtures;
+    const { Store } = Vuex;
+    t.context.state = state;
+    t.context.store = new Store({ state });
+});
+
+test('renders', t => {
+    const { localVue, store } = t.context;
+    const wrapper = mount(ConfigPostProcessing, {
+        localVue,
+        store,
+        stubs: [
+            'app-link',
+            'file-browser',
+            'name-pattern',
+            'select-list',
+            'toggle-button'
+        ]
+    });
+
+    t.snapshot(wrapper.html());
+});
