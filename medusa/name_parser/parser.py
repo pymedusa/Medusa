8209a85901271ea64ed71554894c725bc3c7c314
Dario <dariovizz@hotmail.it>
2018-08-25 16:21:47 +0200

    Prevent Py3 exception: not supported between instances of 'NoneType' and 'int'

--- medusa/name_parser/parser.py
+++ medusa/name_parser/parser.py
@@ -199,7 +199,8 @@ class NameParser(object):
                 #
                 # Don't assume that scene_exceptions season is the same as indexer season.
                 # E.g.: [HorribleSubs] Cardcaptor Sakura Clear Card - 08 [720p].mkv thetvdb s04, thexem s02
-                if result.series.is_scene or (result.season_number is None and scene_season > 0):
+                if result.series.is_scene or (result.season_number is None and
+                                              scene_season is not None and scene_season > 0):
                     a = scene_numbering.get_indexer_absolute_numbering(
                         result.series, absolute_episode, True, scene_season
                     )
@@ -207,7 +208,7 @@ class NameParser(object):
                 # Translate the absolute episode number, back to the indexers season and episode.
                 (season, episode) = helpers.get_all_episodes_from_absolute_number(result.series, [a])
 
-                if result.season_number is None and scene_season > 0:
+                if result.season_number is None and scene_season is not None and scene_season > 0:
                     log.debug(
                         'Detected a season scene exception [{series_name} -> {scene_season}] without a '
                         'season number in the title, '
