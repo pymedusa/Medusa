c313b32947de71caa18dc1c876add67ad95a6d75
Diego <ngosang@hotmail.es>
2018-08-31 13:49:57 +0200

    Update lib unrar2 (python2 and python3 supported)

--- medusa/server/web/config/post_processing.py
+++ medusa/server/web/config/post_processing.py
@@ -184,7 +184,7 @@ class ConfigPostProcessing(Config):
         try:
             rar_path = os.path.join(app.PROG_DIR, 'lib', 'unrar2', 'test.rar')
             testing = RarFile(rar_path).read_files('*test.txt')
-            if testing[0][1] == 'This is only a test.':
+            if testing[0][1] == b'This is only a test.':
                 return 'supported'
             logger.log('Rar Not Supported: Can not read the content of test file', logger.ERROR)
             return 'not supported'
