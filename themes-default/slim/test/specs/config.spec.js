7d82f9ea25d6f1edbfc94475b6b38fb129f3b763
sharkykh <sharkykh@gmail.com>
2018-09-06 02:49:41 +0300

    Convert Post Processing Config to VueRouter (#4946)
    * Add missing config.vue render test
    
    * Convert mako to vue (no changes to Vue code)
    
    * Make config/postProcessing an SFC and add it to VueRouter
    
    * Add render test for config-post-processing
    
    Stub all the components used
    
    * Fix bug when saving empty Post Processing Dir
    
    `Config patch ignored {u'postProcessing': {u'showDownloadDir': None}}`
    
    * Fix style bugs
    
    * Fix tests / Update snapshot
    
    * Update changelog
    
    * Add tabs code
    
    * Flake8
    
    * Revert change to `save()`
    
    * More style fixes
    
    * Update config.vue snapshot

---
+++ themes-default/slim/test/specs/config.spec.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import { createLocalVue, mount } from '@vue/test-utils';
+import { Config } from '../../src/components';
+import fixtures from '../__fixtures__/common';
+
+test.beforeEach(t => {
+    t.context.localVue = createLocalVue();
+    t.context.localVue.use(Vuex);
+    t.context.localVue.use(VueRouter);
+
+    const { state } = fixtures;
+    const { Store } = Vuex;
+    t.context.state = state;
+    t.context.store = new Store({ state });
+});
+
+test('renders', t => {
+    const { localVue, store } = t.context;
+    const wrapper = mount(Config, {
+        localVue,
+        store
+    });
+
+    t.snapshot(wrapper.html());
+});
