c8f0e40d908480b487eebb5b1866801282a99e91
sharkykh <sharkykh@gmail.com>
2018-09-08 04:41:16 +0300

    Explain the code better

--- medusa/init/__init__.py
+++ medusa/init/__init__.py
@@ -47,13 +47,19 @@ def _ext_lib_location():
 
 
 def _configure_syspath():
-    # Note: Paths are inserted in reverse order (LIFO)
+    """Add the vendored libraries into `sys.path`."""
+    # Note: These paths will be inserted into `sys.path` in reverse order (LIFO)
+    # So the last path on this list will be inserted as the first path on `sys.path`
+    # right after the current working dir.
+    # For example: [ cwd, pathN, ..., path1, path0, <rest_of_sys.path> ]
+
     paths_to_insert = [
         _lib_location(),
         _ext_lib_location()
     ]
 
-    # Handle `.pth` files: https://bugs.python.org/issue7744
+    # Insert paths into `sys.path` and handle `.pth` files
+    # Inspired by: https://bugs.python.org/issue7744
     for dirpath in paths_to_insert:
         # Clear `sys.path`
         sys.path, remainder = sys.path[:1], sys.path[1:]
