358e7aaa64298be6713b3653fd5dec55d4bd7288
sharkykh <sharkykh@gmail.com>
2018-09-13 19:04:58 +0300

    Add tablesorter v2.31.0 to webpack

--- themes-default/slim/src/index.js
+++ themes-default/slim/src/index.js
@@ -1,7 +1,20 @@
+/* eslint-disable import/no-unassigned-import */
 import $ from 'jquery';
-import 'bootstrap'; // eslint-disable-line import/no-unassigned-import
-import 'bootstrap/dist/css/bootstrap.min.css'; // eslint-disable-line import/no-unassigned-import
-import '../vendor/css/open-sans.css'; // eslint-disable-line import/no-unassigned-import
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Include `tablesorter` with no widgets, except `zebra` which is included by default
+import 'tablesorter/dist/js/jquery.tablesorter';
+// The `storage` widget is required for saving filters and selected columns
+import 'tablesorter/dist/js/widgets/widget-storage.min';
+import 'tablesorter/dist/js/widgets/widget-columnSelector.min';
+import 'tablesorter/dist/js/widgets/widget-stickyHeaders.min';
+import 'tablesorter/dist/js/widgets/widget-saveSort.min';
+import 'tablesorter/dist/js/widgets/widget-filter.min';
+import 'tablesorter/dist/css/theme.blue.min.css';
+
+import '../vendor/css/open-sans.css';
+/* eslint-enable import/no-unassigned-import */
 
 import Vue from 'vue';
 import Vuex from 'vuex';
