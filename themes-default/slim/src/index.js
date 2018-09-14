0c6c75ff856406d9c1d5bd896d41e45c2404968c
sharkykh <sharkykh@gmail.com>
2018-09-14 20:53:57 +0300

    Move tablesorter imports to `./vendor/js/tablesorter.js`

--- themes-default/slim/src/index.js
+++ themes-default/slim/src/index.js
@@ -2,17 +2,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Include `tablesorter` with no widgets, except `zebra` which is included by default
-import 'tablesorter/dist/js/jquery.tablesorter';
-// The `storage` widget is required for saving filters and selected columns
-import 'tablesorter/dist/js/widgets/widget-storage.min';
-import 'tablesorter/dist/js/widgets/widget-columnSelector.min';
-import 'tablesorter/dist/js/widgets/widget-stickyHeaders.min';
-import 'tablesorter/dist/js/widgets/widget-saveSort.min';
-import 'tablesorter/dist/js/widgets/widget-filter.min';
-import 'tablesorter/dist/css/theme.blue.min.css';
-
+import '../vendor/js/tablesorter';
 import '../vendor/css/open-sans.css';
 /* eslint-enable import/no-unassigned-import */
 
