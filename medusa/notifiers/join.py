fde1b105a9e9c1677e6cd819aa9a2d07d58ce06e
ouldsmobile <ouldsmobile1@gmail.com>
2018-09-21 09:47:20 -0400

    add missing docstrings in join.py

--- medusa/notifiers/join.py
+++ medusa/notifiers/join.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+# Author: Kevin Ould email: ouldsmobile1@gmail.com
+# Thanks to the contributors of the other notifier providers
+# for inspiration and a base to start from
+""" Join Notifications """
 
 from __future__ import unicode_literals
 
@@ -17,12 +21,17 @@ log.logger.addHandler(logging.NullHandler())
 
 
 class Notifier(object):
+    """ Notifier class for Join """
 
     def __init__(self):
+        """ Init method."""
+
         self.session = MedusaSession()
         self.url = 'https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?'
 
     def test_notify(self, join_api, join_device):
+        """ Sends test notification from config screen """
+
         log.debug('Sending a test Join notification.')
         return self._sendjoin(
             join_api=join_api,
@@ -33,6 +42,8 @@ class Notifier(object):
         )
 
     def notify_snatch(self, ep_name, is_proper):
+        """ Send Join notification when nzb snatched if selected in config """
+
         if app.JOIN_NOTIFY_ONSNATCH:
             self._sendjoin(
                 join_api=None,
@@ -41,6 +52,7 @@ class Notifier(object):
             )
 
     def notify_download(self, ep_name):
+        """ Send Join notification when nzb download completed if selected in config """
         if app.JOIN_NOTIFY_ONDOWNLOAD:
             self._sendjoin(
                 join_api=None,
@@ -49,6 +61,7 @@ class Notifier(object):
             )
 
     def notify_subtitle_download(self, ep_name, lang):
+        """ Send Join notification when subtitles downloaded if selected in config """
         if app.JOIN_NOTIFY_ONSUBTITLEDOWNLOAD:
             self._sendjoin(
                 join_api=None,
@@ -57,6 +70,8 @@ class Notifier(object):
             )
 
     def notify_git_update(self, new_version='??'):
+        """ Send Join notification when new version available from git """
+
         self._sendjoin(
             join_api=None,
             event=common.notifyStrings[common.NOTIFY_GIT_UPDATE],
@@ -64,6 +79,8 @@ class Notifier(object):
         )
 
     def notify_login(self, ipaddress=''):
+        """ Send Join notification when login detected """
+
         self._sendjoin(
             join_api=None,
             event=common.notifyStrings[common.NOTIFY_LOGIN],
@@ -71,6 +88,8 @@ class Notifier(object):
         )
 
     def _sendjoin(self, join_api=None, join_device=None, event=None, message=None, force=False):
+        """ Function to compose and send Join notification """
+
         push_result = {'success': False, 'error': ''}
 
         if not (app.USE_JOIN or force):
