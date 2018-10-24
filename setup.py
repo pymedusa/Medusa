a33014e481f49a380c21b49cbe9833ef46868271
Dario <medariox@users.noreply.github.com>
2018-10-24 16:49:56 +0200

    Bump pycodestyle

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'flake8-import-order>=0.18',
         'flake8-quotes>=1.0.0',
         'pep8-naming>=0.7.0',
-        'pycodestyle==2.3.1',
+        'pycodestyle==2.4.0',
         'pytest>=3.8.0',
         'pytest-cov>=2.6.0',
         'pytest-flake8>=1.0.2',
