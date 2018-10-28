b6c3e4b4c8689676e39d0b978cbf9e7eb1d67cce
Alexis Tyler <xo@wvvw.me>
2018-10-29 07:46:48 +1030

    split notifiers into dir
    Signed-off-by: Alexis Tyler <xo@wvvw.me>

---
+++ themes-default/slim/src/store/modules/notifiers/kodi.js
@@ -0,0 +1,30 @@
+export const state = {
+    enabled: null,
+    alwaysOn: null,
+    libraryCleanPending: null,
+    cleanLibrary: null,
+    host: [],
+    username: null,
+    password: null,
+    notifyOnSnatch: null,
+    notifyOnDownload: null,
+    notifyOnSubtitleDownload: null,
+    update: {
+        library: null,
+        full: null,
+        onlyFirst: null
+    }
+};
+
+export const mutations = {};
+
+export const getters = {};
+
+export const actions = {};
+
+export default {
+    state,
+    mutations,
+    getters,
+    actions
+};
