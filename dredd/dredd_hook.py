3b95923455b797022d51c1e5463fb6fd991ada24
Dario <medariox@users.noreply.github.com>
2018-12-30 16:27:19 +0100

    Fix configparser import for Py2

--- dredd/dredd_hook.py
+++ dredd/dredd_hook.py
@@ -9,6 +9,7 @@ import os
 import sys
 
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../ext')))
+sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../ext2')))
 
 from collections import Mapping
 from configparser import ConfigParser
