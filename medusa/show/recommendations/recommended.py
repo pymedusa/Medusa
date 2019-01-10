cdb52cf308f00802dc721e9b111540c0232b26a7
Dario <medariox@users.noreply.github.com>
2019-01-10 16:55:36 +0100

    Fix recommended shows error. Fixes #5962

--- medusa/show/recommendations/recommended.py
+++ medusa/show/recommendations/recommended.py
@@ -221,22 +221,11 @@ def cached_get_imdb_series_details(imdb_id):
 
 
 def create_key_from_series(namespace, fn, **kw):
-    """Generate a key limiting the amount of dictionaries keys that are allowed to be used."""
-    def generate_key(*arg, **kwargs):
-        """
-        Generate the key.
-
-        The key is passed to the decorated function using the kwargs `storage_key`.
-        Following this standard we can cache every object, using this key_generator.
-        """
-        try:
-            if PY2:
-                return kwargs['storage_key'].encode('utf-8')
-            return kwargs['storage_key']
-        except KeyError:
-            log.exception('Make sure you pass kwargs parameter `storage_key` to configure the key,'
-                          ' that is used in the dogpile cache.')
-
+    def generate_key(*args, **kw):
+        show_key = namespace + '|' + args[1]
+        if PY2:
+            return show_key.encode('utf-8')
+        return show_key
     return generate_key
 
 
