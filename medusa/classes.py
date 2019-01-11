e8244d45fbc1b817370d904bf77565fc5ca8e65e
P0psicles <rogier@headshots.nl>
2019-01-11 19:52:48 +0100

    Not needed.

--- medusa/classes.py
+++ medusa/classes.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Medusa. If not, see <http://www.gnu.org/licenses/>.
 """Collection of generic used classes."""
-from __future__ import division, unicode_literals
+from __future__ import unicode_literals
 
 import logging
 
