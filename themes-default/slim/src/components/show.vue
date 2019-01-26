156b1d7c979c2c38071a7eda7ff3d2cc63c3009b
P0psicles <rogier@headshots.nl>
2019-01-26 17:00:01 +0100

    Added the showLoaded flag to prevernt duplicate calls.

--- themes-default/slim/src/components/show.vue
+++ themes-default/slim/src/components/show.vue
@@ -42,52 +42,6 @@ export default {
         return {
             showLoaded: false,
             jumpToSeason: 'jump',
-            // show: {
-            //     airs: null,
-            //     akas: null,
-            //     cache: null,
-            //     classification: null,
-            //     config: {
-            //         airByDate: null,
-            //         aliases: null,
-            //         anime: null,
-            //         defaultEpisodeStatus: null,
-            //         dvdOrder: null,
-            //         location: null,
-            //         paused: null,
-            //         qualities: null,
-            //         release: null,
-            //         scene: null,
-            //         seasonFolders: null,
-            //         sports: null,
-            //         subtitlesEnabled: null,
-            //         airdateOffset: null
-            //     },
-            //     countries: null,
-            //     country_codes: null, // eslint-disable-line camelcase
-            //     genres: null,
-            //     id: {
-            //         tvdb: null,
-            //         slug: null
-            //     },
-            //     indexer: null,
-            //     language: null,
-            //     network: null,
-            //     nextAirDate: null,
-            //     plot: null,
-            //     rating: {
-            //         imdb: {
-            //             rating: null,
-            //             votes: null
-            //         }
-            //     },
-            //     runtime: null,
-            //     showType: null,
-            //     status: null,
-            //     title: null,
-            //     type: null,
-            //     year: {}
-            // },
             shows: []
         };
     },
@@ -121,7 +75,9 @@ export default {
             // Try to get an existing show, using getter.
             let show = getShowById({ indexer, id });
 
-            if (!show || !show.seasons) {
+            if (!this.showLoaded && (!show || !show.seasons)) {
+                // Because this is an async operation, we need to prevent it from accessing the getShow action multiple times.
+                this.showLoaded = true;
                 getShow({ id, indexer, detailed: true })
                     .then((result) => {
                         show = result;
