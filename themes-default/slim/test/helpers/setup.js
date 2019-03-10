0e51859f9c00c460dd6f42f5cee48c86e3ec505e
Dario <medariox@users.noreply.github.com>
2019-03-10 14:04:41 +0100

    Add a few more tests

--- themes-default/slim/test/helpers/setup.js
+++ themes-default/slim/test/helpers/setup.js
@@ -1,23 +1,5 @@
-import browserEnv from 'browser-env';
-import jQuery from 'jquery';
-import hooks from 'require-extension-hooks';
-
-// Setup browser environment
-browserEnv({
-    // This needs to be set otherwise we get errors from vue-router
-    url: 'http://localhost:8081/'
-});
-
-// Setup document variables
-const baseElement = document.createElement('base');
-baseElement.setAttribute('href', 'http://localhost:8081');
-document.head.append(baseElement);
+import $ from 'jquery';
 
 // Setup jQuery
-global.$ = jQuery(window);
-
-// Setup vue files to be processed by `require-extension-hooks-vue`
-hooks('vue').plugin('vue').push();
-// Setup vue and js files to be processed by `require-extension-hooks-babel`
-// This also requires `require-extension-hooks-vue`
-hooks(['vue', 'js']).plugin('babel').push();
+global.$ = $;
+global.jQuery = $;
