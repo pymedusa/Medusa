6168150d223dbd0d11ecdf067d7c0dcc7ed32125
Dario <medariox@users.noreply.github.com>
2019-04-12 16:16:37 +0200

    Fix tablesorter referencing to internal version of jQuery. Fixes #6497 (#6498)

--- themes-default/slim/webpack.config.js
+++ themes-default/slim/webpack.config.js
@@ -72,7 +72,8 @@ const webpackConfig = (env, mode) => ({
     resolve: {
         extensions: ['.js', '.vue', '.json'],
         alias: {
-            vue$: 'vue/dist/vue.esm.js'
+            vue$: 'vue/dist/vue.esm.js',
+            jquery: path.resolve(__dirname, 'node_modules/jquery')
         }
     },
     performance: {
