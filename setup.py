6f9db9f5ef46ace2cfdecaf81cf592704ae3b283
sharkykh <sharkykh@gmail.com>
2019-05-02 21:28:04 +0300

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+# coding=utf-8
 """Use setup tools to install Medusa."""
+import io
 import os
+import re
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
+    user_options = [('pytest-args=', 'a', 'Arguments to pass into py.test')]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
@@ -27,17 +30,46 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
+def get_app_version():
+    """Get the app version from the code."""
+    pattern = re.compile(r"VERSION = '([0-9.]+)'")
+    filename = os.path.join(here, 'medusa', 'common.py')
+    with io.open(filename, 'r', encoding='utf-8') as fh:
+        for line in fh:
+            match = pattern.match(line)
+            if match:
+                return match.group(1)
+
+    raise ValueError('Failed to get the app version!')
+
+
 with open(os.path.join(here, 'readme.md'), 'r') as r:
     long_description = r.read()
 
+
+# These requirements probably won't be needed
+# when `install_requires` is populated with `requirements.txt`
+tests_runtime_require = ['tornado==5.1.1', 'six', 'profilehooks', 'contextlib2', ]
+
 setup(
-    name="medusa",
-    description="Automatic Video Library Manager for TV Shows",
+    name='pymedusa',
+    description='Automatic Video Library Manager for TV Shows',
+    version=get_app_version(),
+    author='pymedusa team',
+    author_email='',
     long_description=long_description,
-    packages=find_packages(),
-    install_requires=['tornado==5.1.1', 'six', 'profilehooks', 'contextlib2', ],
+    long_description_content_type='text/markdown',
+    url='https://github.com/pymedusa/Medusa',
+    license='GPLv3',
+    packages=[],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'medusa = medusa.__main__:main'
+        ]
+    },
     cmdclass={'test': PyTest},
-    tests_require=[
+    tests_require=tests_runtime_require + [
         'flake8>=3.5.0',
         'flake8-docstrings>=1.3.0',
         'flake8-import-order>=0.18',
@@ -56,9 +88,18 @@ setup(
         'system-stats': ['psutil'],
     },
     classifiers=[
-        'Development Status :: ???',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
         'Intended Audience :: Developers',
-        'Operating System :: POSIC :: LINUX',
-        'Topic :: ???',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet',
+        'Topic :: Multimedia :: Video',
     ],
 )
