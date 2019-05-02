265413de33943e0f41fadeea34e5f6e53881b34d
mystycs <mystycs@gmail.com>
2019-05-02 11:33:08 -0400

    Update gimmepeers.py

--- medusa/providers/torrent/html/gimmepeers.py
+++ medusa/providers/torrent/html/gimmepeers.py
@@ -52,6 +52,7 @@ class GimmePeersProvider(TorrentProvider):
     def search(self, search_strings, age=0, ep_obj=None, **kwargs):  # pylint: disable=too-many-locals
         """
         Search a provider and parse the results.
+        
         :param search_strings: A dict with mode (key) and the search value (value)
         :param age: Not used
         :param ep_obj: Not used
@@ -66,8 +67,7 @@ class GimmePeersProvider(TorrentProvider):
             log.debug(u'Search Mode: {0}'.format(mode))
             for search_string in search_strings[mode]:
                 if mode != 'RSS':
-                    log.debug(u'Search string: {0}'.format
-                               (search_string))
+                    log.debug(u'Search string: {0}'.format(search_string))
 
                 self.search_params['search'] = search_string
 
@@ -82,6 +82,7 @@ class GimmePeersProvider(TorrentProvider):
     def parse(self, data, mode):
         """
         Parse search results for items.
+        
         :param data: The raw response from a search
         :param mode: The current mode used to search, e.g. RSS
         :return: A list of items found
