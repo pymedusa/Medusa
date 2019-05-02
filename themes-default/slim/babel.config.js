654edb93dd4eff5bc1255b0cecd2221010bc56b2
sharkykh <sharkykh@gmail.com>
2019-05-02 12:12:31 +0300

    Set targeted browsers

--- themes-default/slim/babel.config.js
+++ themes-default/slim/babel.config.js
@@ -1,5 +1,18 @@
 const presets = [
-    '@babel/preset-env'
+    ['@babel/preset-env', {
+        targets: {
+            browsers: [
+                'last 2 Chrome versions',
+                'last 2 Firefox versions',
+                'last 2 Safari versions',
+                'last 1 Edge version',
+                'Firefox ESR',
+                'last 1 ChromeAndroid version',
+                'last 1 FirefoxAndroid version',
+                'last 1 iOS version'
+            ]
+        }
+    }]
 ];
 
 const plugins = [
