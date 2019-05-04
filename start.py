de50d124fc94fc455e0264c01eb7a4b3eb760299
sharkykh <sharkykh@gmail.com>
2019-05-04 19:42:15 +0300

    Remove unreachable code
    See `medusa/init/__init__.py`:
    `_check_python_version` executes first

--- start.py
+++ start.py
@@ -3,12 +3,7 @@
 """Startup script."""
 from __future__ import unicode_literals
 
-import sys
-
 from medusa.__main__ import main
 
 if __name__ == '__main__':
-    if sys.version_info.major == 3 and sys.version_info.minor < 5:
-        print('Medusa supports Python 2 from version 2.7.10 and Python 3 from version 3.5.0, exiting!')
-        raise Exception('Incorrect Python version. Shutting down!')
     main()
