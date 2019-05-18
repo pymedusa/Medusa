f60ceff6d2015b5fbecd110b99b859d97a5f4840
P0psicles <rogier@headshots.nl>
2019-05-18 13:15:32 +0200

    Implement mutation of ADD_SHOW_EPISODE.

--- themes-default/slim/src/store/mutation-types.js
+++ themes-default/slim/src/store/mutation-types.js
@@ -14,6 +14,7 @@ const NOTIFICATIONS_ENABLED = '🔔 Notifications Enabled';
 const NOTIFICATIONS_DISABLED = '🔔 Notifications Disabled';
 const ADD_CONFIG = '⚙️ Config added to store';
 const ADD_SHOW = '📺 Show added to store';
+const ADD_SHOW_EPISODE = '📺 Shows season with episodes added to store';
 
 export {
     LOGIN_PENDING,
@@ -31,5 +32,6 @@ export {
     NOTIFICATIONS_ENABLED,
     NOTIFICATIONS_DISABLED,
     ADD_CONFIG,
-    ADD_SHOW
+    ADD_SHOW,
+    ADD_SHOW_EPISODE
 };
