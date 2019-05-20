91140e1694a2264affb964e9ffefb729018538f6
sharkykh <sharkykh@gmail.com>
2019-05-20 19:28:56 +0300

    Fix `write_error` for `raise HTTPError(code, message)`

--- medusa/server/api/v2/base.py
+++ medusa/server/api/v2/base.py
@@ -154,8 +154,8 @@ class BaseRequestHandler(RequestHandler):
         exc_info = kwargs.get('exc_info', None)
 
         if exc_info and isinstance(exc_info[1], HTTPError):
-            error = exc_info[1]
-            response = self.api_response(status=status_code, error=error.reason)
+            error = exc_info[1].log_message or exc_info[1].reason
+            response = self.api_response(status=status_code, error=error)
         elif app.DEVELOPER and exc_info:
             self.set_header('content-type', 'text/plain')
             self.set_status(500)
