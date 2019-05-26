ffcc2a8aef4143dcec0bc5cf8f12341bfeeda39b
sharkykh <sharkykh@gmail.com>
2019-05-26 23:56:40 +0300

    Update `utils.combineQualities`

--- themes-default/slim/src/utils.js
+++ themes-default/slim/src/utils.js
@@ -3,10 +3,10 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 /**
  * Calculate the combined value of the selected qualities.
  * @param {number[]} allowedQualities - Array of allowed qualities.
- * @param {number[]} preferredQualities - Array of preferred qualities.
+ * @param {number[]} [preferredQualities=[]] - Array of preferred qualities.
  * @returns {number} - An unsigned integer.
  */
-const combineQualities = (allowedQualities, preferredQualities) => {
+const combineQualities = (allowedQualities, preferredQualities = []) => {
     const reducer = (accumulator, currentValue) => accumulator | currentValue;
     const allowed = allowedQualities.reduce(reducer, 0);
     const preferred = preferredQualities.reduce(reducer, 0);
--- themes-default/slim/test/specs/utils.spec.js
+++ themes-default/slim/test/specs/utils.spec.js
@@ -4,7 +4,8 @@ describe('combineQualities.test.js', () => {
     const testCases = [
         /* C0 */ { allowed: [1, 2, 4], preferred: [], expected: 7 },
         /* C1 */ { allowed: [1], preferred: [2, 4], expected: 393217 },
-        /* C2 */ { allowed: [32768], preferred: [32768], expected: 2147516416 }
+        /* C2 */ { allowed: [32768], preferred: [32768], expected: 2147516416 },
+        /* C3 */ { allowed: [1, 2, 4], preferred: undefined, expected: 7 }
     ];
 
     testCases.forEach(({ allowed, preferred, expected }, caseIndex) => {
