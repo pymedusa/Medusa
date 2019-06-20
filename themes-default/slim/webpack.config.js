11a9d10196292979bac3bfdad02ce74329a9a572
sharkykh <sharkykh@gmail.com>
2019-06-20 20:14:50 +0300

    Replace `oneOf` with `use()`

--- themes-default/slim/webpack.config.js
+++ themes-default/slim/webpack.config.js
@@ -168,30 +168,29 @@ const webpackConfig = (env, mode) => ({
                 // This rule may get either actual `.css` files or the style blocks from `.vue` files.
                 // Here we delegate each request to use the appropriate loaders.
                 test: /\.css$/,
-                oneOf: [
-                    {
-                        // Handle style blocks in `.vue` files
-                        // Based on this query: https://github.com/vuejs/vue-loader/blob/v15.2.7/lib/codegen/styleInjection.js#L27
-                        resourceQuery: /^\?vue&type=style/,
-                        use: [
+                // https://webpack.js.org/configuration/module/#ruleuse
+                use({ resourceQuery }) {
+                    // Handle style blocks in `.vue` files
+                    // Based on this query: https://github.com/vuejs/vue-loader/blob/v15.8.3/lib/codegen/styleInjection.js#L28
+                    if (/^\?vue&type=style/.test(resourceQuery)) {
+                        return [
                             'vue-style-loader',
                             'css-loader'
-                        ]
-                    },
-                    {
-                        // Handle regular `.css` files
-                        use: [
-                            {
-                                loader: MiniCssExtractPlugin.loader,
-                                options: {
-                                    // Fixes loading fonts from the fonts folder
-                                    publicPath: '../'
-                                }
-                            },
-                            'css-loader'
-                        ]
+                        ];
                     }
-                ]
+
+                    // Handle regular `.css` files
+                    return [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                // Fixes loading fonts from the fonts folder
+                                publicPath: '../'
+                            }
+                        },
+                        'css-loader'
+                    ];
+                }
             },
             {
                 test: /\.(woff2?|ttf|eot|svg)$/,
