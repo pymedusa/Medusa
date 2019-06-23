38c80c88e75664195d8701778ca3ec75d944d230
sharkykh <sharkykh@gmail.com>
2019-06-23 11:19:55 +0300

    Update from environment variables when possible

--- medusa/__main__.py
+++ medusa/__main__.py
@@ -473,14 +473,23 @@ class Application(object):
                     app.GIT_REMOTE_URL = app.APPLICATION_URL
 
             # current commit hash
-            commit_hash_env = os.environ.get('MEDUSA_COMMIT_HASH', None)  # This is for our Docker container
-            app.CUR_COMMIT_HASH = check_setting_str(app.CFG, 'General', 'cur_commit_hash', commit_hash_env or '')
+            app.CUR_COMMIT_HASH = check_setting_str(app.CFG, 'General', 'cur_commit_hash', '')
+            # set current commit hash from environment variable, if needed
+            # use this to inject the commit hash information on immutable installations (e.g. Docker containers)
+            commit_hash_env = os.environ.get('MEDUSA_COMMIT_HASH')
+            if commit_hash_env and app.CUR_COMMIT_HASH != commit_hash_env:
+                app.CUR_COMMIT_HASH = commit_hash_env
 
             # current commit branch
-            commit_branch_env = os.environ.get('MEDUSA_COMMIT_BRANCH', None)  # This is for our Docker container
-            app.CUR_COMMIT_BRANCH = check_setting_str(app.CFG, 'General', 'cur_commit_branch', commit_branch_env or '')
+            app.CUR_COMMIT_BRANCH = check_setting_str(app.CFG, 'General', 'cur_commit_branch', '')
+            # set current commit branch from environment variable, if needed
+            # use this to inject the branch information on immutable installations (e.g. Docker containers)
+            commit_branch_env = os.environ.get('MEDUSA_COMMIT_BRANCH')
+            if commit_branch_env and app.CUR_COMMIT_BRANCH != commit_branch_env:
+                app.CUR_COMMIT_BRANCH = commit_branch_env
 
             app.ACTUAL_CACHE_DIR = check_setting_str(app.CFG, 'General', 'cache_dir', 'cache')
+
             # fix bad configs due to buggy code
             if app.ACTUAL_CACHE_DIR == 'None':
                 app.ACTUAL_CACHE_DIR = 'cache'
