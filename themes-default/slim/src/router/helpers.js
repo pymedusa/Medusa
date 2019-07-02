0b65d784111ad57d2dfd52aee5f9aa1635d0403b
sharkykh <sharkykh@gmail.com>
2019-07-02 22:34:37 +0300

    Update docs

--- themes-default/slim/src/router/helpers.js
+++ themes-default/slim/src/router/helpers.js
@@ -2,15 +2,15 @@
  * Make a redirection route for legacy routes that use query parameters,
  * that redirects to the main route that uses path parameters.
  *
- * @param {Object.<string, any>} params Redirect configuration.
+ * @param {Object<string, any>} params Redirect configuration.
  * @param {string} params.target The target route name to redirect to.
  * @param {string} params.from The path to redirect from.
- * @param {Object.<string, function>} [params.targetParams]
+ * @param {Object<string, function>} [params.targetParams]
  *      A mapping linking path parameters on the target route
  *      to a function that returns the matching query parameters' values
  *      (`pathParamName: route => route.query.queryParamName`).
- * @param {...Object.<string, any>} params.config Extra route config options.
- * @returns {Object.<string, any>} A redirection route.
+ * @param {...Object<string, any>} params.config Extra route config options.
+ * @returns {Object<string, any>} A redirection route.
  */
 export const makeLegacyRedirect = ({ target, from, targetParams, ...config }) => ({
     name: `${target}-legacy`,
@@ -45,17 +45,17 @@ const extractLegacyPath = path => {
  *  - A normal route as with all the options passed.
  * Use with spread operator: `[ ...legacyRedirectWrapper({ ... }) ]`.
  *
- * @param {Object.<string, any>} params Route configuration.
- * @param {Object.<string, any>} params.legacyRedirect Redirect route configuration.
+ * @param {Object<string, any>} params Route configuration.
+ * @param {Object<string, any>} params.legacyRedirect Redirect route configuration.
  * @param {string} [params.legacyRedirect.from]
  *      The path to redirect from. If not provided, will extract one from the normal route's path.
- * @param {Object.<string, function>} [params.legacyRedirect.targetParams]
+ * @param {Object<string, function>} [params.legacyRedirect.targetParams]
  *      A mapping linking path parameters on the target route
  *      to a function that returns the matching query parameters' values
  *      (`pathParamName: route => route.query.queryParamName`).
- * @param {...Object.<string, any>} [params.legacyRedirect.redirectConfig] Extra redirect route config options.
- * @param {...Object.<string, any>} params.routeConfig Original route config options.
- * @returns {Object.<string, any>[]} A redirection route + a normal route.
+ * @param {...Object<string, any>} [params.legacyRedirect.redirectConfig] Extra redirect route config options.
+ * @param {...Object<string, any>} params.routeConfig Original route config options.
+ * @returns {Object<string, any>[]} A redirection route + a normal route.
  */
 export const legacyRedirectWrapper = params => {
     const { legacyRedirect, ...routeConfig } = params;
