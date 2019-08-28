f60de2670772fcbdb1d221fe247d6f0430c8b84d
Dario <medariox@users.noreply.github.com>
2019-08-28 13:27:36 +0200

    Fix Torrentz2 size parsing confusing GB with MB (#7094)

--- medusa/providers/torrent/xml/torrentz2.py
+++ medusa/providers/torrent/xml/torrentz2.py
@@ -145,8 +145,8 @@ class Torrentz2Provider(TorrentProvider):
 
     @staticmethod
     def _split_description(description):
-        match = re.findall(r'[0-9]+', description)
-        return int(match[0]) * 1024 ** 2, int(match[1]), int(match[2])
+        match = re.findall(r'(\d+|\d+ \w+)(?= \w+:)', description)
+        return match[0], int(match[1]), int(match[2])
 
 
 provider = Torrentz2Provider()
