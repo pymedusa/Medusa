6410f633499f69751202f84ceb00a66d9efea4e6
P0psicles <rogier@headshots.nl>
2019-11-22 12:03:34 +0100

    Started fixing test_config.py

--- tests/apiv2/test_config.py
+++ tests/apiv2/test_config.py
@@ -3,12 +3,10 @@
 from __future__ import unicode_literals
 
 import json
-import pkgutil
-import platform
 import sys
 
 from medusa import app, classes, common, db, helpers, logger, metadata
-from medusa.indexers.indexer_config import get_indexer_config
+from medusa.sbdatetime import date_presets, time_presets
 from medusa.system.schedulers import all_schedulers
 
 import pytest
@@ -31,45 +29,10 @@ def config_main(monkeypatch, app_config):
     app_config('NAMING_ANIME', 3)
 
     config_data = {}
-    config_data['anonRedirect'] = app.ANON_REDIRECT
-    config_data['animeSplitHome'] = bool(app.ANIME_SPLIT_HOME)
-    config_data['animeSplitHomeInTabs'] = bool(app.ANIME_SPLIT_HOME_IN_TABS)
-    config_data['comingEpsSort'] = app.COMING_EPS_SORT
-    config_data['comingEpsDisplayPaused'] = bool(app.COMING_EPS_DISPLAY_PAUSED)
-    config_data['datePreset'] = app.DATE_PRESET
-    config_data['fuzzyDating'] = bool(app.FUZZY_DATING)
+    # Can't get rid of this because of the usage of themeName in MEDUSA.config.themeName.
     config_data['themeName'] = app.THEME_NAME
-    config_data['posterSortby'] = app.POSTER_SORTBY
-    config_data['posterSortdir'] = app.POSTER_SORTDIR
+    config_data['anonRedirect'] = app.ANON_REDIRECT
     config_data['rootDirs'] = app.ROOT_DIRS
-    config_data['sortArticle'] = bool(app.SORT_ARTICLE)
-    config_data['timePreset'] = app.TIME_PRESET
-    config_data['trimZero'] = bool(app.TRIM_ZERO)
-    config_data['fanartBackground'] = bool(app.FANART_BACKGROUND)
-    config_data['fanartBackgroundOpacity'] = float(app.FANART_BACKGROUND_OPACITY or 0)
-    config_data['gitUsername'] = app.GIT_USERNAME
-    config_data['branch'] = app.BRANCH
-    config_data['commitHash'] = app.CUR_COMMIT_HASH
-    config_data['release'] = app.APP_VERSION
-    config_data['sslVersion'] = app.OPENSSL_VERSION
-    config_data['pythonVersion'] = sys.version
-    config_data['databaseVersion'] = {}
-    config_data['databaseVersion']['major'] = app.MAJOR_DB_VERSION
-    config_data['databaseVersion']['minor'] = app.MINOR_DB_VERSION
-    config_data['os'] = platform.platform()
-    config_data['pid'] = app.PID
-    config_data['locale'] = app_locale
-    config_data['localUser'] = os_user
-    config_data['programDir'] = app.PROG_DIR
-    config_data['dataDir'] = app.DATA_DIR
-    config_data['configFile'] = app.CONFIG_FILE
-    config_data['dbPath'] = db.DBConnection().path
-    config_data['cacheDir'] = app.CACHE_DIR
-    config_data['logDir'] = app.LOG_DIR
-    config_data['appArgs'] = app.MY_ARGS
-    config_data['webRoot'] = app.WEB_ROOT
-    config_data['runsInDocker'] = bool(app.RUNS_IN_DOCKER)
-    config_data['githubUrl'] = app.GITHUB_IO_URL
     config_data['wikiUrl'] = app.WIKI_URL
     config_data['donationsUrl'] = app.DONATIONS_URL
     config_data['sourceUrl'] = app.APPLICATION_URL
@@ -93,75 +56,91 @@ def config_main(monkeypatch, app_config):
     config_data['showDefaults']['anime'] = bool(app.ANIME_DEFAULT)
     config_data['showDefaults']['scene'] = bool(app.SCENE_DEFAULT)
 
-    config_data['news'] = {}
-    config_data['news']['lastRead'] = app.NEWS_LAST_READ
-    config_data['news']['latest'] = app.NEWS_LATEST
-    config_data['news']['unread'] = app.NEWS_UNREAD
-
     config_data['logs'] = {}
     config_data['logs']['debug'] = bool(app.DEBUG)
     config_data['logs']['dbDebug'] = bool(app.DBDEBUG)
     config_data['logs']['loggingLevels'] = {k.lower(): v for k, v in iteritems(logger.LOGGING_LEVELS)}
     config_data['logs']['numErrors'] = len(classes.ErrorViewer.errors)
     config_data['logs']['numWarnings'] = len(classes.WarningViewer.errors)
-
-    config_data['failedDownloads'] = {}
-    config_data['failedDownloads']['enabled'] = bool(app.USE_FAILED_DOWNLOADS)
-    config_data['failedDownloads']['deleteFailed'] = bool(app.DELETE_FAILED)
-
-    config_data['layout'] = {}
-    config_data['layout']['schedule'] = app.COMING_EPS_LAYOUT
-    config_data['layout']['history'] = app.HISTORY_LAYOUT
-    config_data['layout']['home'] = app.HOME_LAYOUT
-    config_data['layout']['show'] = {}
-    config_data['layout']['show']['specials'] = bool(app.DISPLAY_SHOW_SPECIALS)
-    config_data['layout']['show']['showListOrder'] = app.SHOW_LIST_ORDER
+    config_data['logs']['actualLogDir'] = app.ACTUAL_LOG_DIR
+    config_data['logs']['nr'] = int(app.LOG_NR)
+    config_data['logs']['size'] = float(app.LOG_SIZE)
+    config_data['logs']['subliminalLog'] = bool(app.SUBLIMINAL_LOG)
+    config_data['logs']['privacyLevel'] = app.PRIVACY_LEVEL
 
     config_data['selectedRootIndex'] = int(app.SELECTED_ROOT) if app.SELECTED_ROOT is not None else -1  # All paths
 
+    # Added for config - main, needs refactoring in the structure.
+    config_data['launchBrowser'] = bool(app.LAUNCH_BROWSER)
+    config_data['defaultPage'] = app.DEFAULT_PAGE
+    config_data['trashRemoveShow'] = bool(app.TRASH_REMOVE_SHOW)
+    config_data['trashRotateLogs'] = bool(app.TRASH_ROTATE_LOGS)
+
+    config_data['indexerDefaultLanguage'] = app.INDEXER_DEFAULT_LANGUAGE
+    config_data['showUpdateHour'] = int(app.SHOWUPDATE_HOUR) if app.SHOWUPDATE_HOUR is not None else 0
+    config_data['indexerTimeout'] = int(app.INDEXER_TIMEOUT) if app.INDEXER_TIMEOUT is not None else 0
+    config_data['indexerDefault'] = app.INDEXER_DEFAULT
+
+    config_data['plexFallBack'] = {}
+    config_data['plexFallBack']['enable'] = bool(app.FALLBACK_PLEX_ENABLE)
+    config_data['plexFallBack']['notifications'] = bool(app.FALLBACK_PLEX_NOTIFICATIONS)
+    config_data['plexFallBack']['timeout'] = int(app.FALLBACK_PLEX_TIMEOUT)
+
+    config_data['versionNotify'] = bool(app.VERSION_NOTIFY)
+    config_data['autoUpdate'] = bool(app.AUTO_UPDATE)
+    config_data['updateFrequency'] = int(app.UPDATE_FREQUENCY) if app.UPDATE_FREQUENCY is not None else 1
+    config_data['notifyOnUpdate'] = bool(app.NOTIFY_ON_UPDATE)
+    config_data['availableThemes'] = [{'name': theme.name,
+                                        'version': theme.version,
+                                        'author': theme.author}
+                                       for theme in app.AVAILABLE_THEMES]
+
+    config_data['timePresets'] = list(time_presets)
+    config_data['datePresets'] = list(date_presets)
+
+    config_data['webInterface'] = {}
+    config_data['webInterface']['apiKey'] = app.API_KEY
+    config_data['webInterface']['log'] = bool(app.WEB_LOG)
+    config_data['webInterface']['username'] = app.WEB_USERNAME
+    config_data['webInterface']['password'] = app.WEB_PASSWORD
+    config_data['webInterface']['port'] = int(app.WEB_PORT) if app.WEB_PORT is not None else 8080
+    config_data['webInterface']['notifyOnLogin'] = bool(app.NOTIFY_ON_LOGIN)
+    config_data['webInterface']['ipv6'] = bool(app.WEB_IPV6)
+    config_data['webInterface']['httpsEnable'] = bool(app.ENABLE_HTTPS)
+    config_data['webInterface']['httpsCert'] = app.HTTPS_CERT
+    config_data['webInterface']['httpsKey'] = app.HTTPS_KEY
+    config_data['webInterface']['handleReverseProxy'] = bool(app.HANDLE_REVERSE_PROXY)
+
+    config_data['cpuPreset'] = app.CPU_PRESET
+    config_data['sslVerify'] = bool(app.SSL_VERIFY)
+    config_data['sslCaBundle'] = app.SSL_CA_BUNDLE
+    config_data['noRestart'] = bool(app.NO_RESTART)
+    config_data['encryptionVersion'] = bool(app.ENCRYPTION_VERSION)
+    config_data['calendarUnprotected'] = bool(app.CALENDAR_UNPROTECTED)
+    config_data['calendarIcons'] = bool(app.CALENDAR_ICONS)
+    config_data['proxySetting'] = app.PROXY_SETTING
+    config_data['proxyIndexers'] = bool(app.PROXY_INDEXERS)
+    config_data['skipRemovedFiles'] = bool(app.SKIP_REMOVED_FILES)
+    config_data['epDefaultDeletedStatus'] = app.EP_DEFAULT_DELETED_STATUS
+    config_data['developer'] = bool(app.DEVELOPER)
+
+    config_data['git'] = {}
+    config_data['git']['username'] = app.GIT_USERNAME
+    config_data['git']['password'] = app.GIT_PASSWORD
+    config_data['git']['token'] = app.GIT_TOKEN
+    config_data['git']['authType'] = int(app.GIT_AUTH_TYPE)
+    config_data['git']['remote'] = app.GIT_REMOTE
+    config_data['git']['path'] = app.GIT_PATH
+    config_data['git']['org'] = app.GIT_ORG
+    config_data['git']['reset'] = bool(app.GIT_RESET)
+    config_data['git']['resetBranches'] = app.GIT_RESET_BRANCHES
+    config_data['git']['url'] = app.GITHUB_IO_URL
+
+    # backlogOverview has been moved to layout. It's still located here, because manage_backlogOvervew uses it
+    # and still needs to be vieuified. After vueifying it, remove this.
     config_data['backlogOverview'] = {}
-    config_data['backlogOverview']['period'] = app.BACKLOG_PERIOD
     config_data['backlogOverview']['status'] = app.BACKLOG_STATUS
-
-    config_data['indexers'] = {}
-    config_data['indexers']['config'] = get_indexer_config()
-
-    config_data['postProcessing'] = {}
-    config_data['postProcessing']['naming'] = {}
-    config_data['postProcessing']['naming']['pattern'] = app.NAMING_PATTERN
-    config_data['postProcessing']['naming']['multiEp'] = int(app.NAMING_MULTI_EP)
-    config_data['postProcessing']['naming']['patternAirByDate'] = app.NAMING_ABD_PATTERN
-    config_data['postProcessing']['naming']['patternSports'] = app.NAMING_SPORTS_PATTERN
-    config_data['postProcessing']['naming']['patternAnime'] = app.NAMING_ANIME_PATTERN
-    config_data['postProcessing']['naming']['enableCustomNamingAirByDate'] = bool(app.NAMING_CUSTOM_ABD)
-    config_data['postProcessing']['naming']['enableCustomNamingSports'] = bool(app.NAMING_CUSTOM_SPORTS)
-    config_data['postProcessing']['naming']['enableCustomNamingAnime'] = bool(app.NAMING_CUSTOM_ANIME)
-    config_data['postProcessing']['naming']['animeMultiEp'] = int(app.NAMING_ANIME_MULTI_EP)
-    config_data['postProcessing']['naming']['animeNamingType'] = int(app.NAMING_ANIME)
-    config_data['postProcessing']['naming']['stripYear'] = bool(app.NAMING_STRIP_YEAR)
-    config_data['postProcessing']['showDownloadDir'] = app.TV_DOWNLOAD_DIR
-    config_data['postProcessing']['processAutomatically'] = bool(app.PROCESS_AUTOMATICALLY)
-    config_data['postProcessing']['postponeIfSyncFiles'] = bool(app.POSTPONE_IF_SYNC_FILES)
-    config_data['postProcessing']['postponeIfNoSubs'] = bool(app.POSTPONE_IF_NO_SUBS)
-    config_data['postProcessing']['renameEpisodes'] = bool(app.RENAME_EPISODES)
-    config_data['postProcessing']['createMissingShowDirs'] = bool(app.CREATE_MISSING_SHOW_DIRS)
-    config_data['postProcessing']['addShowsWithoutDir'] = bool(app.ADD_SHOWS_WO_DIR)
-    config_data['postProcessing']['moveAssociatedFiles'] = bool(app.MOVE_ASSOCIATED_FILES)
-    config_data['postProcessing']['nfoRename'] = bool(app.NFO_RENAME)
-    config_data['postProcessing']['airdateEpisodes'] = bool(app.AIRDATE_EPISODES)
-    config_data['postProcessing']['unpack'] = bool(app.UNPACK)
-    config_data['postProcessing']['deleteRarContent'] = bool(app.DELRARCONTENTS)
-    config_data['postProcessing']['noDelete'] = bool(app.NO_DELETE)
-    config_data['postProcessing']['processMethod'] = app.PROCESS_METHOD
-    config_data['postProcessing']['reflinkAvailable'] = bool(pkgutil.find_loader('reflink'))
-    config_data['postProcessing']['autoPostprocessorFrequency'] = int(app.AUTOPOSTPROCESSOR_FREQUENCY)
-    config_data['postProcessing']['syncFiles'] = app.SYNC_FILES
-    config_data['postProcessing']['fileTimestampTimezone'] = app.FILE_TIMESTAMP_TIMEZONE
-    config_data['postProcessing']['allowedExtensions'] = list(app.ALLOWED_EXTENSIONS)
-    config_data['postProcessing']['extraScripts'] = app.EXTRA_SCRIPTS
-    config_data['postProcessing']['extraScriptsUrl'] = app.EXTRA_SCRIPTS_URL
-    config_data['postProcessing']['multiEpStrings'] = {text_type(k): v for k, v in iteritems(common.MULTI_EP_STRINGS)}
-
+    config_data['backlogOverview']['period'] = app.BACKLOG_PERIOD
     return config_data
 
 
@@ -182,12 +161,11 @@ def test_config_get(http_client, create_url, auth_headers, config_main):
 
 @pytest.mark.gen_test
 @pytest.mark.parametrize('query', [
-    'pythonVersion',
-    'locale',
-    'localUser',
-    'githubUrl',
-    'dbPath',
-    'postProcessing'
+    'defaultPage',
+    'subtitlesMulti',
+    'showDefaults',
+    'wikiUrl',
+    'cpuPreset'
 ])
 def test_config_get_detailed(http_client, create_url, auth_headers, config_main, query):
     # given
@@ -288,15 +266,15 @@ def config_metadata(monkeypatch, app_config):
         tmp_provider.set_config(cur_metadata_config)
         monkeypatch.setitem(metadata_provider_dict, tmp_provider.name, tmp_provider)
 
-    section_data = {}
+    config_data = {}
 
-    section_data['metadataProviders'] = {}
+    config_data['metadataProviders'] = {}
 
     for provider in itervalues(app.metadata_provider_dict):
         json_repr = provider.to_json()
-        section_data['metadataProviders'][json_repr['id']] = json_repr
+        config_data['metadataProviders'][json_repr['id']] = json_repr
 
-    return section_data
+    return config_data
 
 
 @pytest.mark.gen_test
@@ -320,12 +298,12 @@ def config_system(monkeypatch, app_config):
         return '124.86 MB'
     monkeypatch.setattr(helpers, 'memory_usage', memory_usage_mock)
 
-    section_data = {}
-    section_data['memoryUsage'] = memory_usage_mock()
-    section_data['schedulers'] = [{'key': scheduler[0], 'name': scheduler[1]} for scheduler in all_schedulers]
-    section_data['showQueue'] = []
+    config_data = {}
+    config_data['memoryUsage'] = memory_usage_mock()
+    config_data['schedulers'] = [{'key': scheduler[0], 'name': scheduler[1]} for scheduler in all_schedulers]
+    config_data['showQueue'] = []
 
-    return section_data
+    return config_data
 
 
 @pytest.mark.gen_test
