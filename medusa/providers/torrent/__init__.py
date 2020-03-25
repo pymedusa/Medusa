40fa687490997c512390de52c88c10ba80c185e3
Dario <medariox@users.noreply.github.com>
2020-03-25 18:04:10 +0100

    Fix provider import Python 3.5

--- medusa/providers/torrent/__init__.py
+++ medusa/providers/torrent/__init__.py
@@ -2,18 +2,3 @@
 
 """Initialize all torrent providers."""
 from __future__ import unicode_literals
-
-import importlib
-import os
-import pkgutil
-import sys
-
-
-torrent_kinds = ['html', 'json', 'rss', 'torznab', 'xml']
-dirname = os.path.dirname(__file__)
-modules = [os.path.join(dirname, kind) for kind in torrent_kinds]
-
-for (module_loader, name, ispkg) in pkgutil.iter_modules(modules):
-    base = os.path.basename(module_loader.path)
-    module = importlib.import_module('.' + name, __package__ + '.' + base)
-    setattr(sys.modules[__name__], name, module)
