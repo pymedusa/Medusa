2c7a6becd2dacd99741dfd5a1baef602e6515164
Dario <medariox@users.noreply.github.com>
2020-03-30 22:11:06 +0200

    fix for current code

--- medusa/scheduler.py
+++ medusa/scheduler.py
@@ -1,29 +1,34 @@
 # coding=utf-8
 
+from __future__ import division
 from __future__ import unicode_literals
 
 import datetime
 import logging
-from threading import Event, Thread
+import threading
+import time
 
+from medusa import exception_handler
 from medusa.logger.adapters.style import BraceAdapter
 
 log = BraceAdapter(logging.getLogger(__name__))
 log.logger.addHandler(logging.NullHandler())
 
 
-class Scheduler(Thread):
-    def __init__(self, action, cycleTime=None, start_time=None, threadName=None, silent=True):
-        Thread.__init__(self)
+class Scheduler(threading.Thread):
+    def __init__(self, action, cycleTime=datetime.timedelta(minutes=10), start_time=None,
+                 threadName='ScheduledThread', silent=True):
+        super(Scheduler, self).__init__()
 
         self.action = action
         self.cycleTime = cycleTime if start_time is None else datetime.timedelta(days=1)
         self.start_time = start_time
-        self.name = threadName or 'ScheduledThread'
-        self.silent = silent
 
-        self.stopped = Event()
-        self.lastRun = None
+        self.lastRun = datetime.datetime.now()
+        self.name = threadName
+        self.silent = silent
+        self.stop = threading.Event()
+        self.force = False
         self.enable = False
 
     def timeLeft(self):
@@ -32,33 +37,53 @@ class Scheduler(Thread):
 
         :return: timedelta
         """
-        if self.start_time is None:
-            return self.cycleTime - (datetime.datetime.now() - self.lastRun)
-        else:
-            time_now = datetime.datetime.now()
-            start_time_today = datetime.datetime.combine(time_now.date(), self.start_time)
-            if time_now >= start_time_today:
-                start_time_tomorrow = start_time_today + datetime.timedelta(days=1)
-                return start_time_tomorrow - time_now
+        if self.is_alive():
+            if self.start_time is None:
+                return self.cycleTime - (datetime.datetime.now() - self.lastRun)
             else:
-                return start_time_today - time_now
+                time_now = datetime.datetime.now()
+                start_time_today = datetime.datetime.combine(time_now.date(), self.start_time)
+                if time_now >= start_time_today:
+                    start_time_tomorrow = start_time_today + datetime.timedelta(days=1)
+                    return start_time_tomorrow - time_now
+                else:
+                    return start_time_today - time_now
+        else:
+            return datetime.timedelta(seconds=0)
 
     def forceRun(self):
         if not self.action.amActive:
-            self.run(force=True)
+            self.force = True
             return True
         return False
 
-    def run(self, force=False):
-        if not self.lastRun:
-            self.lastRun = datetime.datetime.now()
-        while force or not self.stopped.wait(self.cycleTime.total_seconds()):
-            self.action.run(force)
-            if force:
-                force = False
-            else:
-                self.lastRun = datetime.datetime.now()
+    def run(self):
+        """Run the thread."""
+        try:
+            while not self.stop.is_set():
+                if self.enable:
+                    should_run = False
+                    time_left = self.timeLeft()
 
-    def stop(self):
-        self.stopped.set()
-        self.join()
+                    # Is self.force enable
+                    if self.force:
+                        should_run = True
+
+                    # check if interval has passed
+                    elif time_left.total_seconds() <= 0:
+                        should_run = True
+
+                    if should_run:
+                        if not self.silent:
+                            log.debug('Starting new thread: {name}', {'name': self.name})
+                        self.action.run(self.force)
+                        self.lastRun = datetime.datetime.now()
+
+                    if self.force:
+                        self.force = False
+
+                time.sleep(1)
+            # exiting thread
+            self.stop.clear()
+        except Exception as e:
+            exception_handler.handle(e)
