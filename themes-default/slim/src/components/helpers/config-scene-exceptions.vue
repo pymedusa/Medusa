18a010aecc0072237cef9907f4cacc4b3bb93fdb
P0psicles <rogier@headshots.nl>
2020-04-04 11:05:42 +0200

    Show Medusa icon for exceptions that are not custom. * Show tooltip with more info.

--- themes-default/slim/src/components/helpers/config-scene-exceptions.vue
+++ themes-default/slim/src/components/helpers/config-scene-exceptions.vue
@@ -16,7 +16,17 @@
                         </option>
                     </select>
 
-                    <div class="input-group-btn" @click="removeException(exception)">
+
+                    <div
+                        v-if="!exception.custom"
+                        class="external-scene-exception"
+                        v-tooltip.right="'This exception has been automatically added through an automated process sourcing title aliases from medusa.github.io, thexem.de or anidb.info'"
+                    >
+                        <div class="align-center">
+                            <img src="images/ico/favicon-16.png" width="16" height="16" alt="search" title="This exception has been automatically added through an automated process sourcing title aliases from medusa.github.io, thexem.de or anidb.info.">
+                        </div>
+                    </div>
+                    <div v-else class="input-group-btn" @click="removeException(exception)">
                         <div style="font-size: 14px" class="btn btn-default input-sm">
                             <i class="glyphicon glyphicon-remove" title="Remove" />
                         </div>
@@ -58,9 +68,13 @@
 <script>
 
 import { mapActions } from 'vuex';
+import { VTooltip } from 'v-tooltip';
 
 export default {
     name: 'config-scene-exceptions',
+    directives: {
+        tooltip: VTooltip
+    },
     props: {
         exceptions: {
             type: Array,
@@ -120,9 +134,7 @@ export default {
         },
         removeException(exception) {
             const { clear, removeSceneException, show } = this;
-            if (!exception.custom) {
-                return;
-            }
+
             removeSceneException({ show, exception });
             clear();
         },
@@ -186,6 +198,144 @@ div.select-list .switch-input {
 
 .select-season[disabled=disabled] {
     background-color: #eee;
+}
+
+.external-scene-exception {
+    display: table-cell;
+    width: 4.5px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    background-color: #fff;
+    border-color: #ccc;
+}
+
+.external-scene-exception div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 30px;
+    border: 1px solid #ccc;
+    border-top-left-radius:0px;
+    border-top-right-radius:4px;
+    border-bottom-left-radius:0px;
+    border-bottom-right-radius:4px;
+    z-index: 2;
+    margin-left: -1px;
+}
+
+.external-scene-exception > img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.external-scene-exception:last-child>.div {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+
+/* We still need to move this to some sort of css import*/
+.tooltip-wrapper {
+    float: left;
+    min-width: 340px;
+}
+.invalid {
+    background-color: #ff5b5b;
+}
+.tooltip {
+    display: block !important;
+    z-index: 10000;
+}
+.tooltip .tooltip-inner {
+    background: #ffef93;
+    color: #555;
+    border-radius: 16px;
+    padding: 5px 10px 4px;
+    border: 1px solid #f1d031;
+    -webkit-box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.15);
+    -moz-box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.15);
+    box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.15);
+}
+.tooltip .tooltip-arrow {
+    width: 0;
+    height: 0;
+    position: absolute;
+    margin: 5px;
+    border: 1px solid #ffef93;
+    z-index: 1;
+}
+.tooltip[x-placement^='top'] {
+    margin-bottom: 5px;
+}
+.tooltip[x-placement^='top'] .tooltip-arrow {
+    border-width: 5px 5px 0 5px;
+    border-left-color: transparent !important;
+    border-right-color: transparent !important;
+    border-bottom-color: transparent !important;
+    bottom: -5px;
+    left: calc(50% - 4px);
+    margin-top: 0;
+    margin-bottom: 0;
+}
+.tooltip[x-placement^='bottom'] {
+    margin-top: 5px;
+}
+.tooltip[x-placement^='bottom'] .tooltip-arrow {
+    border-width: 0 5px 5px 5px;
+    border-left-color: transparent !important;
+    border-right-color: transparent !important;
+    border-top-color: transparent !important;
+    top: -5px;
+    left: calc(50% - 4px);
+    margin-top: 0;
+    margin-bottom: 0;
+}
+.tooltip[x-placement^='right'] {
+    margin-left: 5px;
+}
+.tooltip[x-placement^='right'] .tooltip-arrow {
+    border-width: 5px 5px 5px 0;
+    border-left-color: transparent !important;
+    border-top-color: transparent !important;
+    border-bottom-color: transparent !important;
+    left: -4px;
+    top: calc(50% - 5px);
+    margin-left: 0;
+    margin-right: 0;
+}
+.tooltip[x-placement^='left'] {
+    margin-right: 5px;
+}
+.tooltip[x-placement^='left'] .tooltip-arrow {
+    border-width: 5px 0 5px 5px;
+    border-top-color: transparent !important;
+    border-right-color: transparent !important;
+    border-bottom-color: transparent !important;
+    right: -4px;
+    top: calc(50% - 5px);
+    margin-left: 0;
+    margin-right: 0;
+}
+.tooltip.popover .popover-inner {
+    background: #ffef93;
+    color: #555;
+    padding: 24px;
+    border-radius: 5px;
+    box-shadow: 0 5px 30px rgba(black, 0.1);
+}
+.tooltip.popover .popover-arrow {
+    border-color: #ffef93;
+}
+.tooltip[aria-hidden='true'] {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.15s, visibility 0.15s;
+}
+.tooltip[aria-hidden='false'] {
+    visibility: visible;
+    opacity: 1;
+    transition: opacity 0.15s;
 }
 
 </style>
