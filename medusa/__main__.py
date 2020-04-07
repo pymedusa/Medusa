7b9ba37386bf549a59a41388857e97aee5875813
P0psicles <rogier@headshots.nl>
2020-04-07 17:18:04 +0200

    Add python 2 EOL warning

--- medusa/__main__.py
+++ medusa/__main__.py
@@ -332,6 +332,14 @@ class Application(object):
 
         logger.info('Starting Medusa [{branch}] using {config!r}', branch=app.BRANCH, config=app.CONFIG_FILE)
 
+        # Python 2 EOL warning
+        if sys.version_info < (3,):
+            logger.warning(
+                'As of October 1st Medusa will not run on python 2.x.\n'
+                "Python 2.x has passed it's sunset date as you can read here: {python_sunset_url}\n"
+                'Please upgrade your python version to 3.6 or higer as soon as possible!', python_sunset_url='https://tinyurl.com/y4zwbawq'
+            )
+
         if not is_valid_encoding(app.SYS_ENCODING):
             logger.warning(
                 'Your system is using an invalid encoding: {encoding}. Please change your encoding '
