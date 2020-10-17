c8d5da61088eb81a5f89a07f0d38d548ce7b75e5
P0psicles <rogier@headshots.nl>
2020-10-17 09:05:14 +0200

    Fix flake warnings

--- medusa/providers/torrent/html/speedcd.py
+++ medusa/providers/torrent/html/speedcd.py
@@ -5,17 +5,14 @@
 from __future__ import unicode_literals
 
 import logging
+import re
 
 from medusa import tv
 from medusa.bs4_parser import BS4Parser
-from medusa.helper.common import (
-    convert_size,
-    try_int,
-)
+from medusa.helper.common import convert_size
 from medusa.logger.adapters.style import BraceAdapter
 from medusa.providers.torrent.torrent_provider import TorrentProvider
 
-import re
 from requests.compat import urljoin
 from requests.utils import dict_from_cookiejar
 
