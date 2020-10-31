2de3855b303c09f436602cb516319ce7cec4f420
P0psicles <rogier@headshots.nl>
2020-10-31 15:00:56 +0100

    Add config menu to config - anime. * build runtime

--- themes-default/slim/src/router/routes.js
+++ themes-default/slim/src/router/routes.js
@@ -131,6 +131,7 @@ const configRoutes = [
             title: 'Config - Anime',
             header: 'Anime',
             topMenu: 'config',
+            subMenu: configSubMenu,
             converted: true
         },
         component: () => import('../components/config-anime.vue')
