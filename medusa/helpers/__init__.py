14efae436b49e744941d84f5346fe27c5ecb56e1
p0ps <p0psicles@users.noreply.github.com>
2020-11-26 19:58:42 +0100

    Change decode/encode string -> bytes (#8768)
    * Change decode/encode string -> bytes
    
    * Fix rtorrent encodestring.

--- medusa/helpers/__init__.py
+++ medusa/helpers/__init__.py
@@ -923,11 +923,11 @@ def encrypt(data, encryption_version=0, _decrypt=False):
         # Version 1: unique_key1; Version 2: app.ENCRYPTION_SECRET
         key = unique_key1 if encryption_version == 1 else app.ENCRYPTION_SECRET
         if _decrypt:
-            data = ensure_text(base64.decodestring(ensure_binary(data)))
+            data = ensure_text(base64.decodebytes(ensure_binary(data)))
             return ''.join(chr(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
         else:
             data = ''.join(chr(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
-            return ensure_text(base64.encodestring(ensure_binary(data))).strip()
+            return ensure_text(base64.encodebytes(ensure_binary(data))).strip()
 
 
 def decrypt(data, encryption_version=0):
