dc7f63242659bec2f330707229b6303a97d157a8
p0psicles <rogier@headshots.nl>
2020-12-31 16:58:34 +0100

    Dont need this

--- themes-default/slim/src/components/show-results.vue
+++ themes-default/slim/src/components/show-results.vue
@@ -11,7 +11,6 @@
                     </template>
                 </div>
                 <vue-good-table v-show="show.id.slug"
-                                class="vgt-table-styling"
                                 ref="vgt-show-results"
                                 :columns="columns"
                                 :rows="combinedResults"
