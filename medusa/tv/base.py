7373f4e12fee1c72bd8918ca0768bdfc6d71b2c5
Dario <medariox@users.noreply.github.com>
2021-01-07 16:56:18 +0100

    Fix setting dirty setter for properties

--- medusa/tv/base.py
+++ medusa/tv/base.py
@@ -54,7 +54,9 @@ class TV(object):
         if not key.startswith('_') and key not in self.__ignored_properties:
             obj = getattr(self.__class__, key, None)
             if obj and isinstance(obj, property):
-                self.__dirty |= obj.__get__(self, key) != value
+                key_attr = f'_{key}'
+                if key_attr in self.__dict__:
+                    self.__dirty |= self.__dict__.get(key_attr) != value
             elif key in self.__dict__:
                 self.__dirty |= self.__dict__.get(key) != value
 
@@ -72,12 +74,6 @@ class TV(object):
     def reset_dirty(self):
         """Reset the dirty flag."""
         self.__dirty = False
-
-    @property
-    def tvdb_id(self):
-        """Get the item's tvdb_id."""
-        if self.indexerid and self.indexer == INDEXER_TVDBV2:
-            return self.indexerid
 
     def __getstate__(self):
         """Make object serializable."""
