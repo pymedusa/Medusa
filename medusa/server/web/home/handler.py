7dc08457c8de87d2cdedb81f5d8f46226cfc987d
p0psicles <rogier@headshots.nl>
2021-01-13 20:29:44 +0100

    Remove get_scene_exceptions() call from massUpdateShow handler.

--- medusa/server/web/home/handler.py
+++ medusa/server/web/home/handler.py
@@ -956,8 +956,6 @@ class Home(WebRoot):
             errors += 1
             return errors
 
-        series_obj.aliases = get_scene_exceptions(series_obj)
-
         season_folders = config.checkbox_to_value(season_folders)
         dvd_order = config.checkbox_to_value(dvd_order)
         paused = config.checkbox_to_value(paused)
