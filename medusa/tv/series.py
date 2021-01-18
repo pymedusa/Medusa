38ab1162d358779eebd3b7478779d5f3e26fd586
Dario <medariox@users.noreply.github.com>
2021-01-18 13:36:09 +0100

    small changes

--- medusa/tv/series.py
+++ medusa/tv/series.py
@@ -215,8 +215,9 @@ class Series(TV):
         :param lang:
         :type lang: str
         """
-        super(Series, self).__init__(indexer, indexerid, {'episodes', 'next_aired', 'release_groups', 'exceptions',
-                                                          'external', 'imdb_info'})
+        super(Series, self).__init__(
+            indexer, indexerid,
+            {'episodes', 'release_groups', 'imdb_info'})
         self.show_id = None
         self._name = ''
         self.imdb_id = ''
@@ -641,7 +642,10 @@ class Series(TV):
     @airs.setter
     def airs(self, value):
         """Set episode time that series usually airs."""
-        self._airs = text_type(value).replace('am', ' AM').replace('pm', ' PM').replace('  ', ' ').strip()
+        if value is None or not network_timezones.test_timeformat(self._airs):
+            self.airs = ''
+        else:
+            self._airs = text_type(value).replace('am', ' AM').replace('pm', ' PM').replace('  ', ' ').strip()
 
     @property
     def poster(self):
@@ -1476,7 +1480,6 @@ class Series(TV):
         else:
             self.show_id = int(sql_results[0]['show_id'] or 0)
             self.indexer = int(sql_results[0]['indexer'] or 0)
-            self.start_year = int(sql_results[0]['startyear'] or 0)
 
             if not self.name:
                 self.name = sql_results[0]['show_name']
@@ -1487,51 +1490,42 @@ class Series(TV):
             if not self.classification:
                 self.classification = sql_results[0]['classification']
 
+            self.imdb_id = sql_results[0]['imdb_id']
             self.runtime = sql_results[0]['runtime']
-
-            self.status = sql_results[0]['status']
-            if self.status is None:
-                self.status = 'Unknown'
-
+            self.quality = int(sql_results[0]['quality'] or Quality.NA)
+            self.season_folders = int(not (sql_results[0]['flatten_folders'] or 0))  # TODO: Rename this in the DB
+            self.status = sql_results[0]['status'] or 'Unknown'
             self.airs = sql_results[0]['airs']
-            if self.airs is None or not network_timezones.test_timeformat(self.airs):
-                self.airs = ''
 
+            self.airdate_offset = int(sql_results[0]['airdate_offset'] or 0)
+            self.start_year = int(sql_results[0]['startyear'] or 0)
+            self.paused = int(sql_results[0]['paused'] or 0)
             self.air_by_date = int(sql_results[0]['air_by_date'] or 0)
-            self.anime = int(sql_results[0]['anime'] or 0)
-            self.sports = int(sql_results[0]['sports'] or 0)
-            self.scene = int(sql_results[0]['scene'] or 0)
             self.subtitles = int(sql_results[0]['subtitles'] or 0)
             self.notify_list = json.loads(sql_results[0]['notify_list'] or '{}')
             self.dvd_order = int(sql_results[0]['dvdorder'] or 0)
-            self.quality = int(sql_results[0]['quality'] or Quality.NA)
-            self.season_folders = int(not (sql_results[0]['flatten_folders'] or 0))  # TODO: Rename this in the DB
-            self.paused = int(sql_results[0]['paused'] or 0)
-            self._location = sql_results[0]['location']  # skip location validation
-
             if not self.lang:
                 self.lang = sql_results[0]['lang']
-
             self._last_update_indexer = sql_results[0]['last_update_indexer']
+
+            self.sports = int(sql_results[0]['sports'] or 0)
+            self.anime = int(sql_results[0]['anime'] or 0)
+            self.scene = int(sql_results[0]['scene'] or 0)
+
+            self.default_ep_status = int(sql_results[0]['default_ep_status'] or SKIPPED)
+            self._location = sql_results[0]['location']  # skip location validation
 
             self.rls_ignore_words = sql_results[0]['rls_ignore_words']
             self.rls_require_words = sql_results[0]['rls_require_words']
             self.rls_ignore_exclude = sql_results[0]['rls_ignore_exclude']
             self.rls_require_exclude = sql_results[0]['rls_require_exclude']
 
-            self.default_ep_status = int(sql_results[0]['default_ep_status'] or SKIPPED)
-
-            if not self.imdb_id:
-                self.imdb_id = sql_results[0]['imdb_id']
-
-            self.plot = sql_results[0]['plot']
-            self.airdate_offset = int(sql_results[0]['airdate_offset'] or 0)
-
             self.release_groups = BlackAndWhiteList(self)
 
             # Load external id's from indexer_mappings table.
             self.externals = load_externals_from_db(self.indexer, self.series_id)
 
+            self.plot = sql_results[0]['plot']
             self._show_lists = sql_results[0]['show_lists']
 
         # Get IMDb_info from database
