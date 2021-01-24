a63004f5deddf49cb418c8d18fdbcdfe3fa0a627
p0psicles <rogier@headshots.nl>
2021-01-24 17:17:38 +0100

    Disable, prevent devideByZero exception.

--- medusa/clients/torrent/rtorrent.py
+++ medusa/clients/torrent/rtorrent.py
@@ -198,7 +198,8 @@ class RTorrentAPI(GenericClient):
         client_status.ratio = torrent.ratio
 
         # Store progress
-        client_status.progress = int(torrent.completed_bytes / torrent.bytes_done * 100)
+        # Disabled for now. as it can cause DivideByZero exception.
+        # client_status.progress = int(torrent.completed_bytes / torrent.bytes_done * 100)
 
         # Store destination
         client_status.destination = torrent.directory
