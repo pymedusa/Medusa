f55729ae3de597b5349cd4bc77f1b82b7f2d846b
p0psicles <rogier@headshots.nl>
2021-01-24 16:50:30 +0100

    Revert rtorrent change. Log is not useful

--- medusa/clients/torrent/rtorrent.py
+++ medusa/clients/torrent/rtorrent.py
@@ -206,11 +206,6 @@ class RTorrentAPI(GenericClient):
         # Store resource
         client_status.resource = torrent.base_filename
 
-        log.info('Rtorrent torrent: [{name}] using state: [{state}]', {
-            'name': torrent['name'], 'state': torrent['state']
-        })
-
-
         return client_status
 
 
