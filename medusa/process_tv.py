964bae5cfec0102866ebf8d7eb3840f8d2543e9c
p0psicles <rogier@headshots.nl>
2021-04-27 16:15:30 +0200

    Remove log

--- medusa/process_tv.py
+++ medusa/process_tv.py
@@ -472,7 +472,6 @@ class ProcessResult(object):
             topdown = True if self.directory == path_name else False
             for root, dirs, files in os.walk(path_name, topdown=topdown):
                 if files:
-                    self.log_and_output('Yielding following folder for processing {root}', level=logging.INFO, **{'root': root})
                     yield root, sorted(files)
                 if topdown:
                     break
