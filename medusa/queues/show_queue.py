470ef0158ba7b3b838f6625a96ed0c7ee02cfeb2
p0psicles <rogier@headshots.nl>
2021-07-04 12:37:40 +0200

    Only run the refresh when show is added from existing.

--- medusa/queues/show_queue.py
+++ medusa/queues/show_queue.py
@@ -407,8 +407,6 @@ class QueueItemAdd(ShowQueueItem):
                 log.warning('Unable to load series from indexer: {0!r}'.format(error))
                 raise SaveSeriesException('Unable to load series from indexer: {0!r}'.format(error))
 
-            message_step('check if show is already added')
-
             try:
                 message_step('configure show options')
                 series.configure(self)
@@ -459,11 +457,14 @@ class QueueItemAdd(ShowQueueItem):
             message_step('add scene numbering')
             series.add_scene_numbering()
 
-            message_step('refresh episodes from disk')
-            try:
-                app.show_queue_scheduler.action.refreshShow(series)
-            except CantRefreshShowException as error:
-                log.warning('Unable to rescan episodes from disk: {0!r}'.format(error))
+            if self.show_dir:
+                # If a show dir was passed, this was added as an existing show.
+                # For new shows we should have any files on disk.
+                message_step('refresh episodes from disk')
+                try:
+                    app.show_queue_scheduler.action.refreshShow(series)
+                except CantRefreshShowException as error:
+                    log.warning('Unable to rescan episodes from disk: {0!r}'.format(error))
 
         except SaveSeriesException as error:
             log.warning('Unable to add series: {0!r}'.format(error))
