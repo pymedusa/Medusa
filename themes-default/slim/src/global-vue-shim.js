b9f686b85d9f1c86b4711026250a2e4dd127d177
p0psicles <rogier@headshots.nl>
2021-10-28 12:31:20 +0200

    Cleaned up global component loading.

--- themes-default/slim/src/global-vue-shim.js
+++ themes-default/slim/src/global-vue-shim.js
@@ -14,31 +14,7 @@ library.add([faAlignJustify, faImages, faTimesCircle]);
 
 import {
     App,
-    AddShowOptions,
-    Alerts,
-    AnidbReleaseGroupUi,
-    AppFooter,
-    AppHeader,
-    AppLink,
-    Asset,
-    Backstretch,
-    ConfigTemplate,
-    ConfigTextbox,
-    ConfigTextboxNumber,
-    ConfigToggleSlider,
-    FileBrowser,
-    LanguageSelect,
-    LoadProgressBar,
-    PlotInfo,
-    QualityChooser,
-    QualityPill,
-    RootDirs,
-    Schedule,
-    ScrollButtons,
-    SelectList,
-    ShowSelector,
-    StateSwitch,
-    SubMenu
+    LoadProgressBar
 } from './components';
 import store from './store';
 import { isDevelopment } from './utils/core';
@@ -54,45 +30,7 @@ export const registerGlobalComponents = () => {
     // @TODO: These should be registered in an `App.vue` component when possible,
     //        along with some of the `main.mako` template
     components = components.concat([
-        App,
-        Alerts,
-        AppFooter,
-        AppHeader,
-        ScrollButtons,
-        SubMenu
-    ]);
-
-    // Add global components (in use by pages/components that are not SFCs yet)
-    // Use this when it's not possible to use `components: { ... }` in a component's definition.
-    // If a component that uses any of these is a SFC, please use the `components` key when defining it.
-    // @TODO: Instead of globally registering these,
-    //        they should be registered in each component that uses them
-    components = components.concat([
-        AddShowOptions,
-        AnidbReleaseGroupUi,
-        AppLink,
-        Asset,
-        Backstretch,
-        ConfigTemplate,
-        ConfigTextbox,
-        ConfigTextboxNumber,
-        ConfigToggleSlider,
-        FileBrowser,
-        LanguageSelect,
-        LoadProgressBar,
-        PlotInfo,
-        QualityChooser,
-        QualityPill, // @FIXME: (sharkykh) Used in a hack/workaround in `static/js/ajax-episode-search.js`
-        RootDirs,
-        SelectList,
-        ShowSelector,
-        StateSwitch
-    ]);
-
-    // Add components for pages that use `pageComponent`
-    // @TODO: These need to be converted to Vue SFCs
-    components = components.concat([
-        Schedule
+        App
     ]);
 
     // Register the components globally
