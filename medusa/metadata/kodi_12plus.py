a572ca2267b4502a2d5372eae6eba8bb3648815e
p0psicles <rogier@headshots.nl>
2022-02-21 20:15:42 +0100

    fix kodi metadata actor error

--- medusa/metadata/kodi_12plus.py
+++ medusa/metadata/kodi_12plus.py
@@ -373,11 +373,11 @@ class KODI_12PlusMetadata(generic.GenericMetadata):
                     else:
                         continue
 
-                    if 'role' in actor and actor['role'].strip():
+                    if 'role' in actor and actor['role'] and actor['role'].strip():
                         cur_actor_role = etree.SubElement(cur_actor, 'role')
                         cur_actor_role.text = actor['role'].strip()
 
-                    if 'image' in actor and actor['image'].strip():
+                    if 'image' in actor and actor['image'] and actor['image'].strip():
                         cur_actor_thumb = etree.SubElement(cur_actor, 'thumb')
                         cur_actor_thumb.text = actor['image'].strip()
 
