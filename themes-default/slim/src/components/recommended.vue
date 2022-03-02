ee21bd2b91e6df72f29d683a69488e2a96f29b22
p0psicles <rogier@headshots.nl>
2022-03-02 09:32:43 +0100

    Renamed to load more.

--- themes-default/slim/src/components/recommended.vue
+++ themes-default/slim/src/components/recommended.vue
@@ -72,8 +72,8 @@
                         Search for new recommended shows from {{sourceToString[selectedSource]}}
                     </button>
                 </div>
-                <div v-else-if="page[selectedSource] !== -1" class="get-more">
-                    <button class="btn-medusa" @click="getMore">Get More</button>
+                <div v-else-if="page[selectedSource] !== -1" class="load-more">
+                    <button class="btn-medusa" @click="getMore">Load More</button>
                 </div>
             </div> <!-- End of col -->
         </div> <!-- End of row -->
@@ -423,7 +423,7 @@ span.trakt-warning {
     color: red;
 }
 
-.get-more {
+.load-more {
     display: flex;
     justify-content: center;
 }
