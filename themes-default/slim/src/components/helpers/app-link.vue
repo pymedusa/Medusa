fa7ec5c7413f95151e449539cffc3d5a223063c7
p0psicles <rogier@headshots.nl>
2022-03-09 15:58:58 +0100

    Fix router-links with webroot

--- themes-default/slim/src/components/helpers/app-link.vue
+++ themes-default/slim/src/components/helpers/app-link.vue
@@ -94,7 +94,7 @@ export default {
                 return;
             }
 
-            const { route } = router.resolve(routerBase + computedHref);
+            const { route } = router.resolve('/' + computedHref);
             if (!route.name) {
                 return;
             }
