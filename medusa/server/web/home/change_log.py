e60b624b3181c037f263cf67348b0059bb9ce276
p0psicles <rogier@headshots.nl>
2022-03-18 17:46:06 +0100

    Remove missing imports

--- medusa/server/web/home/change_log.py
+++ medusa/server/web/home/change_log.py
@@ -2,19 +2,14 @@
 
 from __future__ import unicode_literals
 
-import markdown2
-
-from medusa import app, logger
 from medusa.server.web.core import PageTemplate
 from medusa.server.web.home.handler import Home
-from medusa.session.core import MedusaSafeSession
 
 from tornroutes import route
 
 
 @route('/changes(/?.*)')
 class HomeChangeLog(Home):
-    session = MedusaSafeSession()
 
     def __init__(self, *args, **kwargs):
         super(HomeChangeLog, self).__init__(*args, **kwargs)
