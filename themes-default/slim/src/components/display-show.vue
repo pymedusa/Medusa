2745c54b230edf77c711f9942f8b7f363a802c33
p0psicles <rogier@headshots.nl>
2022-06-24 13:18:51 +0200

    Always show subtitle flag
    * Not only for Archived / Downloaded.

--- themes-default/slim/src/components/display-show.vue
+++ themes-default/slim/src/components/display-show.vue
@@ -108,7 +108,7 @@
                         </span>
 
                         <span v-else-if="props.column.label == 'Subtitles'" class="align-center">
-                            <div class="subtitles" v-if="['Archived', 'Downloaded', 'Ignored', 'Skipped'].includes(props.row.status)">
+                            <div class="subtitles">
                                 <div v-for="flag in props.row.subtitles" :key="flag">
                                     <img v-if="flag !== 'und'" :src="`images/subtitles/flags/${flag}.png`" width="16" height="11" :alt="flag" onError="this.onerror=null;this.src='images/flags/unknown.png';" @click="searchSubtitle($event, props.row, flag)">
                                     <img v-else :src="`images/subtitles/flags/${flag}.png`" class="subtitle-flag" width="16" height="11" :alt="flag" onError="this.onerror=null;this.src='images/flags/unknown.png';">
@@ -255,7 +255,7 @@
                         </span>
 
                         <span v-else-if="props.column.label == 'Subtitles'" class="align-center">
-                            <div class="subtitles" v-if="['Archived', 'Downloaded', 'Ignored', 'Skipped'].includes(props.row.status)">
+                            <div class="subtitles">
                                 <div v-for="flag in props.row.subtitles" :key="flag">
                                     <img v-if="flag !== 'und'" :src="`images/subtitles/flags/${flag}.png`" width="16" height="11" alt="{flag}" onError="this.onerror=null;this.src='images/flags/unknown.png';" @click="searchSubtitle($event, props.row, flag)">
                                     <img v-else :src="`images/subtitles/flags/${flag}.png`" class="subtitle-flag" width="16" height="11" alt="flag" onError="this.onerror=null;this.src='images/flags/unknown.png';">
