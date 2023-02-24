32694ba5185c845eae299344c9a7871385ca39c2
sawyersteven <40702009+sawyersteven@users.noreply.github.com>
2023-02-23 23:01:30 -0500

    RSS test modifications
    Change import from ET to ElemTree and change whitespace location in expected string to satisfy Flake8. This doesn't affect the test, but unclogs the test output a little.
    
    Recursive test fails on assertion rather than returning bool to bubble up through the nested generators.
    
    Strip newlines from expected string before parsing in ElementTree

--- tests/clients/test_rss.py
+++ tests/clients/test_rss.py
@@ -1,14 +1,14 @@
 import os
 import re
 import medusa
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElemTree
 from medusa.clients import rss
 
 
 EXPECTED_RAW_TEXT = """<rss xmlns:medusa="https://pymedusa.com" version="2.0"><title>Medusa RSS Feed</title><link>https://pymedusa.com/</link><description>
 Medusa RSS Feed</description><channel><item><title>test_release_name</title><link>https://test_provider.com/test_result.url</link><guid isPermalink="false">
-test_guid</guid><description>test_episode_name | test_episode_description</description><enclosure url="https://test_provider.com/test_result.url" length="0" 
-type="application/x-bittorrent" /><medusa:series isAnime="test_is_anime" tvdb="test_tvdbid" imdb="test_imdbid">test_series_name</medusa:series><medusa:season>
+test_guid</guid><description>test_episode_name | test_episode_description</description><enclosure url="https://test_provider.com/test_result.url" length="0"
+ type="application/x-bittorrent" /><medusa:series isAnime="test_is_anime" tvdb="test_tvdbid" imdb="test_imdbid">test_series_name</medusa:series><medusa:season>
 test_actual_season</medusa:season><medusa:episode>test_actual_episode</medusa:episode><medusa:provider>test_provider</medusa:provider></item></channel></rss>"""
 
 
@@ -46,21 +46,21 @@ def test_add_result_to_feed():
     os.unlink(file_path)
     rss_raw_text = re.sub(r'<pubDate>(.*)<\/pubDate>', '', rss_raw_text)
 
-    result_et = ET.fromstring(rss_raw_text)
-    expected_et = ET.fromstring(EXPECTED_RAW_TEXT)
+    result_et = ElemTree.fromstring(rss_raw_text)
+    expected_et = ElemTree.fromstring(EXPECTED_RAW_TEXT.replace('\n', ''))
 
-    assert all(elements_equal(e1, e2) for e1, e2 in zip(result_et, expected_et))
+    for (e1, e2) in zip(result_et, expected_et):
+        elements_equal(e1, e2)
 
 
 def elements_equal(elem1, elem2):
-    if elem1.tag != elem2.tag:
-        return False
-    if elem1.text != elem2.text:
-        return False
-    if elem1.tail != elem2.tail:
-        return False
-    if elem1.attrib != elem2.attrib:
-        return False
-    if len(elem1) != len(elem2):
-        return False
-    return all(elements_equal(e1, e2) for e1, e2 in zip(elem1, elem2))
+    """
+    Recursively asserts equality between elementtree elements and children.
+    """
+    assert elem1.tag == elem2.tag
+    assert elem1.text == elem2.text
+    assert elem1.tail == elem2.tail
+    assert elem1.attrib == elem2.attrib
+    assert len(elem1) == len(elem2)
+    for (e1, e2) in zip(elem1, elem2):
+        elements_equal(e1, e2)
