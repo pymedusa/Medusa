7a5fab4d9a95fe53d83c92b5dd1f91765e131129
Christophe Painchaud <shellescape@gmail.com>
2023-07-14 15:42:50 +0200

    Update yggtorrent.py to use new URL yggtorrent.wtf (#11404)
    Co-authored-by: Dario <medariox@users.noreply.github.com>

--- medusa/providers/torrent/html/yggtorrent.py
+++ medusa/providers/torrent/html/yggtorrent.py
@@ -36,7 +36,7 @@ class YggtorrentProvider(TorrentProvider):
         self.password = None
 
         # URLs
-        self.url = 'https://yggtorrent.do'
+        self.url = 'https://yggtorrent.wtf'
         self.urls = {
             'auth': urljoin(self.url, 'user/ajax_usermenu'),
             'login': urljoin(self.url, 'user/login'),
