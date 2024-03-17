af810d28f36c7b9f308ceb09749f374d8a7a8f1b
Maksymilian Babarowski <maksymilian@babarowski.pl>
2024-03-17 13:35:29 +0100

    Fix parsing release group in Animebytes results (#11648)

--- medusa/providers/torrent/json/animebytes.py
+++ medusa/providers/torrent/json/animebytes.py
@@ -177,6 +177,8 @@ class AnimeBytes(TorrentProvider):
                                     {'properties': properties_string, 'error': error})
                         continue
 
+                    # the release group could be found in the last segment for finished series
+                    # or in the penultimate segment for ongoing series
                     release_group = ""
                     for i in [-1, -2]:
                         last_field = re.match(r'(.*)\((.*)\)', properties[i])
