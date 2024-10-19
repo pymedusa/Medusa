0e1825795d186318fc8c36129d161a2dbcba9170
p0psicles <rogier@headshots.nl>
2024-10-19 11:31:56 +0200

    Improved UI manage-whitelist  component

--- themes-default/slim/src/components/show-results/manage-whitelist.vue
+++ themes-default/slim/src/components/show-results/manage-whitelist.vue
@@ -4,7 +4,7 @@
         <div v-if="open" class="manage-whitelist-control-wrapper">
             <div class="title-header">
                 <span class="title">{{releaseGroup}}</span>
-                <svg class="manage-whitelist-close" @click="open = !open">
+                <svg class="manage-whitelist-close spin-hover-90" @click="open = !open">
                     <use xlink:href="images/svg/close.svg#close" />
                 </svg>
             </div>
@@ -47,6 +47,7 @@ export default {
                 this.show.config.release.whitelist.push(releaseGroup);
                 this.save(`adding release group ${releaseGroup} from the whitelist`);
             }
+            this.open = false;
         },
         removeFromWhitelist() {
             const { releaseGroup, show } = this;
@@ -54,6 +55,7 @@ export default {
                 this.show.config.release.whitelist = this.show.config.release.whitelist.filter(group => group.toLowerCase() !== releaseGroup.toLowerCase());
                 this.save(`removing release group ${releaseGroup} from the whitelist`);
             }
+            this.open = false;
         },
         addToBlacklist() {
             const { releaseGroup, show } = this;
@@ -61,6 +63,7 @@ export default {
                 this.show.config.release.blacklist.push(releaseGroup);
                 this.save(`adding release group ${releaseGroup} from the blacklist`);
             }
+            this.open = false;
         },
         removeFromBlacklist() {
             const { releaseGroup, show } = this;
@@ -68,6 +71,7 @@ export default {
                 this.show.config.release.blacklist = this.show.config.release.blacklist.filter(group => group.toLowerCase() !== releaseGroup.toLowerCase());
                 this.save(`removing release group ${releaseGroup} from the blacklist`);
             }
+            this.open = false;
         },
         async save(message) {
             const { saveShowConfig, show } = this;
@@ -95,7 +99,7 @@ export default {
     top: 25px;
     left: 0;
     background-color: rgb(51, 51, 51);
-    padding: 1rem;
+    padding: 1rem 1.2rem;
     border-radius: 4px;
     z-index: 1;
     flex-flow: column;
@@ -125,7 +129,7 @@ export default {
 .manage-whitelist-control-wrapper form {
     display: flex;
     flex-flow: column;
-    gap: 5px;
+    gap: 10px;
 }
 
 .manage-whitelist-control-wrapper button {
